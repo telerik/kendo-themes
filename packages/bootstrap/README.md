5112cb6ee275ef4f8971d507b6c756fde810877f
joneff <joneff@gmail.com>
2016-01-28 16:11:04 +0200

    initial state based ot k2 repo

---
+++ packages/bootstrap/README.md
@@ -0,0 +1,7 @@
+# Bootstrap v4 integration for Kendo widgets
+
+Extends Bootstrap theme for use with Kendo UI widgets.
+
+## Building
+
+Install build dependencies via `npm install`. Run `npm run build` to build the css.
---
+++ packages/bootstrap/gulpfile.js
@@ -0,0 +1,23 @@
+/* jshint esnext: true */
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const $ = require('gulp-load-plugins')();
+
+const STYLES = 'src/**/*.scss';
+
+gulp.task('start', ['sass'], function() {
+    browserSync.init({
+        server: ".",
+        startPath: "demo/index.html"
+    });
+
+    return gulp.watch(STYLES, ["sass"]);
+});
+
+gulp.task("sass", function() {
+    return gulp.src(STYLES)
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe(browserSync.stream({match: '**/*.css'}))
+        .pipe($.rename({dirname: ''}))
+        .pipe(gulp.dest('dist'));
+});
