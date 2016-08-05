4f035d265311f97f0271c007340a497085b130ae
joneff <joneff@gmail.com>
2016-08-05 16:07:31 +0300

    Merge branch 'master' of github.com:telerik/kendo-theme-bootstrap

---
+++ packages/bootstrap/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+function BrowserSync() {
+    this.bs = require("browser-sync").create('bs-refresh');
+    this.bs.init({
+        open: false,
+        proxy: 'http://localhost:8080/',
+        port: 3000,
+        host: '0.0.0.0'
+    });
+}
+BrowserSync.prototype = {
+    apply: function(compiler) {
+        compiler.plugin("emit", (compilation, callback) => {
+            this.bs.reload("main.css");
+            callback();
+        });
+    }
+};
+
+module.exports = require('@telerik/kendo-common-tasks')
+    .webpackThemeConfig({ extract: true }, {
+        module: { loaders: [] },
+        entry: {
+            main: './src/main.js'
+        },
+        plugins: [
+            new BrowserSync()
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/dist/',
+            filename: '[name].js'
+        }
+    });
+
