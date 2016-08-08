0cf26dfa6d36b8de1dea84061969c5d39d0bd904
Kiril Nikolov <kiril.nikolov@gmail.com>
2016-08-08 11:25:34 +0300

    chore: enable webpack build for client apps

--- packages/bootstrap/webpack.config.js
+++ packages/bootstrap/webpack.config.js
@@ -1,5 +1,37 @@
-const path = require('path');
-const inDevelopment = process.argv.find(v => v.includes('webpack-dev-server'))
+/*
+ * Build CSS out of SCSS files using webpack, reusing the
+ * configuration already in place for react / ng2 components.
+ * This script creates a webpack entry for each of the packages,
+ * and passes it through the kendo-common-tasks theme config.
+ */
+var glob = require('glob');
+var path = require('path');
+var fs = require('fs');
+
+var FILES = 'src/packages/*.scss';
+
+var tmpDir = './.tmp/';
+var createTmpDir = true;
+try {
+    createTmpDir = !fs.statSync(tmpDir).isDirectory();
+} catch(e) {}
+
+if (createTmpDir) {
+    fs.mkdirSync(tmpDir);
+}
+
+var entries =
+    glob.sync(FILES)
+        .reduce(function(entries, filename) {
+            var name = path.basename(filename, '.scss');
+            var entry = tmpDir + name + '.js';
+
+            fs.writeFileSync(entry, 'require("./../' + filename + '")');
+
+            entries[name] = entry;
+
+            return entries;
+        }, {});
 
 function BrowserSync() {
     this.bs = require("browser-sync").create('bs-refresh');
@@ -19,12 +51,11 @@ BrowserSync.prototype = {
     }
 };
 
+const inDevelopment = process.argv.find(v => v.includes('webpack-dev-server'))
 module.exports = require('@telerik/kendo-common-tasks')
     .webpackThemeConfig({ extract: true }, {
         module: { loaders: [] },
-        entry: {
-            main: './src/main.js'
-        },
+        entry: entries,
         plugins:
             inDevelopment ? [
                 new BrowserSync()
@@ -35,4 +66,3 @@ module.exports = require('@telerik/kendo-common-tasks')
             filename: '[name].js'
         }
     });
-
