1f5946887a3f844d3b11fda82b566a2175fa7428
Alex Gyoshev <alex@gyoshev.net>
2016-10-14 18:06:35 +0300

    chore: extract tasks from theme-bootstrap

--- packages/default/webpack.config.js
+++ packages/default/webpack.config.js
@@ -8,7 +8,7 @@ var glob = require('glob');
 var path = require('path');
 var fs = require('fs');
 
-var FILES = 'styles/packages/*.scss';
+var FILES = '{src,styles}/packages/*.scss';
 
 var tmpDir = './.tmp/';
 var createTmpDir = true;
@@ -33,13 +33,44 @@ var entries =
             return entries;
         }, {});
 
+const port = process.env.PORT || 3000;
+const devServerPort = port + 1;
+
+/*
+ * Webpack plug-in that reloads css via browser-sync upon build
+ */
+function BrowserSync() {
+    this.bs = require("browser-sync").create('bs-refresh');
+    this.bs.init({
+        open: false,
+        proxy: `http://localhost:${devServerPort}/`,
+        port: port,
+        host: '0.0.0.0'
+    });
+}
+BrowserSync.prototype = {
+    apply: function(compiler) {
+        compiler.plugin("emit", (compilation, callback) => {
+            this.bs.reload("all.css");
+            callback();
+        });
+    }
+};
+
+const inDevelopment = process.argv.find(v => v.includes('webpack-dev-server'))
 module.exports = require('@telerik/kendo-common-tasks')
     .webpackThemeConfig({ extract: true }, {
+        devServer: {
+            hot: true,
+            inline: true,
+            port: devServerPort
+        },
         module: { loaders: [] },
         entry: entries,
+        plugins: inDevelopment ? [ new BrowserSync() ] : [],
         output: {
-            publicPath: './dist/',
-            path: './dist/',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/dist/',
             filename: '[name].js'
         }
     });
