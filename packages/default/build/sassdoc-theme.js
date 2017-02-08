58d7f0a79adf2fc2c60962fc68ac3cdd1f4d94de
Alex Gyoshev <alex@gyoshev.net>
2017-02-08 18:03:48 +0200

    chore: build basic customization help topic

--- packages/default/build/sassdoc-theme.js
+++ packages/default/build/sassdoc-theme.js
@@ -1,8 +1,27 @@
-var fs = require("fs");
-var path = require("path");
+'use strict';
 
-module.exports = function(dest, ctx) {
+const fs = require('fs');
+const path = require('path');
+const hbs = require('handlebars');
+const read = (src) =>
+    fs.readFileSync(path.join(__dirname, src), { encoding: 'utf-8' });
+const compile = (src) =>
+    hbs.compile(read(src), { noEscape: true });
+const template = compile('customization.md.hbs');
+
+module.exports = function(dest, context) {
+    const data = context.data
+        .filter((item) => item.access == 'public')
+        .reduce((acc, item) => {
+            acc[item.context.type].push(item);
+            return acc;
+        }, {
+            mixin: [],
+            variable: []
+        });
+
+
     return new Promise(function(done, error) {
-        fs.writeFile(path.join(dest, "customization.md"), "");
+        fs.writeFile(path.join('docs', 'customization.md'), template(data));
     });
 };
