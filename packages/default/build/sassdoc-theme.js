e4090e2919c72bd8b0495558cb411e0168959144
Alex Gyoshev <alex@gyoshev.net>
2017-02-09 18:28:42 +0200

    chore: add more topic styles, group variables

--- packages/default/build/sassdoc-theme.js
+++ packages/default/build/sassdoc-theme.js
@@ -5,11 +5,17 @@ const path = require('path');
 const hbs = require('handlebars');
 const read = (src) =>
     fs.readFileSync(path.join(__dirname, src), { encoding: 'utf-8' });
+
+hbs.registerHelper('formatText', (str) =>
+    str.replace(/\n(.)/g, '<br/>\n$1'));
+
 const compile = (src) =>
     hbs.compile(read(src), { noEscape: true });
 const template = compile('customization.md.hbs');
 
 module.exports = function(dest, context) {
+    const capitalize = (str) => str[0].toUpperCase() + str.substring(1);
+    const isColor = /^#/i;
     const data = context.data
         .filter((item) => item.access == 'public')
         .reduce((acc, item) => {
@@ -20,6 +26,14 @@ module.exports = function(dest, context) {
             variable: []
         });
 
+    data.variable = data.variable.reduce((acc, item) => {
+        const groupId = item.group[0];
+        const group = context.groups[groupId] || capitalize(groupId);
+        item.isColor = isColor.test(item.context.value);
+        acc[group] = acc[group] || [];
+        acc[group].push(item);
+        return acc;
+    }, {});
 
     return new Promise(function(done, error) {
         fs.writeFile(path.join('docs', 'customization.md'), template(data));
