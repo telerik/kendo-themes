e321138d731b7739a0a657415e23df2dfb5a8a50
Alex Gyoshev <alex@gyoshev.net>
2017-06-01 09:38:30 +0300

    chore: use parameters when embedding dependencies, validate build

--- packages/bootstrap/build/embed-dependencies.sh
+++ packages/bootstrap/build/embed-dependencies.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/bash
 
+# Usage: embed-dependnecies.sh [module-name]...
+
 mkdir -p modules
-mkdir -p modules/bootstrap
-mkdir -p modules/@progress/kendo-theme-default
 
-# copy dependencies from node_modules/ to modules/ subdir
-rsync -avc --delete --include "**.scss" node_modules/bootstrap/ modules/bootstrap/
-rsync -avc --delete --include "**.scss" node_modules/@progress/kendo-theme-default/ modules/@progress/kendo-theme-default/
+for module in "$@"
+do
+  # copy dependencies from node_modules/ to modules/ subdir
+  echo "Copy $module dependency"
+  mkdir -p modules/$module
+  rsync -avc --delete --include "**.scss" node_modules/$module/ modules/$module/
+done
 
-# replace references to modules
+# replace references to modules with relative paths
 find ./scss/ -maxdepth 1 -type f -name "*.scss" -exec sed -i 's#\(@import\s\+\)\(''\|\"\)~\(.*\);#\1\2../modules/\3;#' "{}" \;
 find ./scss/ -maxdepth 2 -type f -name "*.scss" -exec sed -i 's#\(@import\s\+\)\(''\|\"\)~\(.*\);#\1\2../../modules/\3;#' "{}" \;
+find ./scss/ -maxdepth 3 -type f -name "*.scss" -exec sed -i 's#\(@import\s\+\)\(''\|\"\)~\(.*\);#\1\2../../../modules/\3;#' "{}" \;
+
