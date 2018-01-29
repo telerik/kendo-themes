cbe137fc44f6dc1b2b9d873f2723ea8db4b80488
joneff <joneff@gmail.com>
2018-01-29 18:32:50 +0200

    chore(test): improve test data

--- tests/data/metrics.js
+++ tests/data/metrics.js
@@ -1,9 +1,87 @@
 var metrics = {
+
+    // Button
     "button": {
         "height": 30
     },
     "iconButton": {
         "width": 30,
         "height": 30
+    },
+
+    // Inputs
+    "autocomplete": {
+        "height": 30
+    },
+    "colorpicker": {
+        "width": 58,
+        "height": 30
+    },
+    "combobox": {
+        "height": 30
+    },
+    "datetime": {
+        "height": 30
+    },
+    "dropdown": {
+        "height": 30
+    },
+    "maskedtextbox": {
+        "height": 30
+    },
+    "multiselect": {
+        "height": 30,
+        "token": {
+            "height": 24
+        }
+    },
+    "numeric": {
+        "height": 30
+    },
+    "slider": {
+        "height": 30,
+        "buttons": {
+            "width": 30,
+            "height": 30
+        },
+        "handle": {
+            "width": 16,
+            "height": 16
+        }
+    },
+    "listbox": {},
+    "upload": {
+        "height": 48,
+        "button": {
+            "height": 30
+        }
+    },
+    "switch": {
+        "width": 60,
+        "height": 30
+    },
+
+    // Window
+    "window": {
+        "titlebar": {
+            "height": 45
+        },
+        "actions": {
+            "width": 30,
+            "height": 30
+        }
+    },
+    "dialog": {
+        "titlebar": {
+            "height": 45
+        },
+        "actions": {
+            "width": 30,
+            "height": 30
+        },
+        "buttons": {
+            "height": 44
+        }
     }
+
 };
