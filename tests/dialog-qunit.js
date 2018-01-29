f0e9e13b65fcd03c1f0a6d77d8b45dfa782b1196
joneff <joneff@gmail.com>
2018-01-29 18:34:36 +0200

    test(dialog): add tests for dialog

---
+++ tests/dialog-qunit.js
@@ -0,0 +1,38 @@
+QUnit.module("Window size", {
+    beforeEach: function() {
+        TestHelper.loadFixture( "/base/tests/fixtures/dialog.html" );
+    },
+    afterEach: function() {
+        TestHelper.clearFixture();
+    },
+    after: function() {
+        TestHelper.clearFixture();
+    }
+});
+
+QUnit.test("Dialog titlebar height should be 45 with title", function( assert ) {
+    var dialog = $("#dialog").parents(".k-dialog");
+    var titlebar = dialog.find(".k-dialog-titlebar");
+    var titlebarHeight = getHeight( titlebar );
+
+    assert.equal( titlebarHeight, metrics.dialog.titlebar.height );
+});
+
+QUnit.test("Dialog titlebar actions should be size 30:30", function( assert ) {
+
+    var dialog = $("#dialog").parents(".k-dialog");
+    var action = dialog.find(".k-dialog-actions .k-dialog-action").get(0);
+    var actionSize = getSize( action );
+
+    assert.equal( actionSize.width, metrics.dialog.actions.width );
+    assert.equal( actionSize.height, metrics.dialog.actions.height );
+});
+
+QUnit.test("Dialog buttons height should be 44", function( assert ) {
+
+    var dialog = $("#dialog").parents(".k-dialog");
+    var button = dialog.find(".k-dialog-buttongroup .k-button").get(0);
+    var buttonSize = getSize( button );
+
+    assert.equal( buttonSize.height, metrics.dialog.buttons.height );
+});
