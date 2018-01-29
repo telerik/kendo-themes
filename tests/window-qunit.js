d1004ff0aa91ea3a6dcde84c5f1790a95ee83c95
joneff <joneff@gmail.com>
2018-01-29 18:34:09 +0200

    test(window): add tests for window

---
+++ tests/window-qunit.js
@@ -0,0 +1,29 @@
+QUnit.module("Window size", {
+    beforeEach: function() {
+        TestHelper.loadFixture( "/base/tests/fixtures/window.html" );
+    },
+    afterEach: function() {
+        TestHelper.clearFixture();
+    },
+    after: function() {
+        TestHelper.clearFixture();
+    }
+});
+
+QUnit.test("Window titlebar height should be 45 with title", function( assert ) {
+    var window = $("#window").parents(".k-window");
+    var titlebar = window.find(".k-window-titlebar");
+    var titlebarHeight = getHeight( titlebar );
+
+    assert.equal( titlebarHeight, metrics.window.titlebar.height );
+});
+
+QUnit.test("Window titlebar actions should be size 30:30", function( assert ) {
+
+    var window = $("#window").parents(".k-window");
+    var action = window.find(".k-window-actions .k-window-action").get(0);
+    var actionSize = getSize( action );
+
+    assert.equal( actionSize.width, metrics.window.actions.width );
+    assert.equal( actionSize.height, metrics.window.actions.height );
+});
