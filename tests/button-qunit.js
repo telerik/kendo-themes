6b5947a8590e9d3e24005a39877541c539460f0a
joneff <joneff@gmail.com>
2018-01-29 18:33:37 +0200

    test(button): add tests for icon button

--- tests/button-qunit.js
+++ tests/button-qunit.js
@@ -1,8 +1,8 @@
 QUnit.module("Button size", {
-    before: function() {
+    beforeEach: function() {
         TestHelper.loadFixture( "/base/tests/fixtures/button.html" );
     },
-    after: function() {
+    afterEach: function() {
         TestHelper.clearFixture();
     }
 });
@@ -12,4 +12,12 @@ QUnit.test("Generic button height should be 30", function( assert ) {
     var buttonHeight = getHeight( button );
 
     assert.equal( buttonHeight, metrics.button.height );
+});
+
+QUnit.test("Icon button size should be 30:30", function( assert ) {
+    var iconButton = $("#icon-button");
+    var iconButtonSize = getSize( iconButton );
+
+    assert.equal( iconButtonSize.width, metrics.iconButton.width );
+    assert.equal( iconButtonSize.height, metrics.iconButton.height );
 });
