446fa3439b634ac586ce98532db9c5025279e071
joneff <joneff@gmail.com>
2018-01-30 15:41:46 +0200

    test(multiselect): add tests for multiselect

---
+++ tests/multiselect-qunit.js
@@ -0,0 +1,23 @@
+QUnit.module("Multiselect", {
+    beforeEach: function() {
+        TestHelper.loadFixture( "/base/tests/fixtures/multiselect.html" );
+    },
+    afterEach: function() {
+        TestHelper.clearFixture();
+    }
+});
+
+QUnit.test("Multiselect height should be 30", function( assert ) {
+    var $multiselect = $("#multiselect");
+    var multiselectHeight = getHeight( $multiselect );
+
+    assert.equal( multiselectHeight, metrics.multiselect.height );
+});
+
+QUnit.test("Multiselect token height should be 24", function( assert ) {
+    var $multiselect = $("#multiselect");
+    var $token = $multiselect.find(".k-multiselect-wrap ul .k-button").eq(0);
+    var tokenHeight = getHeight( $token );
+
+    assert.equal( tokenHeight, metrics.multiselect.token.height );
+});
