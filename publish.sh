86fae4c168659ad616b0306a1b8be96fd1fc7058
Alex Gyoshev <alex@gyoshev.net>
2018-01-31 13:32:06 +0200

    chore: add verbose logging, allow pushing canary builds from develop

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 echo $BRANCH
 
 if [[ $BRANCH == 'develop' ]]; then
-  lerna publish --skip-npm --canary=dev --npm-tag=dev --yes
+  lerna publish --skip-npm --canary=dev --npm-tag=dev --allow-branch=develop --loglevel=verbose
 else
-  lerna publish --message "chore: release packages, update changelog" --skip-npm --conventional-commits --yes
+  lerna publish --message "chore: release packages, update changelog" --skip-npm --conventional-commits --loglevel=verbose
 fi
