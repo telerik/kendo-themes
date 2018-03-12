402a7f849b0e4146ff7d385829281ee52ade10ed
Alex Gyoshev <alexander.gyoshev@gmail.com>
2018-03-12 18:25:38 +0200

    chore: add visual tests (#54)
    * chore: add npm script for visual tests
    
    * chore: create screenshots of visual fixtures
    
    * chore: improve test fixtures
    
    * chore: add test output
    
    * chore: fix script under OSX, use Chrome
    
    * chore: ignore DS_Store
    
    * test: add font family to visual tests
    
    * chore: add visual tests to CI
    
    * chore: update fixtures to allow targeted screenshots
    
    * chore: update terminal colors
    
    * chore: add more button tests
    
    * chore: add tests for dropdowns
    
    * chore: add tests for inputs
    
    * chore: add tests for dateinputs
    
    * chore: commit back images from travis
    
    * chore: fix condition for visual tests
    
    * chore: fix build condition
    
    * chore: output dependent env vars
    
    * chore: use correct branch env var
    
    * chore: fetch origin info before trying to push
    
    * chore: run visual tests on branch builds, not PR builds
    
    PR builds test the repo after a merge commit, and this is not something this build should push
    
    * chore: update visual previews

---
+++ build/visual-test.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [[ $TRAVIS_PULL_REQUEST != 'false' ]]
+then
+  echo "Visual tests do not run on pull request builds (\$TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST)"
+  echo " (PR builds run against the merge commit with the target branch)"
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == '' ]]
+then
+  echo "Visual tests must run on a branch (\$TRAVIS_BRANCH=$TRAVIS_BRANCH)"
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == 'develop' || $TRAVIS_BRANCH == 'master' ]]
+then
+  echo 'Refusing to build on (and possibly push to) the master or develop branches'
+fi
+
+# enable push access
+git remote set-url origin https://${GH_TOKEN}@github.com/telerik/kendo-themes.git > /dev/null 2>&1
+
+# fetch branch info
+git fetch origin > /dev/null 2>&1
+
+# move git HEAD to branch
+git checkout --force $TRAVIS_BRANCH
+
+# sed on OSX and Linux differs, see https://stackoverflow.com/a/38595160/25427
+sedi () {
+    sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+}
+
+export -f sedi
+
+capture_with_theme() {
+    theme=$1
+    # replace theme reference
+    find tests/visual -name '*.html' | while read file; do sedi "s#packages/default/dist#packages/$theme/dist#" "$file"; done
+
+    # capture screenshots
+    npx pastshots --output tests/visual/output/$theme --serve 'tests/visual/*.html' --port 8081
+
+    # revert theme reference
+    find tests/visual -name '*.html' | while read file; do sedi "s#packages/$theme/dist#packages/default/dist#" "$file"; done
+
+}
+
+commit_changes() {
+    BOLD='\033[1;37m'
+    RED='\033[0;33m'
+    NC='\033[0m'
+    echo -e "${BOLD} Found updated screenshots, pushing commit to repository ${NC}"
+
+    # TODO: reduce repetition with publish.sh
+    echo "  Configuring git..."
+    git config user.name "Travis CI"
+    git config user.email "travis"
+
+    echo "  Creating commit..."
+    git add tests/visual/output
+    git commit -m "chore: update visual previews"
+
+    echo "  Pushing new commit..."
+    git push origin $TRAVIS_BRANCH --quiet > /dev/null 2>&1
+}
+
+capture_with_theme 'default'
+capture_with_theme 'bootstrap'
+capture_with_theme 'material'
+
+# if there are captured differences, push the new images
+git diff --exit-code --quiet -- tests/visual/output/ || commit_changes
