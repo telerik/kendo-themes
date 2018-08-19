63885e7657c4a80e1769951389894b87485aa469
joneff <joneff@gmail.com>
2018-08-19 18:03:27 +0300

    chore(test): allow animations to be toggled via parameter

--- tests/visual/assets/scripts.js
+++ tests/visual/assets/scripts.js
@@ -1,5 +1,15 @@
+// Change themes
 var matches = (/(bootstrap|material)/i).exec(window.location.search);;
 if (matches) {
     var link = document.querySelector('link[rel=stylesheet][href*=default]');
     link.href = link.href.replace(/default/i, matches[0]);
+}
+
+// Stop animations
+matches = (/(noanimations)/i).exec(window.location.search);;
+if (matches) {
+    document.documentElement.classList.add("k-no-animations");
+}
+else {
+    document.documentElement.classList.remove("k-no-animations");
 }
