d215a2a52d47ebfd21ba2be810c84fa7a87ed25a
Teya Veselinova <tveselin@progress.com>
2018-12-11 10:52:53 +0200

    chore(build): add clean setup util

---
+++ build/clean-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install theme tasks
+cd packages/theme-tasks
+npm install
+npm link
+
+# Install theme default
+cd ../default
+npm link @progress/kendo-theme-tasks
+npm install
+npm link
+
+# Install theme bootstrap
+cd ../bootstrap
+npm link @progress/kendo-theme-tasks
+npm link @progress/kendo-theme-default
+npm install
+npm link
+
+# Install theme material
+cd ../material
+npm link @progress/kendo-theme-tasks
+npm link @progress/kendo-theme-default
+npm install
+npm link
+
+# Install root packages
+cd ../../
+npm install
+npx lerna bootstrap
