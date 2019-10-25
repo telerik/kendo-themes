ceb44d2b4113de116f97b878b0fe0813508f05a8
joneff <joneff@gmail.com>
2019-10-25 13:02:46 +0300

    chore: include package-lock.json in source control

--- build/install.sh
+++ build/install.sh
@@ -2,32 +2,32 @@
 
 # Install theme tasks
 cd packages/theme-tasks
-npm install
+npm ci
 npm link
 
 # Install theme default
 cd ../default
 npm link @progress/kendo-theme-tasks
-npm install
+npm ci
 npm link
 
 # Install theme bootstrap
 cd ../bootstrap
 npm link @progress/kendo-theme-tasks
 npm link @progress/kendo-theme-default
-npm install
+npm ci
 npm link
 
 # Install theme material
 cd ../material
 npm link @progress/kendo-theme-tasks
 npm link @progress/kendo-theme-default
-npm install
+npm ci
 npm link
 
 # Install root packages
 cd ../../
-npm install
+npm ci
 
 # Bootstrap lerna
 npx lerna bootstrap
