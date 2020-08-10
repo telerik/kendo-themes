134cedf8b070d6794f1be0f32ffdde6fe1744374
joneff <joneff@gmail.com>
2020-08-10 13:05:08 +0300

    feat: simplify scripts usage

--- packages/theme-tasks/index.js
+++ packages/theme-tasks/index.js
@@ -1,40 +1,4 @@
-"use strict";
-
-const { command, opts } = require("./lib/utils/args-parser")(); // eslint-disable-line no-unused-vars
-const registry = require("./gulpfile.js").taskHelper.registry;
-
-function kendotheme() {
-    let tasks = {};
-
-    Object.entries( registry.tasks() ).forEach(function(taskEntry) {
-
-        let taskName = taskEntry[0];
-        let taskBody = taskEntry[1].unwrap();
-
-        tasks[taskName] = taskBody;
-
-    });
-
-    switch (command) {
-        case "sass": {
-            tasks["sass"]();
-            break;
-        }
-        case "dart": {
-            tasks["dart"]();
-            break;
-        }
-        case "sass:swatches": {
-            tasks["sass:swatches"]();
-            break;
-        }
-        case "dart:swatches": {
-            tasks["sass:swatches"]();
-            break;
-        }
-        default:
-            throw new Error("Unknown command");
-    }
-}
-
-module.exports = kendotheme;
+module.exports = {
+    ...require('./src/sass-build'),
+    ...require('./src/sass-flatten')
+};
