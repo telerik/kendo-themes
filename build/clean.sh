4c60dbe3a45a25c75e5eb800c218284198b427d0
joneff <joneff@gmail.com>
2020-10-13 10:07:11 +0300

    chore(infra): remove dist folders on clean install

--- build/clean.sh
+++ build/clean.sh
@@ -4,6 +4,13 @@ if [[ $TRAVIS == true ]]
 then
     echo "Travis CI detected. Skipping clean."
 else
-    echo "Local environment detected. Removing node_modules/ ..."
+    echo "Local environment detected."
+
+    echo "|"
+    echo "|- Removing node_modules/ ..."
     find . -name "node_modules" -type d -prune -exec rm -rf '{}' +
+
+    echo "|"
+    echo "\- Removing dist/ ..."
+    find . -name "dist" -type d -prune -exec rm -rf '{}' +
 fi
