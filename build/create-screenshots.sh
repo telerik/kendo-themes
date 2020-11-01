2e4aab929c1f08f6117456d0555ea78a0cbc6ade
joneff <joneff@gmail.com>
2020-11-02 00:02:59 +0200

    infra(visual-tests): move visualt tests install modules in tests folder

--- build/create-screenshots.sh
+++ build/create-screenshots.sh
@@ -3,23 +3,21 @@
 set -e
 
 dir=$(pwd);
-
 theme=${1-default}
+test_dir="$dir/tests/.tmp/visual/$theme"
 
-# sed on OSX and Linux differs, see https://stackoverflow.com/a/38595160/25427
-sedi () {
-    sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
-}
+mkdir -p $test_dir
 
-mkdir -p $dir/.tmp/tests/visual/$theme
-
-cp $dir/tests/visual/*.html $dir/.tmp/tests/visual/$theme
-cp $dir/tests/visual/assets -r $dir/.tmp/tests/visual/$theme
+cp $dir/tests/visual/*.html $test_dir
+cp $dir/tests/visual/assets -r $test_dir
+cp $dir/packages/$theme/dist -r $test_dir
 
 # replace theme reference
-find $dir/.tmp/tests/visual/$theme -name '*.html' | while read file; do sedi "s#../../packages/default/dist#../../../../packages/$theme/dist#" "$file"; done
+find $test_dir -name '*.html'  | xargs sed -i -e 's#../../packages/default/dist#dist#'
+
+cd tests
 
 # capture screenshots. see .pastshotsrc for config options
-npx pastshots --serve ".tmp/tests/visual/$theme/*.html" --port $((RANDOM % 1000 + 8000)) --output tests/visual/output/$theme
+npx pastshots --serve ".tmp/visual/$theme/*.html" --port $((RANDOM % 1000 + 8000)) --output visual/output/$theme
 
-rm -rf .tmp/tests/visual/$theme
+rm -rf $dir/tests/.tmp/visual/$theme
