e4a5e3794e6535b5ebb75056b275e029478ff851
joneff <joneff@gmail.com>
2020-11-02 14:37:52 +0200

    infra(gh-actions): extract functionality to mark checks as successful

---
+++ build/mark-checks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+COMMIT_REF=${1-null}
+
+if [[ $COMMIT_REF == 'null' ]]
+then
+  exit 0
+fi
+
+declare -a checks=( 'CI | Unit' 'CI | Visual' )
+
+for check in "${checks[@]}"
+do
+    echo "Marking '$check' as successful..."
+    curl -s \
+        -X POST \
+        -H "Authorization: token $STATUS_TOKEN" \
+        -d "{ \"state\": \"success\", \"context\": \"$check\" }" \
+        "https://api.github.com/repos/telerik/kendo-themes/statuses/$COMMIT_REF"
+done
