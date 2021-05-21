9f9025af8f793f49e55e9ba4811db47d03ff12fd
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2021-05-21 16:28:20 +0300

    chore: update screenshot script

--- build/create-screenshots.sh
+++ build/create-screenshots.sh
@@ -5,6 +5,7 @@ shopt -s extglob
 THEME=$1
 TEMP_DIR="./tests/.tmp/visual"
 THEME_DIR="$TEMP_DIR/$THEME"
+SRC_DIR="$THEME_DIR/src"
 
 if [ -z "$THEME" ]; then
     echo "Usage: create-screenshots <default|bootstrap|material>"
@@ -17,13 +18,13 @@ mkdir -p "$THEME_DIR"
 cp -r ./tests/visual/!(output) "$THEME_DIR"
 
 # replace theme reference
-find "$THEME_DIR" -name '*.html' -print0 | xargs -0 sed -i -E \
+find "$SRC_DIR" -name '*.html' -print0 | xargs -0 sed -i -E \
     -e "s#/packages/default/dist/#/../../packages/$THEME/dist/#"
 
 # capture screenshots. see .pastshotsrc for config options
 npm install --no-save pastshots optipng
 
-find $TEMP_DIR \
+find $SRC_DIR \
     -type d \
     -not -wholename "*output*" \
     -not -name "dist" \
@@ -34,4 +35,4 @@ find $TEMP_DIR \
             --serve "$0/*.html" \
             --port $((RANDOM % 1000 + 8000)) \
             --output ./tests/visual/output/$(realpath --relative-to "$1" "$0")' \
-        {} "$TEMP_DIR" \;
+        {} "$SRC_DIR" \;
