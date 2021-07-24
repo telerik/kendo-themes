8a17e576c75fe9f63fd2af46d141ad273f24b96f
joneff <joneff@gmail.com>
2021-07-24 13:47:21 +0300

    chore: rename swatches to include theme name

--- packages/bootstrap/lib/swatches/index.js
+++ packages/bootstrap/lib/swatches/index.js
@@ -1,8 +1,8 @@
-module.exports.main = require('./main.json');
-module.exports.mainDark = require('./main-dark.json');
-module.exports.flat = require('./flat.json');
-module.exports.indigo = require('./indigo.json');
-module.exports.nordic = require('./nordic.json');
-module.exports.turquoise = require('./turquoise.json');
-module.exports.urban = require('./urban.json');
-module.exports.vintage = require('./vintage.json');
+module.exports.main = require('./bootstrap-main.json');
+module.exports.mainDark = require('./bootstrap-main-dark.json');
+module.exports.flat = require('./bootstrap-flat.json');
+module.exports.indigo = require('./bootstrap-indigo.json');
+module.exports.nordic = require('./bootstrap-nordic.json');
+module.exports.turquoise = require('./bootstrap-turquoise.json');
+module.exports.urban = require('./bootstrap-urban.json');
+module.exports.vintage = require('./bootstrap-vintage.json');
