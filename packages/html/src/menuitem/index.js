40cd5cee83800815f67a7996402ab4a29f0c5eec
silviyaboteva <Silviya.Boteva@progress.com>
2021-11-08 15:21:43 +0200

    feat(html): add html helper for menu item

---
+++ packages/html/src/menuitem/index.js
@@ -0,0 +1,2 @@
+export * from './menu-item.jsx';
+export * from './menu-item-content.jsx';
---
+++ packages/html/src/menuitem/menu-item-content.jsx
@@ -0,0 +1,48 @@
+import { Component, globalDefaultProps } from '../component/index';
+
+class MenuItemContent extends Component {
+    render() {
+        return <MenuItemContentStatic {...this.props} />;
+    }
+}
+
+function MenuItemContentStatic(props) {
+
+    const {
+        className: ownClassName,
+
+        children,
+
+        ...htmlAttributes
+    } = props;
+
+    let menuItemContentClasses = [
+        ownClassName,
+        'k-menu-item-content'
+    ];
+
+    if (children.length === 0) {
+        return <></>;
+    }
+
+    return (
+        <span className={menuItemContentClasses} {...htmlAttributes}>
+            {children}
+        </span>
+    );
+}
+
+MenuItemContentStatic.defaultProps = {
+    ...globalDefaultProps,
+
+    children: []
+};
+
+MenuItemContentStatic.propTypes = {
+    children: typeof [],
+    className: typeof '',
+
+    htmlAttributes: typeof []
+};
+
+export { MenuItemContent, MenuItemContentStatic };
