0dd6a4e34f93ed0e42c92d27a5622721dc67dbbf
Juveniel <dimitar.kalaydzhievv@gmail.com>
2021-11-29 17:15:28 +0200

    feat(html): add html helper for multiselect

---
+++ packages/html/src/multiselect/multiselect-chip-list.jsx
@@ -0,0 +1,55 @@
+import { Component, globalDefaultProps } from '../component/index';
+import { ChipListStatic } from '../chip/index';
+
+class MultiSelectChipList extends Component {
+    render() {
+        return <MultiSelectChipListStatic {...this.props} />;
+    }
+}
+
+function MultiSelectChipListStatic(props) {
+
+    const {
+        className: ownClassName,
+
+        children,
+
+        size,
+
+        ...htmlAttributes
+    } = props;
+
+    let inputValuesClasses = [
+        ownClassName,
+        'k-input-values'
+    ];
+
+    if (children.length === 0) {
+        return <></>;
+    }
+
+    return (
+        <ChipListStatic selection="multiple" size={size} className={inputValuesClasses} {...htmlAttributes}>
+            {children}
+        </ChipListStatic>
+    );
+}
+
+MultiSelectChipListStatic.defaultProps = {
+    ...globalDefaultProps,
+
+    size: 'medium',
+
+    children: []
+};
+
+MultiSelectChipListStatic.propTypes = {
+    children: typeof [],
+    className: typeof '',
+
+    size: typeof [ null, 'small', 'medium', 'large' ],
+
+    htmlAttributes: typeof []
+};
+
+export { MultiSelectChipList, MultiSelectChipListStatic };
