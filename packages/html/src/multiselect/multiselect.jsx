63f84fd42939b1f4157a9db4f860025a7fdbbd6e
Juveniel <dimitar.kalaydzhievv@gmail.com>
2021-11-29 17:15:28 +0200

    feat(html): add html helper for multiselect

---
+++ packages/html/src/multiselect/multiselect.jsx
@@ -0,0 +1,189 @@
+import { globalDefaultProps } from '../component/index';
+import { Input, InputStatic, InputInnerInputStatic } from '../input/index';
+import { InputValidationIconStatic, InputLoadingIconStatic, InputClearValueStatic } from '../input/index';
+import { ButtonStatic } from '../button/index';
+import { MultiSelectChipListStatic } from './index';
+
+class MultiSelect extends Input {
+
+    init() {
+        let value = <></>;
+        let children = this._props.children;
+        let newChildren = [];
+
+        children.forEach( child => {
+            let component = child._component;
+
+            if (component === 'MultiSelectChipList') {
+                value.props.children.push.apply(value.props.children, child.props.children);
+                return;
+            }
+
+            newChildren.push( child );
+        });
+
+        this._props.value = value;
+        this._props.children = newChildren;
+    }
+
+    render() {
+        return <MultiSelectStatic {...this.props} />;
+    }
+}
+
+function MultiSelectStatic(props) {
+
+    const {
+        className: ownClassName,
+
+        type,
+        placeholder,
+        autocomplete,
+
+        prefix,
+        suffix,
+        value,
+
+        size,
+        rounded,
+
+        fillMode,
+
+        showArrowButton,
+
+        hover,
+        focus,
+        valid,
+        invalid,
+        required,
+        disabled,
+
+        aria,
+        legacy,
+
+        ...htmlAttributes
+    } = props;
+
+    htmlAttributes.size = size;
+    htmlAttributes.rounded = rounded;
+    htmlAttributes.fillMode = fillMode;
+    htmlAttributes.hover = hover;
+    htmlAttributes.focus = focus;
+    htmlAttributes.valid = valid;
+    htmlAttributes.invalid = invalid;
+    htmlAttributes.required = required;
+    htmlAttributes.disabled = disabled;
+
+    const inputAttributes = {
+        type,
+        placeholder,
+        autocomplete,
+
+        disabled
+    };
+
+    let multiselectClasses = [
+        ownClassName,
+        'k-multiselect'
+    ];
+
+    let ariaAttr = aria
+        ? {}
+        : {};
+
+    if (legacy) {
+
+        let legacyClasses = [
+            ownClassName,
+            'k-widget',
+            'k-multiselect',
+            {
+                'k-state-hover': hover === true,
+                'k-state-focused': focus === true,
+                'k-state-invalid': invalid === true,
+                'k-state-required': required === true,
+                'k-state-disabled': disabled === true
+            }
+        ];
+
+        return (
+            <InputStatic className={legacyClasses} {...htmlAttributes}>
+                <input type={type} className="k-input" {...inputAttributes} />
+                <InputValidationIconStatic {...props} />
+                <InputLoadingIconStatic {...props} />
+                <InputClearValueStatic {...props} />
+            </InputStatic>
+        );
+    }
+
+    return (
+        <InputStatic className={multiselectClasses} {...ariaAttr} {...htmlAttributes}>
+            {prefix}
+            <MultiSelectChipListStatic {...props}>
+                {value}
+                <InputInnerInputStatic {...inputAttributes} />
+            </MultiSelectChipListStatic>
+            {suffix}
+            <InputValidationIconStatic {...props} />
+            <InputLoadingIconStatic {...props} />
+            <InputClearValueStatic {...props} />
+            {showArrowButton && <ButtonStatic className="k-input-button" icon="arrow-s" shape={null} rounded={null} size={size} fillMode={fillMode}></ButtonStatic>}
+        </InputStatic>
+    );
+}
+
+MultiSelectStatic.defaultProps = {
+    ...globalDefaultProps,
+
+    type: 'text',
+    placeholder: '',
+    autocomplete: 'off',
+
+    showValidationIcon: true,
+    showLoadingIcon: true,
+    showClearButton: true,
+    showArrowButton: false,
+
+    size: 'medium',
+    rounded: 'medium',
+
+    fillMode: 'solid'
+};
+
+MultiSelectStatic.propTypes = {
+    children: typeof [],
+    className: typeof '',
+
+    type: typeof [ 'text', 'password' ],
+    placeholder: typeof '',
+    autocomplete: typeof [ 'on', 'off' ],
+
+    showValidationIcon: typeof true,
+    showLoadingIcon: typeof true,
+    showClearButton: typeof true,
+    showArrowButton: typeof true,
+
+    prefix: typeof '#fragment',
+    suffix: typeof '#fragment',
+    value: typeof '#fragment',
+
+    size: typeof [ 'none', 'small', 'medium', 'large' ],
+    rounded: typeof [ 'none', 'small', 'medium', 'large', 'pill' ],
+
+    fillMode: typeof [ 'none', 'solid', 'flat', 'outline' ],
+
+    hover: typeof false,
+    focus: typeof false,
+    valid: typeof false,
+    invalid: typeof false,
+    loading: typeof false,
+    required: typeof false,
+    disabled: typeof false,
+
+    aria: typeof false,
+    legacy: typeof false,
+
+    htmlAttributes: typeof []
+};
+
+export { MultiSelect, MultiSelectStatic };
