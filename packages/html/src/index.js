fd7da251f12b457eda49e5a5a7e278cc2b47da7b
joneff <joneff@gmail.com>
2021-12-09 10:18:37 +0200

    chore(html): stub further components

--- packages/html/src/index.js
+++ packages/html/src/index.js
@@ -27,7 +27,14 @@ export * from './input/index';
 export * from './popup/index';
 export * from './list/index';
 export * from './nodata/index';
+
+// Indicators
 export * from './avatar/index';
+// export * from './badge/index';
+// export * from './chip/index';
+// export * from './color-preview/index';
+// export * from './loader/index';
+// export * from './skeleton/index';
 
 // Native forms
 export * from './button/index';
@@ -59,3 +66,34 @@ export * from './searchbox/index';
 export * from './switch/index';
 // export * from './upload/index';
 // export * from './dropzone/index';
+
+// Command interfaces
+// export * from './action-buttons/index';
+// export * from './fab/index';
+// export * from './menu/index';
+// export * from './toolbar/index';
+
+// Popups and modals
+// export * from './action-sheet/index';
+// export * from './dialog/index';
+// export * from './drawer/index';
+// export * from './notification/index';
+// export * from './popover/index';
+// export * from './tooltip/index';
+// export * from './window/index';
+
+// Navigation
+// export * from './appbar/index';
+// export * from './bottom-nav/index';
+// export * from './breadcrumb/index';
+// export * from './pager/index';
+// export * from './stepper/index';
+// export * from './tabstrip/index';
+// export * from './wizard/index';
+
+// Layout & containers
+// export * from './card/index';
+// export * from './expander/index';
+// export * from './panelbar/index';
+// export * from './splitter/index';
+// export * from './tile-layout/index';
