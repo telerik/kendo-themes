2091d81bef534690809c15bb362e35daf1d73650
joneff <joneff@gmail.com>
2021-12-15 02:10:17 +0200

    fix(html): update datetime picker html helper to reflect v4 universal rendering

--- packages/html/src/datetimepicker/datetimepicker.jsx
+++ packages/html/src/datetimepicker/datetimepicker.jsx
@@ -76,17 +76,15 @@ function DateTimePickerStatic(props) {
             'k-widget',
             'k-datetimepicker',
             {
+                'k-state-hover': hover === true,
+                'k-state-focus': focus === true,
+                'k-state-invalid': invalid === true,
                 'k-state-disabled': disabled === true
             }
         ];
 
         let legacyWrapClasses = [
-            'k-picker-wrap',
-            {
-                'k-state-hover': hover === true,
-                'k-state-focused': focus === true,
-                'k-state-invalid': invalid === true
-            }
+            'k-picker-wrap'
         ];
 
         return (
