c50a6958415ad238d66c54d4351280c068b9a497
TeyaVes <Teya.Veselinova@progress.com>
2022-03-29 15:09:38 +0300

    feat(html): add captcha html helper

--- packages/html/src/index.js
+++ packages/html/src/index.js
@@ -50,8 +50,8 @@ export * from './radio/index';
 
 // Augmented inputs
 export * from './autocomplete/index';
-// export * from './captcha/index';
 export * from './colorpalette/index';
+export * from './captcha/index';
 export * from './colorpicker/index';
 export * from './combobox/index';
 export * from './dateinput/index';
