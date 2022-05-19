ddd5bbd265382e38c44de447666ba72d84faa90e
Juveniel <dimitar.kalaydzhievv@gmail.com>
2022-05-19 16:58:00 +0300

    chore(infra): snapshot reference tests form html package

--- build/create-snapshots.mjs
+++ build/create-snapshots.mjs
@@ -3,38 +3,58 @@ import { Browser, snapshotMarkup } from '@progress/kendo-e2e';
 import { createServer } from 'http-server';
 import { promisify } from 'util';
 import { default as cbGlob } from 'glob';
+import { mkdir } from 'fs/promises';
+import path from 'path';
 const glob = promisify(cbGlob);
 
 const PORT = 18111;
 const HOST = 'localhost';
-const TESTS_PATH = './tests/visual/src/reference';
-const COMPONENT_PAGE_EXT = '.comp.html';
+const TESTS_PATH = './packages/html/src';
+const OUTPUT_PATH = './tests';
+const COMPONENT_PAGE_EXT = '.html';
 
 const browser = new Browser();
 const server = createServer({
-  root: './',
-  port: PORT
+    root: './',
+    port: PORT
 });
 
 const pathUrl = path => `http://${HOST}:${PORT}/${path.replace('./', '')}`;
 
 server.listen(PORT, HOST, async() => {
-  const files = await glob(`${TESTS_PATH}/**/*${COMPONENT_PAGE_EXT}`);
-  const pages = files.map(path => [path, pathUrl(path)]);
-  for (let i = 0; i < pages.length; i++) {
-    const [path, url] = pages[i];
+    const files = await glob(`${TESTS_PATH}/**/*${COMPONENT_PAGE_EXT}`);
+    const pages = files.map(path => [ path, pathUrl(path) ]);
 
-    await browser.navigateTo(url);
+    for (let i = 0; i < pages.length; i++) {
+        const [ filePath, url ] = pages[i];
 
-    const outputPath = path.replace(COMPONENT_PAGE_EXT, '.html');
-    await snapshotMarkup(browser.driver, 'html', outputPath, {
-      template: (output) => `<!doctype html>${output}`,
-      preserveAttributes: true,
-      preserveCommentNodes: true
-    });
-  }
+        await browser.navigateTo(url);
 
-  await browser.close();
-  server.close();
+        const fileName = path.basename(filePath);
+        const folderName = path.basename(path.dirname(path.dirname(filePath)));
+        const outputPath = `${OUTPUT_PATH}/${folderName}/${fileName}`;
+
+        await mkdir(path.dirname(outputPath), { recursive: true });
+
+        await snapshotMarkup(browser.driver, 'html', outputPath, {
+            template: (output) => `<!doctype html>${output}`,
+            preserveAttributes: true,
+            preserveCommentNodes: true,
+            beautifyOptions: {
+                /* eslint-disable camelcase */
+                newline_between_rules: false,
+                brace_style: 'collapse',
+                indent_size: 4,
+                inline: [],
+                extra_liners: [],
+                preserve_newlines: false,
+                end_with_newline: true
+                /* eslint-enable */
+            }
+        });
+    }
+
+    await browser.close();
+    server.close();
 });
 
