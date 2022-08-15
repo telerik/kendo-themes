a93194f87a7cd7f8327b29f7ea407954987b6b7f
Emil Petrov <emil.petrov@progress.com>
2022-08-15 16:35:15 +0300

    test(tabstrip): remove k-state classes

--- packages/html/src/tabstrip/tests/tabstrip-sortable.tsx
+++ packages/html/src/tabstrip/tests/tabstrip-sortable.tsx
@@ -14,19 +14,19 @@ root.render(
                 <div className="k-widget k-tabstrip k-header k-tabstrip-top">
                     <div className="k-tabstrip-items-wrapper k-hstack">
                         <ul className="k-reset k-tabstrip-items">
-                            <li className="k-item k-first k-state-active k-tabstrip-dragging">
+                            <li className="k-item k-first k-active k-tabstrip-dragging">
                                 <span className="k-link">First</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Closable</span>
                                 <Button fillMode="flat" icon="close" className="k-remove-tab"></Button>
                             </li>
-                            <li className="k-item k-state-focused">
+                            <li className="k-item k-focus">
                                 <span className="k-link">Focused</span>
                             </li>
                         </ul>
                     </div>
-                    <div className="k-tabstrip-content k-state-active k-state-focused">
+                    <div className="k-tabstrip-content k-active k-focus">
                         <div>Top sortable</div>
                     </div>
                 </div>
@@ -34,19 +34,19 @@ root.render(
 
             <section>
                 <div className="k-widget k-tabstrip k-header k-tabstrip-bottom">
-                    <div className="k-tabstrip-content k-state-active">
+                    <div className="k-tabstrip-content k-active">
                         <div>Bottom sortable</div>
                     </div>
                     <div className="k-tabstrip-items-wrapper k-hstack">
                         <ul className="k-reset k-tabstrip-items">
-                            <li className="k-item k-first k-state-active k-tabstrip-dragging">
+                            <li className="k-item k-first k-active k-tabstrip-dragging">
                                 <span className="k-link">First</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Closable</span>
                                 <Button fillMode="flat" icon="close" className="k-remove-tab"></Button>
                             </li>
-                            <li className="k-item k-state-focused">
+                            <li className="k-item k-focus">
                                 <span className="k-link">Focused</span>
                             </li>
                         </ul>
@@ -58,28 +58,28 @@ root.render(
                 <div className="k-widget k-tabstrip k-header k-tabstrip-left">
                     <div className="k-tabstrip-items-wrapper k-vstack">
                         <ul className="k-reset k-tabstrip-items">
-                            <li className="k-item k-first k-state-active k-tabstrip-dragging">
+                            <li className="k-item k-first k-active k-tabstrip-dragging">
                                 <span className="k-link">Active</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Normal</span>
                             </li>
-                            <li className="k-item k-state-hover">
+                            <li className="k-item k-hover">
                                 <span className="k-link">Hover</span>
                             </li>
-                            <li className="k-item k-state-focused">
+                            <li className="k-item k-focus">
                                 <span className="k-link">Focused</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Closable</span>
                                 <Button fillMode="flat" icon="close" className="k-remove-tab"></Button>
                             </li>
-                            <li className="k-item k-last k-state-disabled">
+                            <li className="k-item k-last k-disabled">
                                 <span className="k-link">Disabled</span>
                             </li>
                         </ul>
                     </div>
-                    <div className="k-tabstrip-content k-state-active">
+                    <div className="k-tabstrip-content k-active">
                         <div>Left orientation</div>
                     </div>
                 </div>
@@ -89,28 +89,28 @@ root.render(
                 <div className="k-widget k-tabstrip k-header k-tabstrip-right">
                     <div className="k-tabstrip-items-wrapper k-vstack">
                         <ul className="k-reset k-tabstrip-items">
-                            <li className="k-item k-first k-state-active k-tabstrip-dragging">
+                            <li className="k-item k-first k-active k-tabstrip-dragging">
                                 <span className="k-link">Active</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Normal</span>
                             </li>
-                            <li className="k-item k-state-hover">
+                            <li className="k-item k-hover">
                                 <span className="k-link">Hover</span>
                             </li>
-                            <li className="k-item k-state-focused">
+                            <li className="k-item k-focus">
                                 <span className="k-link">Focused</span>
                             </li>
                             <li className="k-item">
                                 <span className="k-link">Closable</span>
                                 <Button fillMode="flat" icon="close" className="k-remove-tab"></Button>
                             </li>
-                            <li className="k-item k-last k-state-disabled">
+                            <li className="k-item k-last k-disabled">
                                 <span className="k-link">Disabled</span>
                             </li>
                         </ul>
                     </div>
-                    <div className="k-tabstrip-content k-state-active">
+                    <div className="k-tabstrip-content k-active">
                         <div>Right orientation</div>
                     </div>
                 </div>
