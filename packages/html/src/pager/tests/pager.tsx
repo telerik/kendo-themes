4f10e21ebd6b6ae3fe39cc8fb0a494c182f4c962
TeyaVes <Teya.Veselinova@progress.com>
2022-08-25 12:57:59 +0300

    fix(pager): add support for pager icons

--- packages/html/src/pager/tests/pager.tsx
+++ packages/html/src/pager/tests/pager.tsx
@@ -15,8 +15,8 @@ root.render(
             <span>LTR</span>
             <section>
                 <div className="k-pager k-widget">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-focus"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-focus"><Icon name="caret-alt-left" /></a>
                     <div className="k-pager-numbers-wrap">
                         <select className="k-picker k-dropdown-list k-dropdown">
                             <option>1</option>
@@ -30,45 +30,45 @@ root.render(
                             <li><a className="k-link k-selected k-focus">6</a></li>
                         </ul>
                     </div>
-                    <a className="k-link k-pager-nav k-hover"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav k-hover"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
             </section>
             <section>
                 <div className="k-pager k-widget k-focus">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-disabled"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-disabled"><Icon name="caret-alt-left" /></a>
                     <span className="k-pager-input k-label">
                         Page
                         <Textbox showClearButton={false} value="999" />
                         of 8
                     </span>
-                    <a className="k-link k-pager-nav"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
             </section>
             <section>
                 <div className="k-pager k-widget">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-disabled"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-disabled"><Icon name="caret-alt-left" /></a>
                     <div className="k-pager-numbers-wrap">
                         <select className="k-picker k-dropdown-list k-dropdown">
                             <option>1</option>
@@ -81,15 +81,15 @@ root.render(
                             <li><a className="k-link">5</a></li>
                         </ul>
                     </div>
-                    <a className="k-link k-pager-nav"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
@@ -99,8 +99,8 @@ root.render(
             <span>RTL</span>
             <section>
                 <div className="k-pager k-widget" dir="rtl">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-disabled"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-disabled"><Icon name="caret-alt-left" /></a>
                     <div className="k-pager-numbers-wrap">
                         <select className="k-picker k-dropdown-list k-dropdown">
                             <option>1</option>
@@ -113,45 +113,45 @@ root.render(
                             <li><a className="k-link">5</a></li>
                         </ul>
                     </div>
-                    <a className="k-link k-pager-nav"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
             </section>
             <section>
                 <div className="k-pager k-widget" dir="rtl">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-disabled"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-disabled"><Icon name="caret-alt-left" /></a>
                     <span className="k-pager-input k-label">
                         Page
                         <Textbox showClearButton={false} value="999" />
                         of 8
                     </span>
-                    <a className="k-link k-pager-nav"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
             </section>
             <section>
                 <div className="k-pager k-widget" dir="rtl">
-                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="arrow-end-left" /></a>
-                    <a className="k-link k-pager-nav k-disabled"><Icon name="arrow-w" /></a>
+                    <a className="k-link k-pager-nav k-pager-first k-disabled"><Icon name="caret-alt-to-left" /></a>
+                    <a className="k-link k-pager-nav k-disabled"><Icon name="caret-alt-left" /></a>
                     <div className="k-pager-numbers-wrap">
                         <select className="k-picker k-dropdown-list k-dropdown">
                             <option>1</option>
@@ -164,15 +164,15 @@ root.render(
                             <li><a className="k-link">5</a></li>
                         </ul>
                     </div>
-                    <a className="k-link k-pager-nav"><Icon name="arrow-e" /></a>
-                    <a className="k-link k-pager-nav k-pager-last"><Icon name="arrow-end-right" /></a>
+                    <a className="k-link k-pager-nav"><Icon name="caret-alt-right" /></a>
+                    <a className="k-link k-pager-nav k-pager-last"><Icon name="caret-alt-to-right" /></a>
 
                     <span className="k-pager-sizes k-label">
                         <DropdownList value="20" />
                         items per page
                     </span>
 
-                    <a className="k-pager-refresh k-link"><Icon name="reload" /></a>
+                    <a className="k-pager-refresh k-link"><Icon name="arrow-rotate-cw" /></a>
 
                     <span className="k-pager-info k-label">1 - 20 of 91 items</span>
                 </div>
