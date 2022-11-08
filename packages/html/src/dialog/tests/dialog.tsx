ef4479c839634599d376ca47ffbe99342bc6d302
Teya Veselinova <tveselin@progress.com>
2022-11-08 14:43:27 +0200

    test(dialog): use dialog helper in tests

--- packages/html/src/dialog/tests/dialog.tsx
+++ packages/html/src/dialog/tests/dialog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ActionButtons } from '../../action-buttons';
-import { Button } from '../../button';
+import { Dialog } from '../../dialog';
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
@@ -18,105 +18,35 @@ root.render(
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-3">
 
-            <div className="k-widget k-window k-dialog">
-                <div className="k-window-titlebar k-dialog-titlebar">
-                    <span className="k-window-title k-dialog-title">
-                        Title
+            <Dialog title="Title
                         g y p
-                        t d l
-                    </span>
-                    <div className="k-window-actions k-dialog-actions">
-                        <Button fillMode="flat" icon="close"></Button>
-                    </div>
-                </div>
+                        t d l" actions={[ 'close' ]} actionButtons actionButtonsAlign="end">
+                Dialog content template
+            </Dialog>
 
-                <div className="k-window-content k-dialog-content">
-                    Dialog content template
-                </div>
-
-                <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                    <Button>Action</Button>
-                    <Button themeColor="primary">Primary</Button>
-                </ActionButtons>
-            </div>
-
-            <div className="k-widget k-window k-dialog">
-                <div className="k-window-titlebar k-dialog-titlebar">
-                    <span className="k-window-title k-dialog-title" style={{ fontFamily: "cursive" }}>
-                        Cursive
+            <Dialog title="Cursive
                         g y p
-                        t d l
-                    </span>
-                </div>
+                        t d l" actionButtons actionButtonsAlign="end">
+                Dialog without titlebar actions
+            </Dialog>
 
-                <div className="k-window-content k-dialog-content">
-                    Dialog without titlebar actions
-                </div>
+            <Dialog title="" actions={[ 'close' ]} actionButtons actionButtonsAlign="end">
+                Dialog with empty title and actions
+            </Dialog>
 
-                <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                    <Button>Action</Button>
-                    <Button themeColor="primary">Primary</Button>
-                </ActionButtons>
-            </div>
+            <Dialog title="">
+                Dialog with empty title and no actions
+            </Dialog>
 
-            <div className="k-widget k-window k-dialog">
-                <div className="k-window-titlebar k-dialog-titlebar">
-                    <span className="k-window-title k-dialog-title"></span>
-                    <div className="k-window-actions k-dialog-actions">
-                        <Button fillMode="flat" icon="close"></Button>
-                    </div>
-                </div>
+            <Dialog actionButtons actionButtonsAlign="end">
+                Dialog without title
+            </Dialog>
 
-                <div className="k-window-content k-dialog-content">
-                    Dialog with empty title and actions
-                </div>
-
-                <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                    <Button>Action</Button>
-                    <Button themeColor="primary">Primary</Button>
-                </ActionButtons>
-            </div>
-
-            <div className="k-widget k-window k-dialog">
-                <div className="k-window-titlebar k-dialog-titlebar">
-                    <span className="k-window-title k-dialog-title"></span>
-                </div>
-
-                <div className="k-window-content k-dialog-content">
-                    Dialog with empty title and no actions
-                </div>
-            </div>
-
-            <div className="k-widget k-window k-dialog">
-                <div className="k-window-content k-dialog-content">
-                    Dialog without title
-                </div>
-
-                <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                    <Button>Action</Button>
-                    <Button themeColor="primary">Primary</Button>
-                </ActionButtons>
-            </div>
-
-            <div className="k-widget k-window k-dialog" style={{ height: "170px" }}>
-                <div className="k-window-titlebar k-dialog-titlebar">
-                    <span className="k-window-title k-dialog-title">Scrolling</span>
-                    <div className="k-window-actions k-dialog-actions">
-                        <Button fillMode="flat" icon="close"></Button>
-                    </div>
-                </div>
-
-                <div className="k-window-content k-dialog-content k-scroll">
-                    <p>Dialog content template with long text and vertical scrolling.</p>
-                    <p>Dialog content template with long text and vertical scrolling.</p>
-                    <p>Dialog content template with long text and vertical scrolling.</p>
-                </div>
-
-                <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                    <Button>Action</Button>
-                    <Button themeColor="primary">Primary</Button>
-                </ActionButtons>
-            </div>
+            <Dialog title="Scrolling" actions={[ 'close' ]} actionButtons actionButtonsAlign="end" style={{ height: "170px" }}>
+                <p>Dialog content template with long text and vertical scrolling.</p>
+                <p>Dialog content template with long text and vertical scrolling.</p>
+                <p>Dialog content template with long text and vertical scrolling.</p>
+            </Dialog>
 
         </div>
     </>
--- packages/html/src/filemanager/tests/filemanager-dialogs.tsx
+++ packages/html/src/filemanager/tests/filemanager-dialogs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ActionButtons } from '../../action-buttons';
+import { Dialog } from '../../dialog';
 import { Button } from '../../button';
 
 const root = ReactDOM.createRoot(
@@ -21,66 +21,36 @@ root.render(
 
             {/* Upload Dialog */}
             <section>
-                <div className="k-window k-dialog k-filemanager-upload-dialog">
-                    <div className="k-window-titlebar k-dialog-titlebar">
-                        <span className="k-window-title k-dialog-title">Upload Files</span>
-                        <div className="k-window-actions k-dialog-actions">
-                            <Button fillMode="flat" icon="close"></Button>
+
+                <Dialog title="Upload Files" actions={[ 'close' ]} actionButtons actionButtonsAlign="end" className="k-filemanager-upload-dialog">
+                    <div className="k-upload k-upload-async k-upload-empty">
+                        <div className="k-dropzone">
+                            <Button className="k-upload-button">Select files...</Button>
+                            <em className="k-dropzone-hint">Drop files here to upload</em>
                         </div>
                     </div>
-                    <div className="k-window-content k-dialog-content">
-                        <div className="k-upload k-upload-async k-upload-empty">
-                            <div className="k-dropzone">
-                                <Button className="k-upload-button">Select files...</Button>
-                                <em className="k-dropzone-hint">Drop files here to upload</em>
-                            </div>
-                        </div>
-                    </div>
-                    <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                        <Button>Clear List</Button>
-                        <Button themeColor="primary">Done</Button>
-                    </ActionButtons>
-                </div>
+                </Dialog>
+
             </section>
             <span>{' '}</span>
 
             {/* Move/Copy Dialog */}
             <section>
-                <div className="k-window k-dialog">
-                    <div className="k-window-titlebar k-dialog-titlebar">
-                        <span className="k-window-title k-dialog-title">Confirm</span>
-                        <div className="k-window-actions k-dialog-actions">
-                            <Button fillMode="flat" icon="close"></Button>
-                        </div>
-                    </div>
-                    <div className="k-window-content k-dialog-content k-text-center">
-                        <p>Do you want to move or copy?</p>
-                    </div>
-                    <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                        <Button>Move</Button>
-                        <Button themeColor="primary">Copy</Button>
-                    </ActionButtons>
-                </div>
+
+                <Dialog title="Confirm" actions={[ 'close' ]} actionButtons actionButtonsAlign="end">
+                    <p>Do you want to move or copy?</p>
+                </Dialog>
+
             </section>
 
             {/* Delete Dialog */}
             <section>
-                <div className="k-window k-dialog">
-                    <div className="k-window-titlebar k-dialog-titlebar">
-                        <span className="k-window-title k-dialog-title">Confirm</span>
-                        <div className="k-window-actions k-dialog-actions">
-                            <Button fillMode="flat" icon="close"></Button>
-                        </div>
-                    </div>
-                    <div className="k-window-content k-dialog-content k-text-center">
-                        <p>Are you sure you want to delete this file?</p>
-                        <p>You can&apos;t undo this action.</p>
-                    </div>
-                    <ActionButtons className="k-dialog-buttongroup" alignment="end">
-                        <Button>Cancel</Button>
-                        <Button themeColor="primary">Delete</Button>
-                    </ActionButtons>
-                </div>
+
+                <Dialog title="Confirm" actions={[ 'close' ]} actionButtons actionButtonsAlign="end">
+                    <p>Are you sure you want to delete this file?</p>
+                    <p>You can&apos;t undo this action.</p>
+                </Dialog>
+
             </section>
 
         </div>
