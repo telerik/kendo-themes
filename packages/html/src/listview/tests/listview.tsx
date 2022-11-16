61fbd4929209c326f2cf7cfaf2389d6c474601d4
Magdalena Angelova <magdalena.angelova@progress.com>
2022-11-16 13:48:04 +0200

    test: use listview component in tests

--- packages/html/src/listview/tests/listview.tsx
+++ packages/html/src/listview/tests/listview.tsx
@@ -1,69 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ListView } from '../listview';
+import { ListViewItem } from '../listview-item';
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
 );
 
 const styles = `
+    .k-listview-header,
+    .k-listview-footer,
     .k-listview-item {
         padding: 10px;
-        display: inline-block;
-        outline: 1px dashed;
     }
 `;
 
 root.render(
     <>
         <style>{styles}</style>
-        <div id="test-area" className="k-d-grid k-grid-cols-2">
+        <div id="test-area" className="k-d-grid k-grid-cols-3">
 
             <section>
-                <span>No layout</span>
-                <div className="k-widget k-listview">
-                    <div className="k-listview-content">
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                    </div>
-                </div>
+                <span>Listview - bottom pager</span>
+                <ListView
+                    header
+                    footer
+                    pageable
+                    layout="flex"
+                    flexDirection="col">
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem focus><div>Focus Item</div></ListViewItem>
+                    <ListViewItem selected><div>Selected Item</div></ListViewItem>
+                    <ListViewItem focus selected><div>Focus Selected Item</div></ListViewItem>
+                </ListView>
             </section>
 
             <section>
-                <span>Flex layout</span>
-                <div className="k-widget k-listview">
-                    <div className="k-listview-content k-d-flex k-flex-col k-flex-nowrap">
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                    </div>
-                </div>
+                <span>Listview - top pager</span>
+                <ListView
+                    header
+                    footer
+                    pageable
+                    pagerPosition="top"
+                    layout="flex"
+                    flexDirection="col">
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                </ListView>
             </section>
 
             <section>
-                <span>Grid layout</span>
-                <div className="k-widget k-listview">
-                    <div className="k-listview-content k-d-grid k-grid-cols-2">
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                        <div className="k-listview-item">Listview item</div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <span>Interactive states</span>
-                <div className="k-widget k-listview k-listview-grid">
-                    <div className="k-listview-content k-d-grid k-grid-cols-2">
-                        <div className="k-listview-item">Normal item</div>
-                        <div className="k-listview-item k-focus">Focus item</div>
-                        <div className="k-listview-item k-selected">Selected item</div>
-                        <div className="k-listview-item k-selected k-focus">Selected focus item</div>
-                    </div>
-                </div>
+                <span>Listview - top pager</span>
+                <ListView
+                    header
+                    footer
+                    pageable
+                    pagerPosition="top"
+                    layout="flex"
+                    flexDirection="col">
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                    <ListViewItem><div>Listview Item</div></ListViewItem>
+                </ListView>
             </section>
 
         </div>
