bf44c239af09cdaf429dc46e62037f5a73801b58
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2022-12-19 16:27:52 +0200

    test(editor): use new toolbar rendering in tests

--- packages/html/src/editor/tests/editor.tsx
+++ packages/html/src/editor/tests/editor.tsx
@@ -7,6 +7,7 @@ import { DropdownList } from '../../dropdownlist';
 import { Editor } from '..';
 import { EditorContent } from '../editor-content';
 import { EditorToolbar } from '../editor-toolbar';
+import { ToolbarItem } from '../../toolbar';
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
@@ -35,7 +36,7 @@ root.render(
                             <Button icon="undo"></Button>
                             <Button icon="redo"></Button>
                         </ButtonGroup>
-                        <Button icon="more-vertical" className="k-overflow-anchor" fillMode="flat" rounded={null}></Button>
+                        <Button icon="more-vertical" className="k-toolbar-overflow-button" fillMode="flat" rounded={null}></Button>
                     </EditorToolbar>
                     <EditorContent>
                         <iframe className="k-iframe" frameBorder="0" src="editor-iFrameContent.html"></iframe>
@@ -49,7 +50,7 @@ root.render(
                             <Button icon="undo"></Button>
                             <Button icon="redo"></Button>
                         </ButtonGroup>
-                        <Button icon="more-vertical" className="k-overflow-anchor" fillMode="flat" rounded={null}></Button>
+                        <Button icon="more-vertical" className="k-toolbar-overflow-button" fillMode="flat" rounded={null}></Button>
                     </EditorToolbar>
                     <EditorContent>
                         <div className="ProseMirror">
@@ -94,98 +95,72 @@ root.render(
             <section>
                 <Editor style={{ height: "350px" }}>
                     <EditorToolbar>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="bold"></Button>
-                                <Button icon="italic"></Button>
-                                <Button icon="underline"></Button>
-                                <Button icon="strikethrough"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="subscript"></Button>
-                                <Button icon="superscript"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="align-left"></Button>
-                                <Button icon="align-center"></Button>
-                                <Button icon="align-right"></Button>
-                                <Button icon="align-justify"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="indent-increase"></Button>
-                                <Button icon="indent-decrease"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="list-ordered"></Button>
-                                <Button icon="list-unordered"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
+                        <ButtonGroup>
+                            <Button icon="bold"></Button>
+                            <Button icon="italic"></Button>
+                            <Button icon="underline"></Button>
+                            <Button icon="strikethrough"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="subscript"></Button>
+                            <Button icon="superscript"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="align-left"></Button>
+                            <Button icon="align-center"></Button>
+                            <Button icon="align-right"></Button>
+                            <Button icon="align-justify"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="indent-increase"></Button>
+                            <Button icon="indent-decrease"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="list-ordered"></Button>
+                            <Button icon="list-unordered"></Button>
+                        </ButtonGroup>
+                        <ToolbarItem>
                             <DropdownList value="Font Size" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <DropdownList value="Font Name" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <DropdownList value="Paragraph" />
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="undo"></Button>
-                                <Button icon="redo"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="link-horizontal"></Button>
-                                <Button icon="unlink-horizontal"></Button>
-                                <Button icon="image"></Button>
-                                <Button icon="html"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-insert"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-row-insert-above"></Button>
-                                <Button icon="table-row-insert-below"></Button>
-                                <Button icon="table-column-insert-left"></Button>
-                                <Button icon="table-column-insert-right"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-row-delete"></Button>
-                                <Button icon="table-column-delete"></Button>
-                                <Button icon="table-delete"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="cells-merge"></Button>
-                                <Button icon="cell-split-horizontally"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ButtonGroup>
+                            <Button icon="undo"></Button>
+                            <Button icon="redo"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="link-horizontal"></Button>
+                            <Button icon="unlink-horizontal"></Button>
+                            <Button icon="image"></Button>
+                            <Button icon="html"></Button>
+                        </ButtonGroup>
+                        <Button icon="table-insert"></Button>
+                        <ButtonGroup>
+                            <Button icon="table-row-insert-above"></Button>
+                            <Button icon="table-row-insert-below"></Button>
+                            <Button icon="table-column-insert-left"></Button>
+                            <Button icon="table-column-insert-right"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="table-row-delete"></Button>
+                            <Button icon="table-column-delete"></Button>
+                            <Button icon="table-delete"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="cells-merge"></Button>
+                            <Button icon="cell-split-horizontally"></Button>
+                        </ButtonGroup>
+                        <ToolbarItem>
                             <ColorPicker value="red" valueIconName="foreground-color" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <ColorPicker value="fuchsia" valueIconName="paint" />
-                        </div>
-                        <div className="k-tool-group">
-                            <Button icon="clear-css"></Button>
-                        </div>
+                        </ToolbarItem>
+                        <Button icon="clear-css"></Button>
                     </EditorToolbar>
                     <EditorContent focus>
                         <iframe className="k-iframe" frameBorder="0" src="editor-iFrameContent.html"></iframe>
@@ -196,98 +171,71 @@ root.render(
             <section>
                 <Editor readonly style={{ height: "350px" }}>
                     <EditorToolbar>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="bold"></Button>
-                                <Button icon="italic"></Button>
-                                <Button icon="underline"></Button>
-                                <Button icon="strikethrough"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="subscript"></Button>
-                                <Button icon="superscript"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="align-left"></Button>
-                                <Button icon="align-center"></Button>
-                                <Button icon="align-right"></Button>
-                                <Button icon="align-justify"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="indent-increase"></Button>
-                                <Button icon="indent-decrease"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="list-ordered"></Button>
-                                <Button icon="list-unordered"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
+                        <ButtonGroup>
+                            <Button icon="bold"></Button>
+                            <Button icon="italic"></Button>
+                            <Button icon="underline"></Button>
+                            <Button icon="strikethrough"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="subscript"></Button>
+                            <Button icon="superscript"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="align-left"></Button>
+                            <Button icon="align-center"></Button>
+                            <Button icon="align-right"></Button>
+                            <Button icon="align-justify"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="indent-increase"></Button>
+                            <Button icon="indent-decrease"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="list-ordered"></Button>
+                            <Button icon="list-unordered"></Button>
+                        </ButtonGroup>
+                        <ToolbarItem>
                             <DropdownList value="Font Size" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <DropdownList value="Font Name" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <DropdownList value="Paragraph" />
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="undo"></Button>
-                                <Button icon="redo"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="link-horizontal"></Button>
-                                <Button icon="unlink-horizontal"></Button>
-                                <Button icon="image"></Button>
-                                <Button icon="html"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-insert"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-row-insert-above"></Button>
-                                <Button icon="table-row-insert-below"></Button>
-                                <Button icon="table-column-insert-left"></Button>
-                                <Button icon="table-column-insert-right"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="table-row-delete"></Button>
-                                <Button icon="table-column-delete"></Button>
-                                <Button icon="table-delete"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
-                            <div className="k-button-group">
-                                <Button icon="cells-merge"></Button>
-                                <Button icon="cell-split-horizontally"></Button>
-                            </div>
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ButtonGroup>
+                            <Button icon="undo"></Button>
+                            <Button icon="redo"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="link-horizontal"></Button>
+                            <Button icon="unlink-horizontal"></Button>
+                            <Button icon="image"></Button>
+                            <Button icon="html"></Button>
+                        </ButtonGroup>
+                        <Button icon="table-insert"></Button>
+                        <ButtonGroup>
+                            <Button icon="table-row-insert-below"></Button>
+                            <Button icon="table-column-insert-left"></Button>
+                            <Button icon="table-column-insert-right"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="table-row-delete"></Button>
+                            <Button icon="table-column-delete"></Button>
+                            <Button icon="table-delete"></Button>
+                        </ButtonGroup>
+                        <ButtonGroup>
+                            <Button icon="cells-merge"></Button>
+                            <Button icon="cell-split-horizontally"></Button>
+                        </ButtonGroup>
+                        <ToolbarItem>
                             <ColorPicker value="red" valueIconName="foreground-color" />
-                        </div>
-                        <div className="k-tool-group">
+                        </ToolbarItem>
+                        <ToolbarItem>
                             <ColorPicker value="fuchsia" valueIconName="paint" />
-                        </div>
-                        <div className="k-tool-group">
-                            <Button icon="clear-css"></Button>
-                        </div>
+                        </ToolbarItem>
+                        <Button icon="clear-css"></Button>
                     </EditorToolbar>
                     <EditorContent focus>
                         <div className="ProseMirror"><p>The Editor allows your users to edit HTML in a familiar, user-friendly way.</p><p>In this version, the Editor provides the core HTML editing engine which includes basic text formatting, hyperlinks, and lists. The widget <strong>outputs identical HTML</strong> across all major browsers, follows accessibility standards, and provides API for content manipulation.</p><p>Features include:</p><ul><li>Text formatting</li><li>Bulleted and numbered lists</li><li>Hyperlinks</li><li>Cross-browser support</li><li>Identical HTML output across browsers</li></ul></div>
