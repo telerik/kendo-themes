a17f9036e4bf7918a3fc2abdddd82b5016f20ef7
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-01-13 09:01:18 +0200

    fix(swatches): export ocean blue a11y swatch

--- packages/default/lib/swatches/index.js
+++ packages/default/lib/swatches/index.js
@@ -1,6 +1,7 @@
 module.exports.main = require('./default-main.json');
 module.exports.mainDark = require('./default-main-dark.json');
 module.exports.oceanBlue = require('./default-ocean-blue.json');
+module.exports.oceanBlueA11Y = require('./default-ocean-blue-a11y.json');
 module.exports.blue = require('./default-blue.json');
 module.exports.green = require('./default-green.json');
 module.exports.nordic = require('./default-nordic.json');
