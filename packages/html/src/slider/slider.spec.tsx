0985a1c0a3cfdab3f60de406fb2444e3e2beb0d1
Emil Petrov <emil.petrov@progress.com>
2023-01-30 17:03:55 +0200

    feat(html): add slider helper

---
+++ packages/html/src/slider/slider.spec.tsx
@@ -0,0 +1,120 @@
+import { classNames, stateClassNames, States } from '../utils';
+import { Button } from '../button';
+
+const SLIDER_CLASSNAME = 'k-slider';
+
+const states = [
+    States.hover,
+    States.focus,
+    States.active,
+    States.disabled,
+    States.readonly
+];
+
+const options = {};
+
+export type KendoSliderProps = {
+    type?: 'single' | 'range' | 'gradient',
+    orientation?: 'horizontal' | 'vertical',
+    dir?: 'ltr' | 'rtl',
+    handlePosition?: 'start' | 'end',
+    trackStyle?: React.CSSProperties
+};
+
+export type KendoSliderState = { [K in (typeof states)[number]]?: boolean };
+
+const defaultProps = {
+    type: 'single',
+    orientation: 'horizontal',
+    readonly: false,
+    disabled: false,
+    handlePosition: 'end'
+};
+
+export const Slider = (
+    props: KendoSliderProps &
+        KendoSliderState &
+        React.HTMLAttributes<HTMLDivElement>
+) => {
+    const {
+        type = defaultProps.type,
+        orientation = defaultProps.orientation,
+        readonly = defaultProps.readonly,
+        disabled = defaultProps.disabled,
+        handlePosition = defaultProps.handlePosition,
+        hover,
+        focus,
+        active,
+        trackStyle,
+        dir
+    } = props;
+
+    let iconIncrease;
+    let iconDecrease;
+
+    if ( orientation === 'horizontal' ) {
+        iconIncrease = 'caret-alt-right';
+        iconDecrease = 'caret-alt-left';
+    } else if ( orientation === 'vertical' ) {
+        iconIncrease = 'caret-alt-up';
+        iconDecrease = 'caret-alt-down';
+    }
+
+    return (
+        <div className={classNames(
+            SLIDER_CLASSNAME,
+            props.className,
+            stateClassNames(SLIDER_CLASSNAME, {
+                readonly,
+                disabled
+            }),
+            {
+                [`k-slider-${orientation}`]: orientation,
+                'k-colorgradient-slider': type === 'gradient',
+                'k-range-slider': type === 'range'
+            }
+        )} dir={dir} >
+            { type !== 'gradient' && <Button className="k-button-decrease" rounded="full" icon={iconDecrease} /> }
+            <div className="k-slider-track-wrap">
+                {
+                    type !== 'gradient' &&
+                        <ul className="k-reset k-slider-items">
+                            {props.children}
+                        </ul>
+                }
+                <div className="k-slider-track" style={trackStyle}>
+                    <div className="k-slider-selection"></div>
+                    { type === 'range' &&
+
+                    <span className={classNames(
+                        'k-draghandle',
+                        'k-draghandle-start',
+                        stateClassNames(SLIDER_CLASSNAME, {
+                            hover,
+                            focus,
+                            active
+                        })
+                    )}></span> }
+
+                    <span className={classNames(
+                        'k-draghandle',
+                        `k-draghandle-${handlePosition}`,
+                        stateClassNames(SLIDER_CLASSNAME, {
+                            hover,
+                            focus,
+                            active
+                        })
+                    )}></span>
+                </div>
+            </div>
+            { type !== 'gradient' && <Button className="k-button-increase" rounded="full" icon={iconIncrease} /> }
+        </div>
+    );
+};
+
+Slider.states = states;
+Slider.options = options;
+Slider.className = SLIDER_CLASSNAME;
+Slider.defaultProps = defaultProps;
+
+export default Slider;
