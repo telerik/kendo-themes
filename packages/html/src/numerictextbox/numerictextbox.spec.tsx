bd0f9e71ec73eb080122af6ca74390a9b8b2da80
Teya Veselinova <tveselin@progress.com>
2023-02-17 12:03:53 +0200

    chore(html): merge numerictextbox html rendering and hierarchy specs

---
+++ packages/html/src/numerictextbox/numerictextbox.spec.tsx
@@ -0,0 +1,108 @@
+import { classNames, States } from '../utils-new';
+import {
+    Input,
+    InputInnerInput,
+    InputClearValue,
+    InputLoadingIcon,
+    InputValidationIcon
+} from '../input';
+import { SpinButton } from '../spinbutton';
+
+
+export const NUMERICTEXTBOX_CLASSNAME = `k-numerictextbox`;
+
+const states = [
+    States.hover,
+    States.focus,
+    States.valid,
+    States.invalid,
+    States.loading,
+    States.required,
+    States.disabled,
+];
+
+const options = {
+    size: [ "small", "medium", "large" ],
+    rounded: [ "small", "medium", "large", "full" ],
+    fillMode: [ "solid", "flat", "outline" ],
+};
+
+export type KendoNumericTextboxOptions = {
+    size?: (typeof options.size)[number] | null;
+    rounded?: (typeof options.rounded)[number] | null;
+    fillMode?: (typeof options.fillMode)[number] | null;
+};
+
+export type KendoNumericTextboxProps = KendoNumericTextboxOptions & {
+    value?: string;
+    placeholder?: string;
+    showSpinButton?: boolean;
+    showClearButton?: boolean;
+};
+
+export type KendoNumericTextboxState = { [K in (typeof states)[number]]?: boolean };
+
+const defaultProps = {
+    showSpinButton: true,
+    showClearButton: true
+};
+
+export const NumericTextbox = (
+    props: KendoNumericTextboxProps &
+        KendoNumericTextboxState &
+        React.HTMLAttributes<HTMLSpanElement>
+) => {
+    const {
+        value,
+        placeholder,
+        size,
+        rounded,
+        fillMode,
+        hover,
+        focus,
+        valid,
+        invalid,
+        required,
+        loading,
+        disabled,
+        showClearButton = defaultProps.showClearButton,
+        showSpinButton = defaultProps.showSpinButton,
+        ...other
+    } = props;
+
+
+    return (
+        <Input
+            {...other}
+            size={size}
+            rounded={rounded}
+            fillMode={fillMode}
+            hover={hover}
+            focus={focus}
+            valid={valid}
+            invalid={invalid}
+            required={required}
+            loading={loading}
+            disabled={disabled}
+            className={classNames(props.className, NUMERICTEXTBOX_CLASSNAME)}
+        >
+            <InputInnerInput placeholder={placeholder} value={value} />
+            <InputValidationIcon {...props} />
+            <InputLoadingIcon {...props} />
+            {showClearButton && (<InputClearValue {...props} />)}
+            {showSpinButton && (
+                <SpinButton
+                    className="k-input-spinner"
+                    size={size}
+                    fillMode={fillMode}
+                />
+            )}
+        </Input>
+    );
+};
+
+NumericTextbox.states = states;
+NumericTextbox.options = options;
+NumericTextbox.className = NUMERICTEXTBOX_CLASSNAME;
+
+export default NumericTextbox;
