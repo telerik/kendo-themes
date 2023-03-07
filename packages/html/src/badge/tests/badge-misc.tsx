b027057da1dec2a9cdfe4254c5969eb106359def
Teya Veselinova <tveselin@progress.com>
2023-03-07 15:20:44 +0200

    chore(html): merge badge html rendering and hierarchy specs

--- packages/html/src/badge/tests/badge-misc.tsx
+++ packages/html/src/badge/tests/badge-misc.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Icon } from '../../icon';
-import { Badge } from '../badge';
+import { Badge } from '../../badge';
 
 const root = ReactDOM.createRoot(
     document.getElementById('app') as HTMLElement
