d4074c453cbbff8d9fc36270f5bafff68cac27df
Teya Veselinova <tveselin@progress.com>
2023-03-07 11:47:17 +0200

    chore(html): merge daterangepicker html rendering and hierarchy specs

---
+++ packages/html/src/daterangepicker/daterangepicker.spec.tsx
@@ -0,0 +1,87 @@
+import { classNames, stateClassNames, States, Size, Roundness, FillMode } from '../utils-new';
+import { DateInput } from '../dateinput';
+import { FloatingLabel } from '../floating-label';
+import { Button } from '../button';
+
+export const DATERANGEPICKER_CLASSNAME = `k-daterangepicker`;
+
+const states = [
+    States.disabled
+];
+
+const options = {
+    size: [ Size.small, Size.medium, Size.large ],
+    rounded: [ Roundness.small, Roundness.medium, Roundness.large, Roundness.full ],
+    fillMode: [ FillMode.solid, FillMode.flat, FillMode.outline ],
+};
+
+export type KendoDateRangePickerOptions = {
+    size?: (typeof options.size)[number] | null;
+    rounded?: (typeof options.rounded)[number] | null;
+    fillMode?: (typeof options.fillMode)[number] | null;
+};
+
+export type KendoDateRangePickerProps = KendoDateRangePickerOptions & {
+    swapButton?: boolean;
+};
+
+export type KendoDateRangePickerState = { [K in (typeof states)[number]]?: boolean };
+
+export const DateRangePicker = (
+    props: KendoDateRangePickerProps &
+        KendoDateRangePickerState &
+        React.HTMLAttributes<HTMLSpanElement>
+) => {
+    const {
+        size,
+        rounded,
+        fillMode,
+        disabled,
+        swapButton,
+        ...other
+    } = props;
+
+
+    return (
+        <span
+            {...other}
+            className={classNames(
+                props.className,
+                DATERANGEPICKER_CLASSNAME,
+                stateClassNames(DATERANGEPICKER_CLASSNAME, { disabled }),
+            )}
+        >
+            <FloatingLabel label="Start">
+                <DateInput
+                    showSpinButton={false}
+                    size={size}
+                    rounded={rounded}
+                    fillMode={fillMode}
+                />
+            </FloatingLabel>
+            {swapButton && (
+                <Button
+                    rounded={rounded}
+                    size={size}
+                    fillMode="flat"
+                    icon="arrows-swap"
+                    iconClassName="k-rotate-90"
+                />
+            )}
+            <FloatingLabel label="End">
+                <DateInput
+                    showSpinButton={false}
+                    size={size}
+                    rounded={rounded}
+                    fillMode={fillMode}
+                />
+            </FloatingLabel>
+        </span>
+    );
+};
+
+DateRangePicker.states = states;
+DateRangePicker.options = options;
+DateRangePicker.className = DATERANGEPICKER_CLASSNAME;
+
+export default DateRangePicker;
