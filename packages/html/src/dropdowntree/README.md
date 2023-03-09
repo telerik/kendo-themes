e2bbbaf7580aecb21a582769807675da17f53240
Teya Veselinova <tveselin@progress.com>
2023-03-09 14:10:32 +0200

    chore(html): merge dropdowntree html rendering and hierarchy specs

---
+++ packages/html/src/dropdowntree/README.md
@@ -0,0 +1,39 @@
+```html
+<!-- default rendering -->
+<span class="k-dropdownlist k-picker k-picker-md k-rounded-md k-picker-solid">
+    <span class="k-input-inner"><span class="k-input-value-text"><span></span>
+    <button type="button" class="k-input-button k-button k-icon-button k-button-md k-button-solid k-button-solid-base">
+        <span class="k-button-icon k-icon k-i-arrow-s"></span>
+    </button>
+</span>
+
+<!-- canonical rendering -->
+<span class="
+    k-dropdownlist
+    k-picker
+    k-picker-{size}
+    k-rounded-{rounded}
+    k-picker-{fillMode}
+
+    {valid && 'k-valid'}
+    {invalid && 'k-invalid'}
+    {loading && 'k-loading'}
+    {required && 'k-required'}
+    {disabled && 'k-disabled'}
+">
+    {showInputIcon && inputIconName !== '' && <span class="k-input-icon k-i-icon k-i-{inputIconName}"></span>}
+    {inputPrefix && <span class="k-input-prefix">...</span>}
+    <span class="k-input-inner">
+        <span class="k-input-value-icon k-icon k-i-{valueIcon}"><span>
+        <span class="k-input-value-text">{valueText}<span>
+    </span>
+    {inputPrefix && <span class="k-input-suffix">...</span>}
+    {showValidationIcon && valid && <span class="k-input-validation-icon k-icon k-i-check"></span>}
+    {showValidationIcon && invalid && <span class="k-input-validation-icon k-icon k-i-warning"></span>}
+    {showLoadingIcon && loading && <span class="k-input-loading-icon k-icon k-i-loading"></span>}
+    {showClearValue && text !== '' && <span class="k-clear-value"><span class="k-icon k-i-x"></span></span>}
+    <button type="button" class="k-input-button k-button k-icon-button k-button-{size} k-button-{fillMode} k-button-{fillMode}-base">
+        <span class="k-button-icon k-icon k-i-arrow-s"></span>
+    </button>
+</span>
+```
---
+++ packages/html/src/dropdowntree/dropdowntree.spec.tsx
@@ -0,0 +1,138 @@
+import { classNames, States, Size, Roundness, FillMode } from '../utils-new';
+import { Button } from '../button';
+import {
+    Picker,
+    InputInnerSpan,
+    InputLoadingIcon,
+    InputPrefix,
+    InputSuffix,
+    InputValidationIcon
+} from '../input';
+import { Popup } from '../popup';
+
+export const DROPDOWNTREE_CLASSNAME = `k-dropdowntree`;
+
+const states = [
+    States.hover,
+    States.focus,
+    States.valid,
+    States.invalid,
+    States.required,
+    States.disabled,
+    States.loading
+];
+
+const options = {
+    size: [ Size.small, Size.medium, Size.large ],
+    rounded: [ Roundness.small, Roundness.medium, Roundness.large, Roundness.full ],
+    fillMode: [ FillMode.solid, FillMode.flat, FillMode.outline ],
+};
+
+export type KendoDropdownTreeOptions = {
+    size?: (typeof options.size)[number] | null;
+    rounded?: (typeof options.rounded)[number] | null;
+    fillMode?: (typeof options.fillMode)[number] | null;
+};
+
+export type KendoDropdownTreeProps = KendoDropdownTreeOptions & {
+    valueIconName?: string;
+    arrowIconName?: string;
+    prefix?: JSX.Element;
+    suffix?: JSX.Element;
+    value?: string;
+    placeholder?: string;
+    showValue?: boolean;
+    tree?: JSX.Element;
+    opened?: boolean;
+};
+
+export type KendoDropdownTreeState = { [K in (typeof states)[number]]?: boolean };
+
+const defaultProps = {
+    showValue: true,
+    arrowIconName: 'arrow-s'
+};
+
+export const DropdownTree = (
+    props: KendoDropdownTreeProps &
+        KendoDropdownTreeState &
+        React.HTMLAttributes<HTMLSpanElement>
+) => {
+    const {
+        valueIconName,
+        arrowIconName = defaultProps.arrowIconName,
+        prefix,
+        suffix,
+        value,
+        placeholder,
+        size,
+        rounded,
+        fillMode,
+        hover,
+        focus,
+        valid,
+        invalid,
+        required,
+        loading,
+        disabled,
+        showValue = defaultProps.showValue,
+        tree,
+        opened,
+        ...other
+    } = props;
+
+
+    return (
+        <>
+            <Picker
+                {...other}
+                size={size}
+                rounded={rounded}
+                fillMode={fillMode}
+                hover={hover}
+                focus={focus}
+                valid={valid}
+                invalid={invalid}
+                required={required}
+                loading={loading}
+                disabled={disabled}
+                className={classNames(
+                    props.className,
+                    DROPDOWNTREE_CLASSNAME,
+                    {
+                        'k-icon-picker': !showValue && valueIconName
+                    }
+                )}
+            >
+                <InputPrefix>{prefix}</InputPrefix>
+                <InputInnerSpan
+                    placeholder={placeholder}
+                    value={value}
+                    showValue={showValue}
+                    valueIconName={valueIconName}
+                />
+                <InputValidationIcon {...props} />
+                <InputLoadingIcon {...props} />
+                <InputSuffix>{suffix}</InputSuffix>
+                <Button
+                    className="k-input-button"
+                    icon={arrowIconName}
+                    rounded={null}
+                    size={props.size}
+                    fillMode={props.fillMode}
+                />
+            </Picker>
+            { opened && tree &&
+                <Popup>
+                    {tree}
+                </Popup>
+            }
+        </>
+    );
+};
+
+DropdownTree.states = states;
+DropdownTree.options = options;
+DropdownTree.className = DROPDOWNTREE_CLASSNAME;
+
+export default DropdownTree;
