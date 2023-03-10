d2cbc88774437e21ce2c8b46ce93a15cfe4f822a
Teya Veselinova <tveselin@progress.com>
2023-03-10 15:12:43 +0200

    chore(html): merge multiviewcalendar html rendering and hierarchy specs

---
+++ packages/html/src/multiviewcalendar/multiviewcalendar.spec.tsx
@@ -0,0 +1,77 @@
+import { classNames, Size } from '../utils-new';
+import { Calendar } from '../calendar';
+
+const MULTIVIEWCALENDAR_CLASSNAME = `k-calendar-range`;
+
+const states = [];
+
+const options = {
+    size: [ Size.small, Size.medium, Size.large ],
+};
+
+export type KendoMultiViewCalendarOptions = {
+    size?: (typeof options.size)[number] | null;
+};
+
+export type KendoMultiViewCalendarProps = KendoMultiViewCalendarOptions & {
+    calendarHeaderText?: string;
+    calendarView?: 'month' | 'year' | 'decade' | 'century';
+    viewsCount?: number;
+    orientation?: 'horizontal' | 'vertical';
+    showCalendarFooter?: boolean;
+    showWeek?: boolean;
+    showOtherMonth?: boolean;
+    showCalendarCaption?: boolean;
+};
+
+const defaultProps = {
+    size: Size.medium,
+    calendarHeaderText: 'October 2021 - November 2021',
+    viewsCount: 2,
+    orientation: 'horizontal',
+    calendarView: 'month',
+} as const;
+
+export const MultiViewCalendar = (
+    props: KendoMultiViewCalendarProps &
+        React.HTMLAttributes<HTMLDivElement>
+) => {
+    const {
+        size = defaultProps.size,
+        orientation = defaultProps.orientation,
+        calendarView = defaultProps.calendarView,
+        calendarHeaderText = defaultProps.calendarHeaderText,
+        viewsCount = defaultProps.viewsCount,
+        showCalendarFooter,
+        showWeek,
+        showOtherMonth,
+        showCalendarCaption,
+        ...other
+    } = props;
+
+    return (
+        <Calendar
+            {...other}
+            className={classNames(
+                props.className,
+                MULTIVIEWCALENDAR_CLASSNAME,
+            )}
+            size={size}
+            calendarHeaderText={calendarHeaderText}
+            calendarView={calendarView}
+            viewsCount={viewsCount}
+            orientation={orientation}
+            showCalendarFooter={showCalendarFooter}
+            showWeek={showWeek}
+            showOtherMonth={showOtherMonth}
+            showCalendarCaption={showCalendarCaption}
+            selectedRange={true}
+        />
+    );
+};
+
+MultiViewCalendar.states = states;
+MultiViewCalendar.options = options;
+MultiViewCalendar.className = MULTIVIEWCALENDAR_CLASSNAME;
+
+export default MultiViewCalendar;
