7493489abf8d2bffe29312a58d4f92a348d054b6
Teya Veselinova <tveselin@progress.com>
2023-03-13 12:21:02 +0200

    chore(html): merge datetimeselector html rendering and hierarchy specs

---
+++ packages/html/src/datetime-selector/datetime-selector.spec.tsx
@@ -0,0 +1,82 @@
+import { ActionButtons } from '../action-buttons';
+import { Button } from '../button';
+import { ButtonGroup } from '../button-group';
+import { Calendar } from '../calendar';
+import { TimeSelector, TimeSelectorHeader } from '../time-selector';
+import { classNames } from '../utils-new';
+
+const DATETIMESELECTOR_CLASSNAME = `k-datetime-wrap`;
+
+const states = [];
+
+const options = {};
+
+export type KendoDateTimeSelectorProps = {
+    tab?: 'time' | 'date'
+};
+
+const defaultProps = {
+    tab: 'date'
+} as const;
+
+export const DateTimeSelector = (
+    props: KendoDateTimeSelectorProps &
+        React.HTMLAttributes<HTMLDivElement>
+) => {
+    const {
+        tab = defaultProps.tab,
+        ...other
+    } = props;
+
+
+    return (
+        <div
+            {...other}
+            className={classNames(
+                props.className,
+                DATETIMESELECTOR_CLASSNAME,
+                {
+                    'k-date-tab': tab === 'date',
+                    'k-time-tab': tab === 'time',
+                }
+            )}>
+            <div className="k-datetime-buttongroup">
+                <ButtonGroup stretched>
+                    <Button className="k-group-start" selected={tab === 'date'}>Date</Button>
+                    <Button className="k-group-end" selected={tab === 'time'}>Time</Button>
+                </ButtonGroup>
+            </div>
+            <div className="k-datetime-selector">
+                {tab === 'time'
+                    ? <>
+                        <div className="k-datetime-calendar-wrap"></div>
+                        <div className="k-datetime-time-wrap">
+                            <TimeSelector columns={[ "HH", "mm", "ss", "tt" ]} focusedColumn="mm" header={(
+                                <TimeSelectorHeader title="10:00:00 AM">
+                                    <Button fillMode="flat" className="k-time-now">Now</Button>
+                                </TimeSelectorHeader>
+                            )}/>
+                        </div>
+                    </>
+                    : <>
+                        <div className="k-datetime-calendar-wrap">
+                            <Calendar showOtherMonth></Calendar>
+                        </div>
+                        <div className="k-datetime-time-wrap"></div>
+                    </>
+                }
+            </div>
+
+            <ActionButtons className="k-datetime-footer" alignment="stretched">
+                <Button className="k-time-cancel">Cancel</Button>
+                <Button themeColor="primary" className="k-time-accept">Set</Button>
+            </ActionButtons>
+        </div>
+    );
+};
+
+DateTimeSelector.states = states;
+DateTimeSelector.options = options;
+DateTimeSelector.className = DATETIMESELECTOR_CLASSNAME;
+
+export default DateTimeSelector;
