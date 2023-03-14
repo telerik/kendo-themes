eb9f8f7396abdccfe02ef0e4814f93fc66b8f30d
Teya Veselinova <tveselin@progress.com>
2023-03-14 17:32:22 +0200

    chore(html): merge captcha html rendering and hierarchy specs

---
+++ packages/html/src/captcha/captcha.spec.tsx
@@ -0,0 +1,90 @@
+import { Button } from '../button';
+import { Textbox } from '../textbox';
+import { classNames, stateClassNames, States } from '../utils-new';
+
+export const CAPTCHA_CLASSNAME = `k-captcha`;
+
+const states = [
+    States.loading,
+    States.valid,
+    States.invalid,
+    States.disabled
+];
+
+const options = {};
+
+export type KendoCaptchaProps = {
+    value?: string;
+};
+
+export type KendoCaptchaState = { [K in (typeof states)[number]]?: boolean };
+
+export const Captcha = (
+    props: KendoCaptchaProps &
+        KendoCaptchaState &
+        React.HTMLAttributes<HTMLDivElement>
+) => {
+    const {
+        value,
+        valid,
+        invalid,
+        loading,
+        disabled,
+        ...other
+    } = props;
+
+    return (
+        <div
+            {...other}
+            className={classNames(
+                props.className,
+                CAPTCHA_CLASSNAME,
+                stateClassNames(CAPTCHA_CLASSNAME, {
+                    disabled,
+                }),
+                {
+                    'k-captcha-loading': loading,
+                }
+            )}>
+            <div className="k-captcha-image-wrap k-hstack">
+                <div className="k-captcha-image">
+                    <img
+                        className={classNames({ 'k-hidden': loading })}
+                        src="/packages/html/assets/captcha.jpg"
+                    />
+                    {loading && (
+                        <span
+                            style={{ width: '100%', height: '100%' }}
+                            className="k-skeleton k-skeleton-rect">
+                        </span>
+                    )}
+                </div>
+                <div className="k-captcha-image-controls k-vstack">
+                    <Button icon="volume-up" disabled={loading === true}></Button>
+                    <Button icon="reload" disabled={loading === true}></Button>
+                </div>
+                <div className="k-captcha-volume-control k-vstack k-pos-absolute k-hidden">
+                    <div className="k-widget k-slider k-slider-vertical"></div>
+                </div>
+            </div>
+            <div className="k-captcha-input k-vstack">
+                <Textbox
+                    value={value}
+                    invalid={invalid}
+                    disabled={loading === true}
+                />
+                {valid && !loading && (
+                    <span className="k-captcha-validation-message !k-text-success">
+                        Verification Success
+                    </span>
+                )}
+            </div>
+        </div>
+    );
+};
+
+Captcha.states = states;
+Captcha.options = options;
+Captcha.className = CAPTCHA_CLASSNAME;
+
+export default Captcha;
