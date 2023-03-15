891fee75281e9e8ae31e0e198bdba3e02d0af023
Teya Veselinova <tveselin@progress.com>
2023-03-15 14:32:26 +0200

    chore(html): merge colorpicker, colorpalette and colorpreview html rendering and hierarchy specs

--- packages/html/src/colorpalette/colorpalette-row.tsx
+++ packages/html/src/colorpalette/colorpalette-row.tsx
@@ -1,21 +1,3 @@
-import * as React from 'react';
-import { ColorPaletteTileProps } from '../colorpalette';
-
-export interface ColorPaletteRowProps {
-    className?: string;
-    children?: React.ReactElement<ColorPaletteTileProps> | React.ReactElement<ColorPaletteTileProps>[];
-
-}
-
-export class ColorPaletteRow extends React.Component<ColorPaletteRowProps> {
-
-    render() {
-        const {
-            children,
-        } = this.props;
-
-        return (
-            <tr>{children}</tr>
-        );
-    }
-}
+export const ColorPaletteRow = (props: React.HTMLAttributes<HTMLTableRowElement>) => (
+    <tr>{props.children}</tr>
+);
