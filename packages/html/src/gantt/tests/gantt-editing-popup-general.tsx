0624a770cb1255bc493f050fcc43dc1c5057e0a7
Emil Petrov <emil.petrov@progress.com>
2023-04-13 20:02:57 +0300

    test(gantt): add gantt popup edit tests

---
+++ packages/html/src/gantt/tests/gantt-editing-popup-general.tsx
@@ -0,0 +1,97 @@
+import ReactDOM from 'react-dom/client';
+import { Button } from '../../button';
+import { Window } from '../../window';
+import { Textbox } from '../../textbox';
+import { DateTimePicker } from '../../datetimepicker';
+import { NumericTextbox } from '../../numerictextbox';
+import { DropdownList } from '../../dropdownlist';
+import { Form, FormField } from '../../form';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('app') as HTMLElement
+);
+
+const styles = `
+    .k-window {
+        min-width: 580px;
+        position: relative;
+    }
+`;
+
+root.render(
+    <>
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-2">
+
+            <section>
+                <Window title="Edit task" actions={[ 'window-minimize', 'window-maximize', 'close' ]} actionButtons={
+                    <>
+                        <Button icon="save" themeColor="primary">Save</Button>
+                        <Button icon="cancel">Cancel</Button>
+                        <span className="k-spacer"></span>
+                        <Button icon="delete" fillMode="flat" themeColor="primary">Delete</Button>
+                    </>
+                }>
+                    <div className="k-tabstrip k-tabstrip-top">
+                        <div className="k-tabstrip-items-wrapper k-hstack">
+                            <ul className="k-reset k-tabstrip-items">
+                                <li className="k-item k-first k-active">
+                                    <span className="k-link">General</span>
+                                </li>
+                                <li className="k-item">
+                                    <span className="k-link">Resources</span>
+                                </li>
+                                <li className="k-item">
+                                    <span className="k-link">Predecessors</span>
+                                </li>
+                                <li className="k-item k-last">
+                                    <span className="k-link">Successors</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="k-tabstrip-content k-active">
+                            <Form layout="grid" cols={4} gapX={2}>
+                                <FormField
+                                    className="k-form-field-error k-col-span-full"
+                                    label="Title" error="Error"
+                                    editor={ <Textbox invalid value="Invalid" /> }
+                                />
+                                <FormField
+                                    className="k-col-span-2"
+                                    label="Planned Start Date"
+                                    editor={ <DateTimePicker placeholder="dd/MM/yyyy hh:mm AM" /> }
+                                />
+                                <FormField
+                                    className="k-col-span-2"
+                                    label="Planned End Date"
+                                    editor={ <DateTimePicker placeholder="dd/MM/yyyy hh:mm AM" /> }
+                                />
+                                <FormField
+                                    className="k-col-span-2"
+                                    label="Actual Start Date"
+                                    editor={ <DateTimePicker placeholder="dd/MM/yyyy hh:mm AM" /> }
+                                />
+                                <FormField
+                                    className="k-col-span-2"
+                                    label="Actual End Date"
+                                    editor={ <DateTimePicker placeholder="dd/MM/yyyy hh:mm AM" /> }
+                                />
+                                <FormField
+                                    className="k-col-span-1"
+                                    label="Complete"
+                                    editor={ <NumericTextbox /> }
+                                    hint="value from 0 to 1"
+                                />
+                                <FormField
+                                    className="k-col-span-full"
+                                    label="Parent"
+                                    editor={ <DropdownList placeholder="-None-"/> }
+                                />
+                            </Form>
+                        </div>
+                    </div>
+                </Window>
+            </section>
+        </div>
+    </>
+);
