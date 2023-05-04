3e65e9eb53cc716fda906f0eb842071f8c840e48
Kiril Peyanski <peyanski@progress.com>
2023-05-04 16:37:58 +0300

    chore: cleanup

--- packages/html/scripts/build-tests.js
+++ packages/html/scripts/build-tests.js
@@ -2,7 +2,6 @@ const esbuild = require("esbuild");
 const { globSync } = require('glob');
 const config = require('./config');
 
-// const tests = globSync('./src/**/tests/**/*.tsx', { dotRelative: true });
 const tests = globSync('./src/**/tests/**/*.tsx', { dotRelative: true });
 
 (async() => {
