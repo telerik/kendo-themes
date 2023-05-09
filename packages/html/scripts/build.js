1049225e3dd7401ba1af26b230106216891c3593
Kiril Peyanski <peyanski@progress.com>
2023-05-09 09:17:14 +0300

    chore(html): tweak distribution

--- packages/html/scripts/build.js
+++ packages/html/scripts/build.js
@@ -6,10 +6,13 @@ components.push('./src/index.ts');
 
 esbuild.buildSync({
     logLevel: 'error',
+    format: 'esm',
+    target: 'esnext',
     entryPoints: components,
     outdir: './dist',
+    jsx: 'automatic',
     bundle: true,
     minify: false,
-    sourcemap: true,
+    sourcemap: false,
     external: [ 'react', 'react-dom' ]
 });
