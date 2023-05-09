0d818a12d13c7a6c0c63c87fee16b6f2943ab417
Teya Veselinova <tveselin@progress.com>
2023-05-09 18:54:00 +0300

    test(menu): add menu rtl test

--- packages/html/src/menu/tests/menu.tsx
+++ packages/html/src/menu/tests/menu.tsx
@@ -103,6 +103,59 @@ root.render(
                     <Button icon="caret-alt-right" className="k-menu-scroll-button k-scroll-right"></Button>
                 </div>
             </section>
+
+            <span>Horizontal Menu RTL</span>
+            <span></span>
+
+            <section>
+                <ul id="menu" className="k-widget k-reset k-header k-menu k-menu-horizontal" dir="rtl">
+                    <li className="k-item k-menu-item k-disabled k-first">
+                        <span className="k-link k-menu-link"> Disabled</span>
+                    </li>
+                    <li className="k-item k-menu-item">
+                        <span className="k-link k-menu-link">Normal</span>
+                    </li>
+                    <li className="k-item k-menu-item k-hover">
+                        <span className="k-link k-menu-link">
+                            <span className="k-menu-link-text">Hover</span>
+                            <span className="k-menu-expand-arrow">
+                                <Icon icon="caret-alt-down"></Icon>
+                            </span>
+                        </span>
+                    </li>
+                    <li className="k-item k-menu-item">
+                        <span className="k-link k-menu-link k-active">
+                            <span className="k-menu-link-text">Active</span>
+                            <span className="k-menu-expand-arrow">
+                                <Icon icon="caret-alt-down"></Icon>
+                            </span>
+                        </span>
+                        <div className="k-animation-container k-animation-container-fixed k-animation-container-shown">
+                            <Popup className="k-menu-popup">
+                                <MenuList>
+                                    <MenuItem text="Disabled" disabled></MenuItem>
+                                    <MenuItem text="Normal"></MenuItem>
+                                    <MenuItem text="Hover" hover showArrow></MenuItem>
+                                    <MenuItem text="Active" active showArrow></MenuItem>
+                                    <MenuItem text="Focus" focus></MenuItem>
+                                    <MenuItem text="Focus & Active" active focus></MenuItem>
+                                </MenuList>
+                            </Popup>
+                        </div>
+                    </li>
+                    <li className="k-item k-menu-item k-focus">
+                        <span className="k-link k-menu-link">
+                            <span className="k-menu-link-text">Focus</span>
+                        </span>
+                    </li>
+                    <li className="k-item k-menu-item k-last">
+                        <span className="k-link k-menu-link">
+                            <Icon icon="folder"></Icon>
+                            <span className="k-menu-link-text">Normal</span>
+                        </span>
+                    </li>
+                </ul>
+            </section>
         </div>
 
     </>
