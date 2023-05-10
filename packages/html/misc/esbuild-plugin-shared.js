32a6291a7537d4715ddb3d0092086d7a55759414
Kiril Peyanski <peyanski@progress.com>
2023-05-10 15:34:28 +0300

    chore: network intercept instead of file copy

--- packages/html/misc/esbuild-plugin-shared.js
+++ packages/html/misc/esbuild-plugin-shared.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 const fs = require("fs");
 const p = require("path");
-const glob = require("glob");
 
 module.exports = ({ dir: sharedDir, main: mainFile }) => ({
     name: "shared",
@@ -15,14 +14,6 @@ module.exports = ({ dir: sharedDir, main: mainFile }) => ({
 
         const sharedMain = p.join(sharedDir, mainFile);
         const sharedApp = fs.readFileSync(sharedMain, "utf-8");
-
-        build.onStart(() => {
-            if (fs.existsSync(build.initialOptions.outdir)) {
-                fs.rmSync(build.initialOptions.outdir, {
-                    recursive: true,
-                });
-            }
-        });
 
         build.onResolve({ filter: entryPointsRegex }, (args) => {
             const path = p.resolve(args.path);
@@ -44,29 +35,6 @@ module.exports = ({ dir: sharedDir, main: mainFile }) => ({
             };
         });
 
-        build.onEnd((args) => {
-            if (args.errors.length > 0) {
-                // eslint-disable-next-line no-console
-                console.error(args.errors);
-            } else {
-                Object.keys(args.metafile.outputs).forEach((key) => {
-                    const output = args.metafile.outputs[key];
-                    // console.log(output);
-                    if (output.entryPoint) {
-                        const destination = p.resolve(key);
-                        const dirname = p.dirname(destination);
-                        const statics = glob.sync(`${sharedDir}/**/*.{html,css,png,jpg,jpeg,gif,svg,js}`);
 
-                        statics.forEach((file) => {
-                            const filename = p.join(dirname, p.basename(file));
-                            if (!fs.existsSync(filename)) {
-                                fs.copyFileSync(file, filename);
-                            }
-                        });
-                    }
-                });
-                console.info("Compilation successful!");
-            }
-        });
     },
 });
