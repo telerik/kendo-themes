ed8221c06ca9fd7cb428eb120f91a3aea7ef25e7
Kiril Peyanski <peyanski@progress.com>
2023-05-12 08:42:41 +0300

    chore: add button templates

--- packages/html/src/button/button.spec.tsx
+++ packages/html/src/button/button.spec.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '../icon';
-import { classNames, optionClassNames, stateClassNames, States, Size, Roundness, FillMode, ThemeColor } from '../utils';
+import { classNames, optionClassNames, stateClassNames, States, Size, Roundness, FillMode, ThemeColor } from '../misc';
 
 export const BUTTON_CLASSNAME = `k-button`;
 
