3e0866924fd3e608bb6150f007b3466773bc2a93
Kiril Peyanski <peyanski@progress.com>
2023-05-12 08:40:23 +0300

    chore: add dev server

---
+++ packages/html/misc/esbuild-plugin-shared.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+const fs = require("fs");
+const p = require("path");
+
+module.exports = ({ dir: sharedDir, main: mainFile }) => ({
+    name: "shared",
+    setup(build) {
+        const initialEntryPoints = build.initialOptions.entryPoints;
+        const entryPoints = initialEntryPoints.map((entryPoint) => p.join(p.resolve(p.dirname(entryPoint)), p.basename(entryPoint, p.extname(entryPoint)), mainFile));
+
+        build.initialOptions.entryPoints = entryPoints;
+
+        const entryPointsRegex = new RegExp(`(${entryPoints.join("|")})`);
+
+        const sharedMain = p.join(sharedDir, mainFile);
+        const sharedApp = fs.readFileSync(sharedMain, "utf-8");
+
+        build.onResolve({ filter: entryPointsRegex }, (args) => {
+            const path = p.resolve(args.path);
+            return { path: path, namespace: "file" };
+        });
+
+        build.onLoad({ filter: entryPointsRegex }, (args) => {
+            const ext = p.extname(args.path);
+
+            const dirname = p.dirname(args.path);
+            const filename = p.basename(dirname);
+            const modifiedContent = sharedApp.replace("%MAIN_FILE%", `./${filename}`);
+
+            return {
+                watchFiles: [ args.path ],
+                contents: modifiedContent,
+                loader: ext.replace(/^./, ""),
+                resolveDir: p.dirname(dirname),
+            };
+        });
+
+
+    },
+});
--- packages/html/scripts/build-tests.js
+++ packages/html/scripts/build-tests.js
@@ -1,13 +1,9 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 const { globSync } = require('glob');
+const config = require('./config');
 
 const tests = globSync('./src/**/tests/**/*.tsx', { dotRelative: true });
 
-esbuild.buildSync({
-    logLevel: 'error',
-    entryPoints: tests,
-    outdir: './dist',
-    bundle: true,
-    minify: false,
-    sourcemap: false
-});
+(async() => {
+    await esbuild.build(config(tests));
+})();
