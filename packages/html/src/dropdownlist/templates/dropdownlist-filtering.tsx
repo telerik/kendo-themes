81d177f7d8266b422509670327a75e778bf08226
Kiril Peyanski <peyanski@progress.com>
2023-05-12 08:43:31 +0300

    chore: add dropdownlist templates

---
+++ packages/html/src/dropdownlist/templates/dropdownlist-filtering.tsx
@@ -0,0 +1,22 @@
+import { List, ListItem } from "../../list";
+import { Searchbox } from "../../searchbox";
+import DropdownList from "../dropdownlist.spec";
+
+export const DropdownListFiltering = (props) => (
+    <DropdownList
+        opened={true}
+        popup={
+            <>
+                <div className="k-list-filter">
+                    <Searchbox />
+                </div>
+                <List>
+                    <ListItem>List item</ListItem>
+                    <ListItem>List item</ListItem>
+                    <ListItem>List item</ListItem>
+                </List>
+            </>
+        }
+        {...props}
+    />
+);
--- packages/html/src/dropdownlist/tests/dropdownlist-adaptive.tsx
+++ packages/html/src/dropdownlist/tests/dropdownlist-adaptive.tsx
@@ -1,10 +1,6 @@
-import ReactDOM from 'react-dom/client';
 import { ActionSheet, ActionSheetFooter, ActionSheetHeader } from '../../action-sheet';
 import { List, ListItem } from '../../list';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('app') as HTMLElement
-);
 
 const styles = `
     #test-area {
@@ -19,7 +15,7 @@ const styles = `
     }
 `;
 
-root.render(
+export default () =>(
     <>
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-2">
