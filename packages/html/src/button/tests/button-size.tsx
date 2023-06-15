34a3c6ed58a86a7690befc814854cdb32d0e7ff5
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-06-15 12:41:48 +0300

    chore(html): use button templates in tests

--- packages/html/src/button/tests/button-size.tsx
+++ packages/html/src/button/tests/button-size.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../button.spec';
 import { TextButton } from '../templates/text-button';
 import { IconTextButton } from '../templates/icon-text-button';
 import { IconButton } from '../templates/icon-button';
@@ -23,92 +24,18 @@ export default () => (
             <span>small</span>
             <span>medium</span>
             <span>large</span>
-
-            <span>null</span>
-            <section>
-                <TextButton size="small" rounded={null}></TextButton>
-                <IconTextButton size="small" rounded={null} ></IconTextButton>
-                <IconButton size="small" rounded={null} ></IconButton>
-            </section>
-            <section>
-                <TextButton size="medium" rounded={null}></TextButton>
-                <IconTextButton size="medium" rounded={null} ></IconTextButton>
-                <IconButton size="medium" rounded={null} ></IconButton>
-            </section>
-            <section>
-                <TextButton size="large" rounded={null}></TextButton>
-                <IconTextButton size="large" rounded={null} ></IconTextButton>
-                <IconButton size="large" rounded={null} ></IconButton>
-            </section>
-
-            <span>small</span>
-            <section>
-                <TextButton size="small" rounded="small"></TextButton>
-                <IconTextButton size="small" rounded="small" ></IconTextButton>
-                <IconButton size="small" rounded="small" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="medium" rounded="small"></TextButton>
-                <IconTextButton size="medium" rounded="small" ></IconTextButton>
-                <IconButton size="medium" rounded="small" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="large" rounded="small"></TextButton>
-                <IconTextButton size="large" rounded="small" ></IconTextButton>
-                <IconButton size="large" rounded="small" ></IconButton>
-            </section>
-
-            <span>medium</span>
-            <section>
-                <TextButton size="small" rounded="medium"></TextButton>
-                <IconTextButton size="small" rounded="medium" ></IconTextButton>
-                <IconButton size="small" rounded="medium" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="medium" rounded="medium"></TextButton>
-                <IconTextButton size="medium" rounded="medium" ></IconTextButton>
-                <IconButton size="medium" rounded="medium" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="large" rounded="medium"></TextButton>
-                <IconTextButton size="large" rounded="medium" ></IconTextButton>
-                <IconButton size="large" rounded="medium" ></IconButton>
-            </section>
-
-            <span>large</span>
-            <section>
-                <TextButton size="small" rounded="large"></TextButton>
-                <IconTextButton size="small" rounded="large" ></IconTextButton>
-                <IconButton size="small" rounded="large" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="medium" rounded="large"></TextButton>
-                <IconTextButton size="medium" rounded="large" ></IconTextButton>
-                <IconButton size="medium" rounded="large" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="large" rounded="large"></TextButton>
-                <IconTextButton size="large" rounded="large" ></IconTextButton>
-                <IconButton size="large" rounded="large" ></IconButton>
-            </section>
-
-            <span>full</span>
-            <section>
-                <TextButton size="small" rounded="full"></TextButton>
-                <IconTextButton size="small" rounded="full" ></IconTextButton>
-                <IconButton size="small" rounded="full" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="medium" rounded="full"></TextButton>
-                <IconTextButton size="medium" rounded="full" ></IconTextButton>
-                <IconButton size="medium" rounded="full" ></IconButton>
-            </section>
-            <section>
-                <TextButton size="large" rounded="full"></TextButton>
-                <IconTextButton size="large" rounded="full" ></IconTextButton>
-                <IconButton size="large" rounded="full" ></IconButton>
-            </section>
-
+            {[ null,...Button.options.rounded ].map((roundness) => (
+                <>
+                    <span>{roundness || 'null'}</span>
+                    {Button.options.size.map((size) => (
+                        <section>
+                            <TextButton size={size} rounded={roundness}></TextButton>
+                            <IconTextButton size={size} rounded={roundness} ></IconTextButton>
+                            <IconButton size={size} rounded={roundness} ></IconButton>
+                        </section>
+                    ))}
+                </>
+            ))}
         </div>
 
     </>
