4bf7920f078516c53294fb1954ad35504d8a35d1
Emil Petrov <emil.petrov@progress.com>
2023-07-04 11:17:14 +0300

    test(chat): reuse spec

--- packages/html/src/chat/tests/chat-rtl.tsx
+++ packages/html/src/chat/tests/chat-rtl.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Avatar } from '../../avatar';
-import { Button } from '../../button';
-import { Textbox } from '../../textbox';
+import { Card, CardDeck, CardBody } from '../../card';
+import { Chat, ChatMessage, ChatMessageGroup, ChatQuickReply } from '../../chat';
 
 
 const styles = `
@@ -17,86 +16,48 @@ export default () =>(
         <div id="test-area" className="k-d-grid k-grid-cols-2">
 
             <section>
-                <div className="k-widget k-chat k-rtl">
-                    <div className="k-message-list k-avatars">
-                        <div className="k-message-list-content">
-                            <div className="k-timestamp">Bubbles</div>
-                            <div className="k-message-group">
-                                <p className="k-author">Bruv</p>
-                                <Avatar type="image">
-                                    <img src="/packages/html/assets/avatar.jpg" />
-                                </Avatar>
-                                <div className="k-message k-only">
-                                    <time className="k-message-time">Thu Feb 08 2018</time>
-                                    <div className="k-bubble">
-                                        How can I help you?
-                                    </div>
-                                    <span className="k-message-status">Seen</span>
-                                </div>
-                            </div>
-                            <div className="k-message-group k-alt">
-                                <p className="k-author">Bruv</p>
-                                <Avatar type="image">
-                                    <img src="/packages/html/assets/avatar.jpg" />
-                                </Avatar>
-                                <div className="k-message k-only">
-                                    <time className="k-message-time">Thu Feb 08 2018</time>
-                                    <div className="k-bubble">
-                                        How can I help you?
-                                    </div>
-                                    <span className="k-message-status">Seen</span>
-                                </div>
-                            </div>
-                            <div className="k-timestamp">Quick Replies</div>
-                            <div className="k-quick-replies">
-                                <span className="k-quick-reply">Quick</span>
-                                <span className="k-quick-reply">Reply</span>
-                                <span className="k-quick-reply">Another quick</span>
-                                <span className="k-quick-reply">Reply with different length</span>
-                            </div>
-                            <div className="k-timestamp">Card Deck</div>
-                            <div className="k-card-deck">
-                                <div className="k-card">
-                                    <div className="k-card-body">
-                                        <h5 className="k-card-title">Card Title</h5>
-                                        <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div className="k-card">
-                                    <div className="k-card-body">
-                                        <h5 className="k-card-title">Card Title</h5>
-                                        <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div className="k-card">
-                                    <div className="k-card-body">
-                                        <h5 className="k-card-title">Card Title</h5>
-                                        <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div className="k-card">
-                                    <div className="k-card-body">
-                                        <h5 className="k-card-title">Card Title</h5>
-                                        <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <Textbox
-                        className="k-message-box"
-                        placeholder="Type a message..."
-                        suffix={
-                            <>
-                                <Button fillMode="flat" icon="pencil" className="k-chat-send"></Button>
-                            </>
-                        }
-                    />
-                </div>
+                <Chat dir="rtl" showToolbar={false} showMoreButton={false}>
+                    <div className="k-timestamp">Bubbles</div>
+                    <ChatMessageGroup>
+                        <ChatMessage className="k-only" />
+                    </ChatMessageGroup>
+                    <ChatMessageGroup alt>
+                        <ChatMessage className="k-only" />
+                    </ChatMessageGroup>
+                    <div className="k-timestamp">Quick Replies</div>
+                    <ChatQuickReply />
+                    <div className="k-timestamp">Card Deck</div>
+                    <CardDeck>
+                        <Card>
+                            <CardBody>
+                                <h5 className="k-card-title">Card Title</h5>
+                                <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardBody>
+                                <h5 className="k-card-title">Card Title</h5>
+                                <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardBody>
+                                <h5 className="k-card-title">Card Title</h5>
+                                <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardBody>
+                                <h5 className="k-card-title">Card Title</h5>
+                                <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>
+                </Chat>
             </section>
 
         </div>
--- packages/html/src/chat/tests/chat-scrollable-deck.tsx
+++ packages/html/src/chat/tests/chat-scrollable-deck.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Avatar } from '../../avatar';
 import { Button } from '../../button';
-import { Card, CardBody, CardDeck } from '../../card';
+import { Card, CardDeck, CardBody } from '../../card';
+import { Chat, ChatMessage, ChatMessageGroup } from '../../chat';
 
 
 const styles = `
@@ -17,60 +17,46 @@ export default () =>(
         <div id="test-area" className="k-d-grid k-grid-cols-2">
 
             <section>
-                <div className="k-widget k-chat">
-                    <div className="k-message-list k-avatars">
-                        <div className="k-message-list-content">
-                            <div className="k-timestamp">Card Deck Scrollable</div>
-                            <div className="k-message-group">
-                                <p className="k-author">Bruv</p>
-                                <Avatar type="image">
-                                    <img src="/packages/html/assets/avatar.jpg" />
-                                </Avatar>
-                                <div className="k-message k-only">
-                                    <time className="k-message-time">Thu Feb 08 2018</time>
-                                    <div className="k-bubble">
-                                        Message before card deck
-                                    </div>
-                                    <span className="k-message-status">Seen</span>
-                                </div>
-                            </div>
-                            <div className="k-card-deck-scrollwrap">
-                                <Button icon="chevron-left"></Button>
-                                <CardDeck>
-                                    <Card orientation="horizontal">
-                                        <CardBody>
-                                            <h5 className="k-card-title">Card Title</h5>
-                                            <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </CardBody>
-                                    </Card>
-                                    <Card orientation="horizontal">
-                                        <CardBody>
-                                            <h5 className="k-card-title">Card Title</h5>
-                                            <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </CardBody>
-                                    </Card>
-                                    <Card orientation="horizontal">
-                                        <CardBody>
-                                            <h5 className="k-card-title">Card Title</h5>
-                                            <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </CardBody>
-                                    </Card>
-                                    <Card orientation="horizontal">
-                                        <CardBody>
-                                            <h5 className="k-card-title">Card Title</h5>
-                                            <h6 className="k-card-subtitle">Card Subtitle</h6>
-                                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        </CardBody>
-                                    </Card>
-                                </CardDeck>
-                                <Button icon="chevron-right"></Button>
-                            </div>
-                        </div>
+                <Chat showToolbar={false} showMessageBox={false}>
+                    <div className="k-timestamp">Card Deck Scrollable</div>
+                    <ChatMessageGroup>
+                        <ChatMessage className="k-only" text="Message before card deck" />
+                    </ChatMessageGroup>
+                    <div className="k-card-deck-scrollwrap">
+                        <Button icon="chevron-left"></Button>
+                        <CardDeck>
+                            <Card orientation="horizontal">
+                                <CardBody>
+                                    <h5 className="k-card-title">Card Title</h5>
+                                    <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </CardBody>
+                            </Card>
+                            <Card orientation="horizontal">
+                                <CardBody>
+                                    <h5 className="k-card-title">Card Title</h5>
+                                    <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </CardBody>
+                            </Card>
+                            <Card orientation="horizontal">
+                                <CardBody>
+                                    <h5 className="k-card-title">Card Title</h5>
+                                    <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </CardBody>
+                            </Card>
+                            <Card orientation="horizontal">
+                                <CardBody>
+                                    <h5 className="k-card-title">Card Title</h5>
+                                    <h6 className="k-card-subtitle">Card Subtitle</h6>
+                                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </CardBody>
+                            </Card>
+                        </CardDeck>
+                        <Button icon="chevron-right"></Button>
                     </div>
-                </div>
+                </Chat>
             </section>
 
         </div>
