0e582d1484665a5fa3503083368265d4bcaafe09
Iliyana Dyankova <inikolova@progress.com>
2023-07-10 12:44:16 +0300

    chore: add multiselect templates

--- packages/html/src/multiselect/index.ts
+++ packages/html/src/multiselect/index.ts
@@ -1,1 +1,7 @@
 export * from './multiselect.spec';
+export * from "./templates/multiselect-normal";
+export * from "./templates/multiselect-popup";
+export * from "./templates/multiselect-value";
+export * from "./templates/multiselect-arrow-button";
+export * from "./templates/multiselect-adaptive";
+export * from "./templates/multiselect-grouping";
