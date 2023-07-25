2a6ef6c594860fd43c9fffe1a4b996a004a5d3c4
Zhuliyan Penkov <penkov@progress.com>
2023-07-25 13:04:44 +0300

    chore: add pager templates

--- packages/html/src/pager/tests/pager-adaptive.tsx
+++ packages/html/src/pager/tests/pager-adaptive.tsx
@@ -1,4 +1,4 @@
-import { Pager } from '..';
+import { PagerNormal, PagerInput, PagerAdaptive, PagerAdaptiveInput } from "../../pager";
 
 
 const styles = `
@@ -12,14 +12,14 @@ export default () =>(
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid">
             <span>&gt;= 640 px</span>
-            <Pager size="large"/>
-            <Pager size="large" type="input"/>
+            <PagerNormal size="large"/>
+            <PagerInput size="large"/>
             <span>500 px</span>
-            <Pager size="large" adaptive info={false} refresh={false} itemsPerPage={false} style={{ width: "500px" }}/>
-            <Pager size="large" type="input" adaptive info={false} refresh={false} itemsPerPage={false} style={{ width: "500px" }}/>
+            <PagerAdaptive size="large" info={false} refresh={false} itemsPerPage={false} style={{ width: "500px" }}/>
+            <PagerAdaptiveInput size="large" info={false} refresh={false} itemsPerPage={false} style={{ width: "500px" }}/>
             <span>360 px</span>
-            <Pager size="large" adaptive info={false} refresh={false} itemsPerPage={false} pageSizes={false} style={{ width: "360px" }}/>
-            <Pager size="large" type="input" adaptive info={false} refresh={false} itemsPerPage={false} pageSizes={false} style={{ width: "360px" }}/>
+            <PagerAdaptive size="large" pageSizes={false} info={false} refresh={false} itemsPerPage={false} style={{ width: "360px" }}/>
+            <PagerAdaptiveInput size="large" pageSizes={false} info={false} refresh={false} itemsPerPage={false} style={{ width: "360px" }}/>
         </div>
     </>
 );
