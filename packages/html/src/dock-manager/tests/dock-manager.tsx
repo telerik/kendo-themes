0568176811a5c94049d340fd2bf330315cfe83ff
Emil Petrov <emil.petrov@progress.com>
2023-07-27 18:11:28 +0300

    test(dock-manager): add tests

---
+++ packages/html/src/dock-manager/tests/dock-manager.tsx
@@ -0,0 +1,133 @@
+import { Button } from "../../button";
+import { Toolbar, ToolbarSeparator } from '../../toolbar';
+
+const styles = `
+
+    #test-area {
+        max-width: 1040px;
+    }
+
+    .k-dock-manager {
+        height: 800px;
+    }
+
+`;
+
+export default () => (
+    <>
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-2">
+            <section className="k-col-span-full">
+                <div className="k-dock-manager">
+                    <Toolbar className="k-dock-manager-toolbar k-toolbar-left">
+                        <Button fillMode="flat" size="medium">File 3</Button>
+                        <ToolbarSeparator className="k-separator-horizontal"></ToolbarSeparator>
+                        <Button fillMode="flat" size="medium">File 4</Button>
+                    </Toolbar>
+                    <div className="k-dock-manager-pane-container">
+                        <div className="k-splitter k-splitter-flex k-splitter-vertical k-dock-manager-splitter">
+                            <div className="k-pane k-pane-static" style={{ flexBasis: "70%" }}>
+                                <div className="k-splitter k-splitter-flex k-splitter-horizontal k-dock-manager-splitter">
+                                    <div className="k-pane k-pane-static k-pane-pinned" style={{ flexBasis: "20%" }}>
+                                        <div className="k-pane-scrollable">
+                                            <div className="k-pane-header">
+                                                <span className="k-pane-title">Tools</span>
+                                                <div className="k-pane-actions">
+                                                    <Button fillMode="flat" icon="pin"></Button>
+                                                    <Button fillMode="flat" icon="more-vertical"></Button>
+                                                </div>
+                                            </div>
+                                            <div className="k-pane-content">Some tools.</div>
+                                        </div>
+                                    </div>
+                                    <div className="k-splitbar k-splitbar-horizontal k-splitbar-draggable-horizontal">
+                                        <div className="k-resize-handle"></div>
+                                    </div>
+                                    <div className="k-pane k-pane-static k-pane-tabbed" style={{ flexBasis: "40%" }}>
+                                        <div className="k-tabstrip k-header k-tabstrip-top" style={{ height: "100%" }}>
+                                            <div className="k-tabstrip-items-wrapper k-hstack">
+                                                <ul className="k-reset k-tabstrip-items">
+                                                    <li className="k-item k-first k-active">
+                                                        <span className="k-link">File 1</span>
+                                                        <Button fillMode="flat" icon="pin"></Button>
+                                                        <Button fillMode="flat" icon="more-vertical"></Button>
+                                                    </li>
+                                                    <li className="k-item">
+                                                        <span className="k-link">File 2</span>
+                                                        <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
+                                                        <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="k-tabstrip-content k-active">
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi suscipit porro optio veritatis obcaecati nostrum molestias dolor maxime! Molestias ducimus placeat quia possimus esse atque odio, a recusandae iste exercitationem. Sapiente fugit assumenda at explicabo quos consequuntur dicta cumque ex aspernatur, nesciunt itaque doloremque exercitationem eaque iusto. Iste non officia consequatur quis veritatis magni, quo autem dolores numquam dignissimos nemo.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="k-splitbar k-splitbar-horizontal k-splitbar-draggable-horizontal">
+                                        <div className="k-resize-handle"></div>
+                                    </div>
+                                    <div className="k-pane">
+                                        <div className="k-splitter k-splitter-flex k-splitter-vertical k-dock-manager-splitter">
+                                            <div className="k-pane k-pane-static k-pane-tabbed" style={{ flexBasis: "50%" }}>
+                                                <div className="k-tabstrip k-header k-tabstrip-top" style={{ height: "100%" }}>
+                                                    <div className="k-tabstrip-items-wrapper k-hstack">
+                                                        <ul className="k-reset k-tabstrip-items">
+                                                            <li className="k-item k-first k-active">
+                                                                <span className="k-link">File 5</span>
+                                                                <Button fillMode="flat" icon="pin"></Button>
+                                                                <Button fillMode="flat" icon="more-vertical"></Button>
+                                                            </li>
+                                                            <li className="k-item">
+                                                                <span className="k-link">File 6</span>
+                                                                <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
+                                                                <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div className="k-tabstrip-content k-active">
+                                                        Properties content.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="k-splitbar k-splitbar-vertical k-splitbar-draggable-vertical">
+                                                <div className="k-resize-handle"></div>
+                                            </div>
+                                            <div className="k-pane k-pane-pinned">
+                                                <div className="k-pane-scrollable">
+                                                    <div className="k-pane-header">
+                                                        <span className="k-pane-title">Layers</span>
+                                                        <div className="k-pane-actions">
+                                                            <Button fillMode="flat" icon="pin"></Button>
+                                                            <Button fillMode="flat" icon="more-vertical"></Button>
+                                                        </div>
+                                                    </div>
+                                                    <div className="k-pane-content">Layers content.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="k-splitbar k-splitbar-vertical k-splitbar-draggable-vertical">
+                                <div className="k-resize-handle"></div>
+                            </div>
+                            <div className="k-pane k-pane-pinned">
+                                <div className="k-pane-scrollable">
+                                    <div className="k-pane-header">
+                                        <span className="k-pane-title">Console</span>
+                                        <div className="k-pane-actions">
+                                            <Button fillMode="flat" icon="pin"></Button>
+                                            <Button fillMode="flat" icon="more-vertical"></Button>
+                                        </div>
+                                    </div>
+                                    <div className="k-pane-content">Console content.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </>
+);
