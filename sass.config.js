39d9fa91f6abd78ac745b85adf67449ed6027998
PROGRESS\grigorova <magdalena.angelova@telerik.com>
2023-07-31 10:50:58 +0300

    feat: add shadow to core

--- sass.config.js
+++ sass.config.js
@@ -7,43 +7,43 @@ module.exports = {
             cwd: 'packages/default',
             file: 'scss/all.scss',
             outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/bootstrap',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/classic',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/material',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/nouvelle',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/fluent',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css',
-            compiler: 'sass',
-            api: 'modern'
-        },
-        {
-            cwd: 'packages/core',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/utils',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
         }
+        // {
+        //     cwd: 'packages/bootstrap',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/classic',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/material',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/nouvelle',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/fluent',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css',
+        //     compiler: 'sass',
+        //     api: 'modern'
+        // },
+        // {
+        //     cwd: 'packages/core',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/utils',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // }
     ]
 };
