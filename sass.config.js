57ebc6a4dd0c8b9783441c789b46b53783700a08
PROGRESS\grigorova <magdalena.angelova@telerik.com>
2023-08-10 17:34:03 +0300

    chore: remove shadow color customization

--- sass.config.js
+++ sass.config.js
@@ -8,26 +8,26 @@ module.exports = {
             file: 'scss/all.scss',
             outFile: 'dist/all.css'
         },
-        // {
-        //     cwd: 'packages/bootstrap',
-        //     file: 'scss/all.scss',
-        //     outFile: 'dist/all.css'
-        // },
-        // {
-        //     cwd: 'packages/classic',
-        //     file: 'scss/all.scss',
-        //     outFile: 'dist/all.css'
-        // },
-        // {
-        //     cwd: 'packages/material',
-        //     file: 'scss/all.scss',
-        //     outFile: 'dist/all.css'
-        // },
-        // {
-        //     cwd: 'packages/nouvelle',
-        //     file: 'scss/all.scss',
-        //     outFile: 'dist/all.css'
-        // },
+        {
+            cwd: 'packages/bootstrap',
+            file: 'scss/all.scss',
+            outFile: 'dist/all.css'
+        },
+        {
+            cwd: 'packages/classic',
+            file: 'scss/all.scss',
+            outFile: 'dist/all.css'
+        },
+        {
+            cwd: 'packages/material',
+            file: 'scss/all.scss',
+            outFile: 'dist/all.css'
+        },
+        {
+            cwd: 'packages/nouvelle',
+            file: 'scss/all.scss',
+            outFile: 'dist/all.css'
+        },
         {
             cwd: 'packages/fluent',
             file: 'scss/all.scss',
