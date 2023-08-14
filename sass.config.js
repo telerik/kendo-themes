986c31853b45de7c1c36a57f17c2012307a24c6b
PROGRESS\grigorova <magdalena.angelova@telerik.com>
2023-08-14 09:57:24 +0300

    chore: improve generating utils with a loaded theme

--- sass.config.js
+++ sass.config.js
@@ -13,33 +13,33 @@ module.exports = {
             file: 'scss/all.scss',
             outFile: 'dist/all.css'
         },
-        {
-            cwd: 'packages/classic',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/material',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/nouvelle',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
-        {
-            cwd: 'packages/fluent',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css',
-            compiler: 'sass',
-            api: 'modern'
-        },
-        {
-            cwd: 'packages/core',
-            file: 'scss/all.scss',
-            outFile: 'dist/all.css'
-        },
+        // {
+        //     cwd: 'packages/classic',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/material',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/nouvelle',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
+        // {
+        //     cwd: 'packages/fluent',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css',
+        //     compiler: 'sass',
+        //     api: 'modern'
+        // },
+        // {
+        //     cwd: 'packages/core',
+        //     file: 'scss/all.scss',
+        //     outFile: 'dist/all.css'
+        // },
         {
             cwd: 'packages/utils',
             file: 'scss/all.scss',
