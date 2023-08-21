16911694e5184069a853aaed1838e31796cb969d
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-08-21 14:57:40 +0300

    test(spreadsheet): add tests for all tools

--- packages/html/src/spreadsheet/tests/spreadsheet-data-validation.tsx
+++ packages/html/src/spreadsheet/tests/spreadsheet-data-validation.tsx
@@ -1,63 +1,44 @@
 import { Window } from '../../window';
 import { Button } from '../../button';
-import { Checkbox } from '../../checkbox';
-import { RadioButtonLabel, RadioGroup, RadioItem } from '../../radio';
 import { Form, FormField } from '../../form';
 import { Textbox } from '../../textbox';
 import { DropdownList } from '../../dropdownlist';
+import { Checkbox } from '../../checkbox';
+import { RadioButtonLabel, RadioGroup, RadioItem } from '../../radio';
+
 
 const style = `
     .k-window {
-        width: 450px;
+        min-width: 400px;
         position: relative;
     }
 `;
 
-export default () => (
+export default () =>(
     <>
         <style>{style}</style>
-        <div id="test-area" className="k-d-grid">
-            <Window title="Data Validation" actions={[ 'x' ]} className="k-spreadsheet-window" actionButtonsAlign="start" actionButtons={
-                <>
-                    <Button themeColor="primary">Apply</Button>
-                    <Button>Cancel</Button>
-                </>
-            }>
-                <Form>
-                    <FormField
-                        label="Criteria:"
-                        editor={ <DropdownList placeholder="Number" /> }
-                    >
-                    </FormField>
+        <div id="test-area" className="k-d-grid k-grid-cols-2">
 
-                    <FormField
-                        label="Comparer:"
-                        editor={
-                            <DropdownList placeholder="greater than" />
-                        }
-                    >
-                    </FormField>
-
-                    <FormField
-                        label="Min:"
-                        editor={ <Textbox placeholder="e.g. 10"/> }
-                    >
-                    </FormField>
-
-                    <FormField
-                        editor={
+            <section>
+                <Window title="Data Validation" actions={[ 'x' ]} className="k-spreadsheet-window" actionButtonsAlign="start" actionButtons={
+                    <>
+                        <Button themeColor="primary">Apply</Button>
+                        <Button>Cancel</Button>
+                    </>
+                }>
+                    <Form>
+                        <FormField label="Cell Range" editor={ <Textbox value="Input Value" /> }/>
+                        <FormField label="Criteria" editor={ <DropdownList value="Number" /> }/>
+                        <FormField label="Comparer" editor={ <DropdownList value="Greater than" /> } />
+                        <FormField label="Min" editor={ <Textbox placeholder="Placeholder" /> } />
+                        <FormField editor={
                             <>
-                                <Checkbox checked/>
+                                <Checkbox/>
                                 <label className="k-checkbox-label">Ignore blank</label>
                             </>
-                        }
-                    >
-                    </FormField>
-
-                    <FormField
-                        label="On invalid data:"
-                        editor={
-                            <RadioGroup layout="horizontal">
+                        } />
+                        <FormField label="On invalid data" editor={
+                            <RadioGroup layout="vertical">
                                 <RadioItem>
                                     <RadioButtonLabel checked>Reject input</RadioButtonLabel>
                                 </RadioItem>
@@ -65,21 +46,30 @@ export default () => (
                                     <RadioButtonLabel>Show warning</RadioButtonLabel>
                                 </RadioItem>
                             </RadioGroup>
-                        }
-                    >
-                    </FormField>
-
-                    <FormField
-                        editor={
+                        }/>
+                        <FormField editor={
                             <>
                                 <Checkbox/>
                                 <label className="k-checkbox-label">Show hint</label>
                             </>
-                        }
-                    >
-                    </FormField>
-                </Form>
-            </Window>
+                        } />
+                    </Form>
+                </Window>
+            </section>
+
+            <section>
+                <Window title="Birth Date validaiton error" actions={[ 'x' ]} className="k-spreadsheet-window" actionButtonsAlign="start" actionButtons={
+                    <>
+                        <Button themeColor="primary">Retry</Button>
+                        <Button>Cancel</Button>
+                    </>
+                }>
+                    Birth Date should be between 1899 and 1998 year.
+                </Window>
+            </section>
+
         </div>
     </>
 );
+
+
