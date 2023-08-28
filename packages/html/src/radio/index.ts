17634b3a858ed13c694e53596fe6aea1bdbf6c53
Zhuliyan Penkov <penkov@progress.com>
2023-08-28 16:44:39 +0300

    chore(spec): add RadioGroup spec

--- packages/html/src/radio/index.ts
+++ packages/html/src/radio/index.ts
@@ -1,4 +1,5 @@
 export * from './radio.spec';
+export * from "./radio-group.spec";
 export * from "./templates/radio-normal";
 export * from "./templates/radio-label";
 export * from "./templates/radio-inside-label";
