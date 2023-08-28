5095b96d7092a5dde162988dfe45b6da644ee47a
Zhuliyan Penkov <penkov@progress.com>
2023-08-28 09:49:45 +0300

    chore(spec): add RadioItem spec

--- packages/html/src/radio/index.ts
+++ packages/html/src/radio/index.ts
@@ -1,5 +1,6 @@
 export * from './radio.spec';
 export * from "./radio-group.spec";
+export * from "./radio-item.spec";
 export * from "./templates/radio-normal";
 export * from "./templates/radio-label";
 export * from "./templates/radio-inside-label";
---
+++ packages/html/src/radio/radio-item.spec.tsx
@@ -0,0 +1,38 @@
+import { classNames } from '../misc';
+
+export const RADIOITEM_CLASSNAME = `k-radio-list-item`;
+
+const states = [];
+
+const options = {};
+
+const defaultProps = {};
+
+export type KendoRadioItemState = { [K in (typeof states)[number]]?: boolean };
+
+export const RadioItem = (
+    props: KendoRadioItemState &
+     React.HTMLAttributes<HTMLLIElement>
+) => {
+    const {
+        ...other
+    } = props;
+    return (
+        <li
+            {...other}
+            className={classNames(
+                props.className,
+                RADIOITEM_CLASSNAME,
+            )}
+        >
+            {props.children}
+        </li>
+    );
+};
+
+RadioItem.states = states;
+RadioItem.options = options;
+RadioItem.className = RADIOITEM_CLASSNAME;
+RadioItem.defaultProps = defaultProps;
+
+export default RadioItem;
