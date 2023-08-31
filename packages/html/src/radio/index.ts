58033f903a51c6a43188b706df43e231292c45a4
Zhuliyan Penkov <penkov@progress.com>
2023-08-31 15:31:00 +0300

    chore: add radio templates for label position

--- packages/html/src/radio/index.ts
+++ packages/html/src/radio/index.ts
@@ -2,5 +2,6 @@ export * from './radio.spec';
 export * from "./radio-group.spec";
 export * from "./radio-item.spec";
 export * from "./templates/radio-normal";
-export * from "./templates/radio-label";
+export * from "./templates/radio-with-label-after";
+export * from "./templates/radio-with-label-before";
 export * from "./templates/radio-inside-label";
