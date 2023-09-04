13fa5ba034f4e8c3ff5b425e360d1bad780029fd
Zhuliyan Penkov <penkov@progress.com>
2023-09-04 11:35:21 +0300

    chore: add toolbar-popup spec

--- packages/html/src/toolbar/index.ts
+++ packages/html/src/toolbar/index.ts
@@ -2,3 +2,4 @@ export * from './toolbar.spec';
 export * from './toolbar-angular.spec';
 export * from './toolbar-separator';
 export * from './toolbar-item.spec';
+export * from './toolbar-popup.spec';
