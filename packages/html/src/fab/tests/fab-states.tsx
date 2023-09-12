a68103c390c2ba56495b95aff54fa461bdff89cf
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-09-12 10:10:09 +0300

    test(fab): simplify states test

--- packages/html/src/fab/tests/fab-states.tsx
+++ packages/html/src/fab/tests/fab-states.tsx
@@ -1,186 +1,35 @@
 import { FloatingActionButton } from '../../fab';
 
+const styles = `
+    #test-area {
+        grid-template-columns: 100px repeat(6, 1fr);
+        grid-template-rows: 10px repeat(11, 50px);
+    }
+`;
 
 export default () =>(
     <>
+        <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-6">
-
             <span></span>
             <span>Normal</span>
             <span>Hover</span>
             <span>Focus</span>
             <span>Active</span>
+            <span>Selected</span>
             <span>Disabled</span>
-
-            <span>primary</span>
-            <span>
-                <FloatingActionButton icon="plus" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" disabled />
-            </span>
-
-            <span>secondary</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="secondary" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="secondary" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="secondary" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="secondary" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="secondary" disabled />
-            </span>
-
-            <span>tertiary</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="tertiary" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="tertiary" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="tertiary" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="tertiary" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="tertiary" disabled />
-            </span>
-
-            <span>info</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="info" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="info" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="info" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="info" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="info" disabled />
-            </span>
-
-            <span>success</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="success" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="success" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="success" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="success" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="success" disabled />
-            </span>
-
-            <span>warning</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="warning" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="warning" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="warning" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="warning" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="warning" disabled />
-            </span>
-
-            <span>error</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="error" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="error" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="error" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="error" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="error" disabled />
-            </span>
-
-            <span>dark</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="dark" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="dark" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="dark" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="dark" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="dark" disabled />
-            </span>
-
-            <span>light</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="light" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="light" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="light" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="light" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="light" disabled />
-            </span>
-
-            <span>inverse</span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="inverse" />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="inverse" hover />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="inverse" focus />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="inverse" active />
-            </span>
-            <span>
-                <FloatingActionButton icon="plus" themeColor="inverse" disabled />
-            </span>
+            {[ ...FloatingActionButton.options.themeColor ].map((themeColor) => (
+                <>
+                    <span style={{ alignSelf: "center" }}>{themeColor}</span>
+                    {[ 'normal', ...FloatingActionButton.states ].map((state) => (
+                        <>
+                            <span style={{ position: "relative" }}>
+                                <FloatingActionButton icon="plus" position="top-start" themeColor={themeColor} {...{ [state]: true }}></FloatingActionButton>
+                            </span>
+                        </>
+                    ))}
+                </>
+            ))}
         </div>
     </>
 );
