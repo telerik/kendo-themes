981b454f4fa41e36303137eca2535f698225e91f
Zhuliyan Penkov <penkov@progress.com>
2023-09-19 17:34:05 +0300

    chore: add coloreditor templates & update test

--- packages/html/src/coloreditor/index.ts
+++ packages/html/src/coloreditor/index.ts
@@ -1,1 +1,5 @@
 export * from './color-editor.spec';
+export * from './templates/coloreditor-normal';
+export * from './templates/coloreditor-palette';
+export * from './templates/coloreditor-group';
+export * from './templates/coloreditor-palette-group';
