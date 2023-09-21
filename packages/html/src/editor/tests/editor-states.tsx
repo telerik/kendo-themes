463e330831c5558828b815d71fba23d19212e54e
Zhuliyan Penkov <penkov@progress.com>
2023-09-21 11:03:00 +0300

    chore: add editor templates & update test

---
+++ packages/html/src/editor/tests/editor-states.tsx
@@ -0,0 +1,44 @@
+import { Editor, EditorContent, EditorToolbar, EditorNormal } from '../../editor';
+import { Button } from '../../button';
+import { ButtonGroup } from '../../button-group';
+
+
+const styles = `
+    #test-area {
+        max-width: 660px;
+    }
+    /* needed for test */
+    .k-editor-content > .ProseMirror {
+        white-space: normal;
+    }
+    .k-editor {
+        width: 550px;
+    }
+`;
+
+export default () =>(
+    <>
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-1">
+
+            {[ 'normal', ...Editor.states ].map((state) => (
+                <section>
+                    <div>{state}</div>
+                    <EditorNormal { ...{ [state]: true }} >
+                        <EditorToolbar resizable>
+                            <ButtonGroup>
+                                <Button icon="undo"></Button>
+                                <Button icon="redo"></Button>
+                            </ButtonGroup>
+                            <Button icon="more-vertical" className="k-toolbar-overflow-button" fillMode="flat" rounded={null}></Button>
+                        </EditorToolbar>
+                        <EditorContent>
+                            <p className="ProseMirror">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus mollitia obcaecati dignissimos beatae ipsam voluptatem pariatur consectetur culpa asperiores veniam?</p>
+                        </EditorContent>
+                    </EditorNormal>
+                </section>
+            ))}
+
+        </div>
+    </>
+);
