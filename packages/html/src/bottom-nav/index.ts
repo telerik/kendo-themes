2cb00ffd8350e9d4b2983a2424ea4814346af969
Iliyana Dyankova <inikolova@progress.com>
2023-09-28 14:30:22 +0300

    chore: add bottom-navigation templates

--- packages/html/src/bottom-nav/index.ts
+++ packages/html/src/bottom-nav/index.ts
@@ -1,2 +1,5 @@
 export * from './bottom-nav.spec';
 export * from './bottom-nav-item';
+export * from './templates/bottom-nav-normal';
+export * from './templates/bottom-nav-icons';
+export * from './templates/bottom-nav-links';
