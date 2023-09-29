ef78a01764f18b85ed876b833ac9a609a3565198
Iliyana Dyankova <inikolova@progress.com>
2023-09-29 11:22:34 +0300

    chore: add expansion-panel templates

--- packages/html/src/expansion-panel/tests/expansion-panel.tsx
+++ packages/html/src/expansion-panel/tests/expansion-panel.tsx
@@ -1,58 +1,29 @@
-import { ExpansionPanel } from '../../expansion-panel';
-
+import { ExpansionPanel, ExpansionPanelNormal, ExpansionPanelExpanded } from '../../expansion-panel';
 
 export default () =>(
     <>
-        <div id="test-area">
-            <ExpansionPanel title="Collapsed panel" subtitle="South America"></ExpansionPanel>
+        <div id="test-area" className="k-d-grid k-grid-cols-2">
+            <div>
+                {[ 'normal', ...ExpansionPanel.states ].map((state) => (
+                    <ExpansionPanelExpanded { ...{ [state]: true }} title={"Expanded panel - " + state} subtitle="Subtitle" />
+                ))}
 
-            <ExpansionPanel title="Expanded panel" subtitle="South America" expanded >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
+                <ExpansionPanelExpanded title="Expanded panel - hover & focus" hover focus subtitle="Subtitle" />
+            </div>
 
-            <ExpansionPanel title="Expanded Focused panel" subtitle="South America" expanded focus >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
+            <div>
+                {[ 'normal', ...ExpansionPanel.states ].map((state) => (
+                    <ExpansionPanelNormal { ...{ [state]: true }} title={"Collapsed panel - " + state} subtitle="Subtitle" />
+                ))}
 
-            <ExpansionPanel title="Collapsed panel" subtitle="South America" >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
+                <ExpansionPanelNormal title="Collapsed panel - hover & focus" hover focus subtitle="Subtitle" />
 
-            <ExpansionPanel title="Focus state" subtitle="South America" focus >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
-
-            <ExpansionPanel title="Hover state" subtitle="South America" hover >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
-
-            <ExpansionPanel title="Hover & Focus state" subtitle="South America" hover focus >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
-
-            <ExpansionPanel title="Disabled panel" subtitle="South America" disabled >
-                <span>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </span>
-            </ExpansionPanel>
-
-            <ExpansionPanel title="Content with fixed height" subtitle="South America" expanded contentStyle={ { height: "100px" } } >
-                <div style={ { height: "100%", overflow: "auto" } }>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </div>
-            </ExpansionPanel>
+                <ExpansionPanelExpanded title="Content with fixed height" subtitle="Subtitle" contentStyle={ { height: "100px" } } >
+                    <div style={ { height: "100%", overflow: "auto" } }>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
+                </ExpansionPanelExpanded>
+            </div>
         </div>
     </>
 );
