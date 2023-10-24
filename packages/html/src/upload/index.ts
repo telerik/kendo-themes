d5a16624a395396f0b44a641006adc0044f96faa
Zhuliyan Penkov <penkov@progress.com>
2023-10-24 13:56:21 +0300

    chore: add upload templates and update tests

--- packages/html/src/upload/index.ts
+++ packages/html/src/upload/index.ts
@@ -3,3 +3,6 @@ export * from './upload-dropzone';
 export * from './upload-file';
 export * from './upload-file-multiple';
 export * from './upload-file-info';
+export * from './templates/upload-chunk';
+export * from './templates/upload-batch';
+export * from './templates/upload-normal';
---
+++ packages/html/src/upload/templates/upload-batch.tsx
@@ -0,0 +1,14 @@
+import { Upload, UploadFileMultiple, UploadFileInfo } from "..";
+
+export const UploadBatch = (props) => (
+    <Upload async children={[
+        <>
+            <UploadFileMultiple fileSummary="Total: 2 files, 157.66 KB">
+                <UploadFileInfo name="Video1_File_with_Very_Very_Very_Very_Long_Name.png" size="51.23 KB"></UploadFileInfo>
+                <UploadFileInfo name="Image2.jpg" size="106.43 KB"></UploadFileInfo>
+            </UploadFileMultiple>
+        </>
+    ]}
+    {...props}>
+    </Upload>
+);
