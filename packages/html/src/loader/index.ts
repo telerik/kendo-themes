02129b0fcfaaf8e98fa94fb05a90f909cf56dec6
Zhuliyan Penkov <penkov@progress.com>
2023-11-06 17:58:48 +0200

    chore: add loader templates & update tests

--- packages/html/src/loader/index.ts
+++ packages/html/src/loader/index.ts
@@ -1,2 +1,6 @@
 export * from './loader.spec';
 export * from './loader-container.spec';
+export * from './templates/loader-normal';
+export * from './templates/loader-infinite';
+export * from './templates/loader-converging';
+export * from './templates/loader-container-normal';
