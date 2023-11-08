f6fcc8b63ab4f2a4c6679d43dc06784a125d28bf
Zhuliyan Penkov <penkov@progress.com>
2023-11-08 16:46:21 +0200

    chore: add form templates & update tests

--- packages/html/src/form/tests/form-sizes.tsx
+++ packages/html/src/form/tests/form-sizes.tsx
@@ -1,71 +1,39 @@
-import { DropdownList } from '../../dropdownlist';
-import { Textbox } from '../../textbox';
-import { Button } from '../../button';
-import { Form, FormField, Fieldset } from '../../form';
+import { Form, FormNormal, FormHorizontal } from '../../form';
 
 
-const styles = { border: '1px solid grey', padding: '10px' };
+const styles = `
+    #test-area {
+        grid-template-columns: 80px repeat(3, 1fr);
+    }
+
+    section {
+        padding: 10px;
+        border: 1px solid gray;
+    }
+`;
 
 export default () =>(
     <>
-        <div id="test-area">
-            <div>Form Sizes</div>
-            <br />
-            <div style={{ display: 'flex', gap: '20px' }}>
-                <section style={styles}>
-                    <div><strong>sm</strong></div>
-                    <br />
-                    <Form size="small"
-                        formButtons={
-                            <>
-                                <Button className="k-form-submit" themeColor="primary" size="small">Submit</Button>
-                                <Button className="k-form-clear" size="small">Clear</Button>
-                            </>
-                        }>
-                        <Fieldset legend="Shipping Address" layout="grid" cols={2} gapX={2} >
-                            <FormField colSpan="1" label="Country" editor={ <DropdownList value="France" size="small"/> } />
-                            <FormField colSpan="1" label="City" editor={ <Textbox size="small"/> } />
-                            <FormField colSpan="2" className="k-form-field-error" label="Address Line" error="Address Line is required" editor={ <Textbox size="small"/> } />
-                        </Fieldset>
-                    </Form>
-                </section>
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid">
+            <span></span>
+            <span>small</span>
+            <span>medium</span>
+            <span>large</span>
 
-                <section style={styles}>
-                    <div><strong>md</strong></div>
-                    <br />
-                    <Form size="medium"
-                        formButtons={
-                            <>
-                                <Button className="k-form-submit" themeColor="primary" size="medium">Submit</Button>
-                                <Button className="k-form-clear" size="medium">Clear</Button>
-                            </>
-                        }>
-                        <Fieldset legend="Shipping Address" layout="grid" cols={2} gapX={2} >
-                            <FormField colSpan="1" label="Country" editor={ <DropdownList value="France" size="medium"/> } />
-                            <FormField colSpan="1" label="City" editor={ <Textbox size="medium"/> } />
-                            <FormField colSpan="2" className="k-form-field-error" label="Address Line" error="Address Line is required" editor={ <Textbox size="medium"/> } />
-                        </Fieldset>
-                    </Form>
+            <span>vertical</span>
+            {Form.options.size.map((size) => (
+                <section>
+                    <FormNormal size={size}></FormNormal>
                 </section>
+            ))}
 
-                <section style={styles}>
-                    <div><strong>lg</strong></div>
-                    <br />
-                    <Form size="large"
-                        formButtons={
-                            <>
-                                <Button className="k-form-submit" themeColor="primary" size="large">Submit</Button>
-                                <Button className="k-form-clear" size="large">Clear</Button>
-                            </>
-                        }>
-                        <Fieldset legend="Shipping Address" layout="grid" cols={2} gapX={2} >
-                            <FormField colSpan="1" label="Country" editor={ <DropdownList value="France" size="large"/> } />
-                            <FormField colSpan="1" label="City" editor={ <Textbox size="large"/> } />
-                            <FormField colSpan="2" className="k-form-field-error" label="Address Line" error="Address Line is required" editor={ <Textbox size="large"/> } />
-                        </Fieldset>
-                    </Form>
+            <span>horizontal</span>
+            {Form.options.size.map((size) => (
+                <section>
+                    <FormHorizontal size={size}></FormHorizontal>
                 </section>
-            </div>
+            ))}
         </div>
     </>
 );
