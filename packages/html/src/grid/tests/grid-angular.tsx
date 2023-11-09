bb90ae0b49e592734c173be6c721411f2e0e1523
Zhuliyan Penkov <penkov@progress.com>
2023-11-09 14:42:30 +0200

    chore: use skeleton templates in components' tests

--- packages/html/src/grid/tests/grid-angular.tsx
+++ packages/html/src/grid/tests/grid-angular.tsx
@@ -4,7 +4,7 @@ import { Checkbox } from '../../checkbox';
 import { Icon } from '../../icon';
 import { Pager } from '../../pager';
 import { Popup } from '../../popup';
-import { Skeleton } from '../../skeleton';
+import { SkeletonNormal } from '../../skeleton';
 
 
 export default () =>(
@@ -71,7 +71,7 @@ export default () =>(
                                             </tr>
                                             <tr className="k-table-row k-table-alt-row k-alt">
                                                 <td className="k-table-td">4</td>
-                                                <td className="k-table-td"><Skeleton animation={false}/></td>
+                                                <td className="k-table-td"><SkeletonNormal animation={false}/></td>
                                             </tr>
                                         </tbody>
                                     </table>
