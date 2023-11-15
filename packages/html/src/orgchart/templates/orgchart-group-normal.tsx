23d978e5c93fc772a3ee0d644a6be260c41bfdcc
Zhuliyan Penkov <penkov@progress.com>
2023-11-15 19:02:43 +0200

    chore(html): extend component's helper API

--- packages/html/src/orgchart/templates/orgchart-group-normal.tsx
+++ packages/html/src/orgchart/templates/orgchart-group-normal.tsx
@@ -3,11 +3,11 @@ import { Orgchart, OrgchartNode, OrgchartGroup } from "..";
 export const OrgchartGroupNormal = (props) => (
     <Orgchart
         children={[
-            <OrgchartGroup title="Group Title" subtitle="Group SubTitle" line plus>
+            <OrgchartGroup title="Group Title" subtitle="Group SubTitle" line button>
                 <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8"></OrgchartNode>
                 <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8"></OrgchartNode>
             </OrgchartGroup>,
-            <OrgchartGroup title="Group Title" subtitle="Group SubTitle" line plus>
+            <OrgchartGroup title="Group Title" subtitle="Group SubTitle" line button buttonIcon="minus">
                 <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8"></OrgchartNode>
             </OrgchartGroup>
         ]}
--- packages/html/src/orgchart/templates/orgchart-normal.tsx
+++ packages/html/src/orgchart/templates/orgchart-normal.tsx
@@ -3,9 +3,9 @@ import { Orgchart, OrgchartNode } from "..";
 export const OrgchartNormal = (props) => (
     <Orgchart
         children={[
-            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line plus></OrgchartNode>,
-            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line plus></OrgchartNode>,
-            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line plus></OrgchartNode>
+            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line button></OrgchartNode>,
+            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line button buttonIcon="minus"></OrgchartNode>,
+            <OrgchartNode title="Card Title" subtitle="Card, Subtitle" details avatar="DK" avatarType="text" color="#0275d8" line button></OrgchartNode>
         ]}
         {...props}
     />
