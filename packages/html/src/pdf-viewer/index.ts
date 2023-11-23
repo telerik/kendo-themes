4c2f7aaed2e4cc17ac07afcee5a69ca5e847782c
Zhuliyan Penkov <penkov@progress.com>
2023-11-23 14:25:53 +0200

    chore: add pdf-viewer templates & update tests

--- packages/html/src/pdf-viewer/index.ts
+++ packages/html/src/pdf-viewer/index.ts
@@ -1,2 +1,5 @@
 export * from './pdf-viewer.spec';
 export * from './pdf-viewer-page';
+export * from './templates/pdf-viewer-normal';
+export * from './templates/pdf-viewer-blank';
+export * from './templates/pdf-viewer-with-search-panel';
