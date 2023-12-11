c12b5bf1954e30fbf0acca950029cad5b88d2c1b
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-12-11 19:24:34 +0200

    chore(html): add Timeline templates

---
+++ packages/html/src/timeline/templates/timeline-normal.tsx
@@ -0,0 +1,15 @@
+import { Timeline, TimelineFlagWrap, VerticalTimelineEvent } from "../../timeline";
+
+export const TimelineNormal = (props) => (
+    <Timeline
+        children={
+            <ul>
+                <TimelineFlagWrap year="2010"/>
+                <VerticalTimelineEvent date="May 11, 2010"></VerticalTimelineEvent>
+                <TimelineFlagWrap year="2011" />
+                <VerticalTimelineEvent date="Oct 11, 2011"></VerticalTimelineEvent>
+            </ul>
+        }
+        {...props}>
+    </Timeline>
+);
