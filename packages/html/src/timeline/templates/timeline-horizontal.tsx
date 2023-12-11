147163dac6f15ee533d510bac4e7c1de529aca9e
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-12-11 19:24:34 +0200

    chore(html): add Timeline templates

---
+++ packages/html/src/timeline/templates/timeline-horizontal.tsx
@@ -0,0 +1,35 @@
+import { Timeline, TimelineTrackWrap, TimelineTrackItem, TimelineFlag, TimelineDateWrap, TimelineCircle, TimelineEventList, HorizontalTimelineEvent } from "../../timeline";
+
+export const TimelineHorizontal = (props) => (
+    <Timeline
+        orientation="horizontal"
+        children={[
+            <>
+                <TimelineTrackWrap>
+                    <TimelineTrackItem className="k-timeline-flag-wrap">
+                        <TimelineFlag content="2015"></TimelineFlag>
+                    </TimelineTrackItem>
+                    <TimelineTrackItem>
+                        <TimelineDateWrap date="Jan 14, 2015"></TimelineDateWrap>
+                        <TimelineCircle></TimelineCircle>
+                    </TimelineTrackItem>
+                    <TimelineTrackItem>
+                        <TimelineDateWrap date="Jun 1, 2015"></TimelineDateWrap>
+                        <TimelineCircle></TimelineCircle>
+                    </TimelineTrackItem>
+                    <TimelineTrackItem>
+                        <TimelineDateWrap date="Sep 29, 2015"></TimelineDateWrap>
+                        <TimelineCircle></TimelineCircle>
+                    </TimelineTrackItem>
+                    <TimelineTrackItem className="k-timeline-flag-wrap">
+                        <TimelineFlag content="2016"></TimelineFlag>
+                    </TimelineTrackItem>
+                </TimelineTrackWrap>
+                <TimelineEventList>
+                    <HorizontalTimelineEvent />
+                </TimelineEventList>
+            </>
+        ]}
+        {...props}>
+    </Timeline>
+);
