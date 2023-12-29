5e63d20f09cca072be110923c0126bc5502fdfb2
Zhuliyan Penkov <penkov@progress.com>
2023-12-29 18:42:45 +0200

    chore(html): add prefix/suffix support to maskedtextbox

--- packages/html/src/maskedtextbox/index.ts
+++ packages/html/src/maskedtextbox/index.ts
@@ -1,2 +1,4 @@
 export * from './maskedtextbox.spec';
 export * from "./templates/maskedtextbox-normal";
+export * from "./templates/maskedtextbox-prefix";
+export * from "./templates/maskedtextbox-suffix";
---
+++ packages/html/src/maskedtextbox/templates/maskedtextbox-suffix.tsx
@@ -0,0 +1,13 @@
+import Textbox from "../maskedtextbox.spec";
+import { Icon } from '../../icon';
+
+export const MaskedTextboxSuffix = (props) => (
+    <Textbox
+        suffix={
+            <>
+                <Icon icon="lock" />
+            </>
+        }
+        {...props}
+    />
+);
