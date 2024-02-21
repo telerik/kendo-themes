6915acef581c77ccd5a7ec65ae40f2122c51cebf
Iliyana Dyankova <inikolova@progress.com>
2024-02-21 17:04:09 +0200

    feat(default): integrate spacing system

--- packages/html/src/action-buttons/tests/action-buttons.tsx
+++ packages/html/src/action-buttons/tests/action-buttons.tsx
@@ -6,13 +6,21 @@ const styles = `
     .k-actions {
         outline: 1px dotted;
     }
+
+    .k-body {
+        --k-elevation-4: 2px 2px red, 4px 4px green, 8px 8px blue, 12px 12px black;
+
+        --kendo-spacing-1: 10px;
+        --kendo-spacing-2: 20px;
+        --kendo-spacing-25: 200px;
+    }
 `;
 
 export default () =>(
     <>
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-2">
-            <span>start</span>
+            <span className="k-elevation-4">start</span>
             <span>end</span>
             <div>
                 <ActionButtons alignment="start">
