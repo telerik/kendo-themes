0d3e12267f46077acc21cf43d10721f9760b25b9
Zhuliyan Penkov <penkov@progress.com>
2024-02-26 13:20:49 +0200

    chore(html): add focus state to timeline-track-item

--- packages/html/src/timeline/timeline-track-item.spec.tsx
+++ packages/html/src/timeline/timeline-track-item.spec.tsx
@@ -1,18 +1,26 @@
-import { classNames } from '../misc';
+import { States, classNames, stateClassNames } from '../misc';
 
 export const TIMELINETRACKITEM_CLASSNAME = `k-timeline-track-item`;
+
+const states = [
+    States.focus,
+];
 
 export type KendoTimelineTrackItemOptions = {
     children?: JSX.Element | JSX.Element[];
     content?: string;
 };
 
+export type KendoTimelineTrackItemState = { [K in (typeof states)[number]]?: boolean };
+
 export const TimelineTrackItem = (
     props: KendoTimelineTrackItemOptions &
+        KendoTimelineTrackItemState &
         React.HTMLAttributes<HTMLLIElement>
 ) => {
     const {
         content,
+        focus,
         ...other
     } = props;
 
@@ -22,7 +30,10 @@ export const TimelineTrackItem = (
             {...other}
             className={classNames(
                 props.className,
-                TIMELINETRACKITEM_CLASSNAME
+                TIMELINETRACKITEM_CLASSNAME,
+                stateClassNames(TIMELINETRACKITEM_CLASSNAME, {
+                    focus
+                }),
             )}>
             {content}
             {props.children}
