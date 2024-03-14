fec9dbc2e820c76593dd41baeed0dde5df5bb344
Emil Petrov <epetrov@progress.com>
2024-03-14 16:57:22 +0200

    chore(html): update calendar templates

--- packages/html/src/calendar/templates/calendar-decade.tsx
+++ packages/html/src/calendar/templates/calendar-decade.tsx
@@ -1,27 +1,27 @@
-import { Calendar, CalendarCell } from "..";
+import { Calendar, CalendarTableRow, CalendarCell } from "..";
 
 export const CalendarDecade = (props: any) => (
     <Calendar calendarView="decade" calendarHeaderText="2020 - 2029"
         children={
             <>
-                <tr className="k-calendar-tr">
-                    <CalendarCell className="k-out-of-range" />
+                <CalendarTableRow>
+                    <CalendarCell empty />
                     <CalendarCell text="2020" />
                     <CalendarCell text="2021" today />
                     <CalendarCell text="2022" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="2023" />
                     <CalendarCell text="2024" />
                     <CalendarCell text="2025" />
                     <CalendarCell text="2026" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="2027" />
                     <CalendarCell text="2028" />
                     <CalendarCell text="2029" />
-                    <CalendarCell className="k-out-of-range" />
-                </tr>
+                    <CalendarCell empty />
+                </CalendarTableRow>
             </>
         }
         {...props}
