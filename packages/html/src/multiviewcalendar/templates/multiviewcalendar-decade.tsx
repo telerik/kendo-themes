977070875ac388802575d475e75ce47c09aefcb8
Emil Petrov <epetrov@progress.com>
2024-03-14 17:22:27 +0200

    chore: update multiview calendar templates

--- packages/html/src/multiviewcalendar/templates/multiviewcalendar-decade.tsx
+++ packages/html/src/multiviewcalendar/templates/multiviewcalendar-decade.tsx
@@ -1,28 +1,29 @@
 import { MultiViewCalendar } from "..";
 import { CalendarCell } from "../..";
+import { CalendarTableRow } from "../..";
 
 export const MultiViewCalendarDecade = (props: any) => (
     <MultiViewCalendar calendarView="decade" calendarHeaderText="2020 - 2029"
         children={
             <>
-                <tr className="k-calendar-tr">
-                    <CalendarCell className="k-out-of-range" />
+                <CalendarTableRow>
+                    <CalendarCell empty />
                     <CalendarCell text="2020" />
                     <CalendarCell text="2021" today />
                     <CalendarCell text="2022" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="2023" />
                     <CalendarCell text="2024" />
                     <CalendarCell text="2025" />
                     <CalendarCell text="2026" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="2027" />
                     <CalendarCell text="2028" />
                     <CalendarCell text="2029" />
-                    <CalendarCell className="k-out-of-range" />
-                </tr>
+                    <CalendarCell empty />
+                </CalendarTableRow>
             </>
         }
         {...props}
--- packages/html/src/multiviewcalendar/templates/multiviewcalendar-year.tsx
+++ packages/html/src/multiviewcalendar/templates/multiviewcalendar-year.tsx
@@ -1,28 +1,29 @@
 import { MultiViewCalendar } from "..";
 import { CalendarCell } from "../..";
+import { CalendarTableRow } from "../..";
 
 export const MultiViewCalendarYear = (props: any) => (
     <MultiViewCalendar calendarView="year" calendarHeaderText="2019"
         children={
             <>
-                <tr className="k-calendar-tr">
+                <CalendarTableRow>
                     <CalendarCell text="Jan" />
                     <CalendarCell text="Feb" />
                     <CalendarCell text="Mar" />
                     <CalendarCell text="Apr" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="May" />
                     <CalendarCell text="Jun" />
                     <CalendarCell text="Jul" />
                     <CalendarCell text="Au" />
-                </tr>
-                <tr className="k-calendar-tr">
+                </CalendarTableRow>
+                <CalendarTableRow>
                     <CalendarCell text="Sep" />
                     <CalendarCell text="Oct" today />
                     <CalendarCell text="Nov" />
                     <CalendarCell text="Dec" />
-                </tr>
+                </CalendarTableRow>
             </>
         }
         {...props}
