cec866bef654b05026cf19b3ffe3836ede74b3ae
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2024-03-15 01:11:22 +0200

    chore(html): add drawer templates

--- packages/html/src/drawer/index.ts
+++ packages/html/src/drawer/index.ts
@@ -3,3 +3,9 @@ export * from './drawer-content.spec';
 export * from './drawer-item.spec';
 export * from './drawer-item-separator';
 
+export * from './templates/drawer-normal';
+export * from './templates/drawer-mini';
+export * from './templates/drawer-with-hierarchy';
+export * from './templates/drawer-with-overlay';
+
+
--- packages/html/src/drawer/tests/drawer-hierarchical-item-levels.tsx
+++ packages/html/src/drawer/tests/drawer-hierarchical-item-levels.tsx
@@ -1,51 +1,21 @@
-import { Drawer, DrawerItem } from '..';
+import { DrawerWithHierarchy } from '..';
 
+const styles = `
+    #test-area .k-drawer-content {
+        padding: 0 20px;
+    }
+`;
 
 export default () =>(
     <>
-        <div id="test-area" className="k-d-grid k-grid-cols-4">
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-1">
 
             <span>hierarchical items</span>
-            <span>hierarchical items - RTL</span>
-            <span></span>
             <span></span>
 
             <section>
-                <Drawer expanded items={
-                    <>
-                        <DrawerItem icon="info-circle" text="Getting Started" hasChildren />
-                        <DrawerItem icon="question-circle" text="About Kendo UI" level={1} hover />
-                        <DrawerItem icon="palette" text="Supported Themes" level={2} selected />
-                        <DrawerItem icon="search" text="Overview" hasChildren expanded />
-                        <DrawerItem icon="star" text="Most Popular Widgets" hasChildren />
-                        <DrawerItem icon="palette" text="Supported Themes" level={1} />
-                        <DrawerItem icon="button" text="Button Package" level={1} />
-                        <DrawerItem icon="button" text="Split Button" level={2} />
-                        <DrawerItem icon="question-circle" text="API" level={3} />
-                        <DrawerItem icon="button" text="Chip List" level={2} />
-                        <DrawerItem icon="question-circle" text="API" level={3} />
-                    </>
-                }>
-                </Drawer>
-            </section>
-
-            <section>
-                <Drawer dir="rtl" expanded items={
-                    <>
-                        <DrawerItem icon="info-circle" text="Getting Started" hasChildren />
-                        <DrawerItem icon="question-circle" text="About Kendo UI" level={1} hover />
-                        <DrawerItem icon="palette" text="Supported Themes" level={2} selected />
-                        <DrawerItem icon="search" text="Overview" hasChildren expanded />
-                        <DrawerItem icon="star" text="Most Popular Widgets" hasChildren />
-                        <DrawerItem icon="palette" text="Supported Themes" level={1} />
-                        <DrawerItem icon="button" text="Button Package" level={1} />
-                        <DrawerItem icon="button" text="Split Button" level={2} />
-                        <DrawerItem icon="question-circle" text="API" level={3} />
-                        <DrawerItem icon="button" text="Chip List" level={2} />
-                        <DrawerItem icon="question-circle" text="API" level={3} />
-                    </>
-                }>
-                </Drawer>
+                <DrawerWithHierarchy />
             </section>
 
         </div>
