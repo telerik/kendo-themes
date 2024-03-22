d67f6588b05e7fb143ae98345c224e61f826ace1
Zhuliyan Penkov <penkov@progress.com>
2024-03-22 15:22:10 +0200

    chore(html): add showClearButton option for dateinput

--- packages/html/src/dateinput/tests/dateinput-outline.tsx
+++ packages/html/src/dateinput/tests/dateinput-outline.tsx
@@ -16,29 +16,29 @@ export default () =>(
             <span>DateInput Outline RTL</span>
 
             <div>
-                <DateInputNormal showSpinButton={true} fillMode="outline" placeholder="placeholder..." />
+                <DateInputNormal showClearButton showSpinButton fillMode="outline" placeholder="placeholder..." />
             </div>
             <div dir="rtl">
-                <DateInputNormal showSpinButton={true} fillMode="outline" placeholder="placeholder..." />
+                <DateInputNormal showClearButton showSpinButton fillMode="outline" placeholder="placeholder..." />
             </div>
 
             {[ 'normal', ...DateInput.states ].map((state) => (
                 <>
                     <div>
-                        <DateInputNormal { ...{ [state]: true }} showSpinButton={true} fillMode="outline" value={state} />
+                        <DateInputNormal showClearButton { ...{ [state]: true }} showSpinButton fillMode="outline" value={state} />
                     </div>
 
                     <div dir="rtl">
-                        <DateInputNormal { ...{ [state]: true }} showSpinButton={true} fillMode="outline" value={state} />
+                        <DateInputNormal showClearButton { ...{ [state]: true }} showSpinButton fillMode="outline" value={state} />
                     </div>
                 </>
             ))}
 
             <div>
-                <DateInputNormal showSpinButton={true} value="invalid focus" fillMode="outline" invalid focus />
+                <DateInputNormal showClearButton showSpinButton value="invalid focus" fillMode="outline" invalid focus />
             </div>
             <div dir="rtl">
-                <DateInputNormal showSpinButton={true} value="invalid focus" fillMode="outline" invalid focus />
+                <DateInputNormal showClearButton showSpinButton value="invalid focus" fillMode="outline" invalid focus />
             </div>
         </div>
     </>
