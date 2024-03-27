9a2d1bd31036b2834e4bb337368932d4795ac712
Zhuliyan Penkov <penkov@progress.com>
2024-03-27 14:02:39 +0200

    chore(html): add upload with action buttons template

---
+++ packages/html/src/upload/templates/upload-with-action-buttons.tsx
@@ -0,0 +1,13 @@
+import { UploadFile, Upload } from "..";
+
+export const UploadWithActionButtons = (props) => (
+    <Upload async actions
+        children={
+            <>
+                <UploadFile name="Image_With_Very_Very_Very_Very_Very_Long_Name1.png" icon="file-image" size="51.23 KB"></UploadFile>
+                <UploadFile name="Image2.jpg" icon="file-image" size="106.43 KB"></UploadFile>
+            </>
+        }
+        {...props}>
+    </Upload>
+);
