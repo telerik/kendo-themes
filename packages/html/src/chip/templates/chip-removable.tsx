0e9551d4bdfbbcb0cecb8244fb20d231211c1c54
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2024-04-03 16:06:23 +0300

    chore(html): set default text for chip templates

--- packages/html/src/chip/templates/chip-removable.tsx
+++ packages/html/src/chip/templates/chip-removable.tsx
@@ -1,4 +1,4 @@
 import Chip from "../chip.spec";
 import { ChipAction } from "../chip-action";
 
-export const ChipRemovable = (props: any) => <Chip {...props} actions={ <ChipAction type="remove"/>} />;
+export const ChipRemovable = (props: any) => <Chip text="Removable" {...props} actions={ <ChipAction type="remove" />} />;
