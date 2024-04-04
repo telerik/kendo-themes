96343745680fd7ab8b4dbdfac353e3e498f5f98a
Zhuliyan Penkov <penkov@progress.com>
2024-04-04 17:40:08 +0300

    chore(html): add skeleton container animation in test

--- packages/html/src/skeleton/tests/skeleton.tsx
+++ packages/html/src/skeleton/tests/skeleton.tsx
@@ -19,15 +19,13 @@ export default () => (
             <SkeletonCircle style={{ width: '50px', height: '50px' }} />
             <SkeletonRectangle style={{ width: '300px', height: '200px' }} />
 
-            <div className="k-skeleton-container k-d-grid">
-                <SkeletonCircle style={{ gridColumn: "1 / 3", gridRow: "1 / 3" }} />
-                <SkeletonNormal style={{ gridColumn: "3 / 10", gridRow: "1 / 2" }} />
-                <SkeletonNormal style={{ gridColumn: "3 / 7", gridRow: "2 / 3" }} />
-
-                <SkeletonRectangle style={{ gridColumn: "1 / 11", gridRow: "3 / 8" }} />
-
-                <SkeletonNormal style={{ gridColumn: "1 / 11", gridRow: "9 / 10" }} />
-                <SkeletonNormal style={{ gridColumn: "1 / 11", gridRow: "10 / 10" }} />
+            <div className="k-skeleton-container k-d-grid k-skeleton-pulse">
+                <SkeletonCircle animation={false} style={{ gridColumn: "1 / 3", gridRow: "1 / 3" }} />
+                <SkeletonNormal animation={false} style={{ gridColumn: "3 / 10", gridRow: "1 / 2" }} />
+                <SkeletonNormal animation={false} style={{ gridColumn: "3 / 7", gridRow: "2 / 3" }} />
+                <SkeletonRectangle animation={false} style={{ gridColumn: "1 / 11", gridRow: "3 / 8" }} />
+                <SkeletonNormal animation={false} style={{ gridColumn: "1 / 11", gridRow: "9 / 10" }} />
+                <SkeletonNormal animation={false} style={{ gridColumn: "1 / 11", gridRow: "10 / 10" }} />
             </div>
 
         </div>
