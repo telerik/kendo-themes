442f7b10583a4d7c021a46364d69cea6b816732e
Zhuliyan Penkov <penkov@progress.com>
2024-04-05 11:14:11 +0300

    chore(html): update pager spec and tests

--- packages/html/src/pager/tests/pager.tsx
+++ packages/html/src/pager/tests/pager.tsx
@@ -6,28 +6,28 @@ export default () =>(
         <div id="test-area" className="k-d-grid">
 
             <span>LTR</span>
-            {[ 'normal', ...Pager.states ].map((state, index) => (
-                index % 2 === 0 ?
+            {[ "normal", ...Pager.states ].map((state) => (
+                <>
                     <section>
                         <PagerNormal { ...{ [state]: true }} />
                     </section>
-                    :
                     <section>
                         <PagerInput { ...{ [state]: true }} />
                     </section>
+                </>
             ))}
 
 
             <span>RTL</span>
-            {[ 'normal', ...Pager.states ].map((state, index) => (
-                index % 2 === 0 ?
+            {[ "normal", ...Pager.states ].map((state) => (
+                <>
                     <section>
                         <PagerNormal dir="rtl" { ...{ [state]: true }} />
                     </section>
-                    :
                     <section>
                         <PagerInput dir="rtl" { ...{ [state]: true }} />
                     </section>
+                </>
             ))}
 
         </div>
