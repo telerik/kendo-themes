fc977db7a5e871993cbb1f840ef4abc9c5b30abc
Zhuliyan Penkov <penkov@progress.com>
2024-04-29 12:38:34 +0300

    chore: update mediaplayer test

--- packages/html/src/mediaplayer/tests/mediaplayer.tsx
+++ packages/html/src/mediaplayer/tests/mediaplayer.tsx
@@ -1,7 +1,4 @@
-import { Button } from '../../button';
-import { DropdownList } from '../../dropdownlist';
-import { Toolbar } from '../../toolbar';
-import { SliderNormal } from '../../slider';
+import { MediaPlayerNormal } from '../templates/mediaplayer-normal';
 
 
 const styles = `
@@ -18,31 +15,7 @@ export default () =>(
     <>
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid">
-
-            <div className="k-mediaplayer k-widget">
-                <div className="k-mediaplayer-titlebar">Media player title</div>
-                <div className="k-mediaplayer-toolbar-wrap">
-                    <SliderNormal className="k-mediaplayer-seekbar" showButtons={false} showTicks={false} />
-                    <Toolbar className="k-mediaplayer-toolbar">
-                        <Button fillMode="flat" icon="play" className="k-play-button"></Button>
-                        <div className="k-mediaplayer-currenttime-wrap">
-                            <span className="k-mediaplayer-currenttime">00:18</span> /
-                            <span className="k-mediaplayer-duration">00:55</span>
-                        </div>
-                        <div className="k-spacer"></div>
-                        <Button fillMode="flat" icon="volume-up" className="k-volume-button"></Button>
-                        <div className="k-mediaplayer-volume-wrap">
-                            <SliderNormal className="k-mediaplayer-volume" showButtons={false} showTicks={false} />
-                        </div>
-                        <div className="k-mediaplayer-quality-wrap">
-                            <DropdownList value="360p" arrowIconName="hd" className="k-mediaplayer-quality" />
-                        </div>
-                        <Button fillMode="flat" icon="fullscreen" className="k-fullscreen-button"></Button>
-                    </Toolbar>
-                </div>
-                <div className="k-mediaplayer-overlay"></div>
-            </div>
-
+            <MediaPlayerNormal />
         </div>
     </>
 );
