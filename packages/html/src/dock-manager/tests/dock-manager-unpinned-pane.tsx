01f75607095b09087fde15b5985b449588088b74
Zhuliyan Penkov <penkov@progress.com>
2024-04-30 11:42:43 +0300

    chore: update dock-manager tests

--- packages/html/src/dock-manager/tests/dock-manager-unpinned-pane.tsx
+++ packages/html/src/dock-manager/tests/dock-manager-unpinned-pane.tsx
@@ -1,9 +1,7 @@
-import { Button } from "../../button";
-import { TabStripNormal, TabStripContent, TabStripItem } from "../../tabstrip";
-import { Toolbar } from '../../toolbar';
+import { Button, TabStripNormal, TabStripContent, TabStripItem, SplitterPane } from "../..";
+import { PaneScrollable, DockManagerSplitter, DockManagerUnpinnedPane } from "../";
 
 const styles = `
-
     #test-area {
         max-width: 1040px;
     }
@@ -11,120 +9,70 @@ const styles = `
     .k-dock-manager {
         height: 800px;
     }
-
 `;
 
 export default () => (
     <>
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid">
-            <div className="k-dock-manager">
-                <Toolbar className="k-dock-manager-toolbar k-toolbar-left">
-                    <Button fillMode="flat" size="small" selected>Tools</Button>
-                </Toolbar>
-                <div className="k-dock-manager-unpinned-container" style={{ left: "43px" }}>
-                    <div className="k-pane k-pane-flex k-pane-unpinned">
-                        <div className="k-pane-scrollable">
-                            <div className="k-pane-header">
-                                <span className="k-pane-title">Tools</span>
-                                <div className="k-pane-actions">
-                                    <Button fillMode="flat" icon="unpin"></Button>
-                                    <Button fillMode="flat" icon="more-vertical"></Button>
-                                </div>
-                            </div>
-                            <div className="k-pane-content">Some tools.</div>
-                        </div>
-                    </div>
-                    <div className="k-splitbar k-splitbar-horizontal k-splitbar-draggable-horizontal">
-                        <div className="k-resize-handle"></div>
-                    </div>
-                </div>
-                <div className="k-dock-manager-pane-container">
-                    <div className="k-splitter k-splitter-flex k-splitter-vertical k-dock-manager-splitter">
-                        <div className="k-pane k-pane-static" style={{ flexBasis: "70%" }}>
-                            <div className="k-splitter k-splitter-flex k-splitter-horizontal k-dock-manager-splitter">
-                                <div className="k-pane k-pane-static k-pane-tabbed" style={{ flexBasis: "40%" }}>
-                                    <TabStripNormal style={{ height: "100%" }}
-                                        tabStripItems={
-                                            <>
-                                                <TabStripItem first value="File 1" active>
-                                                    <Button fillMode="flat" icon="pin"></Button>
-                                                    <Button fillMode="flat" icon="more-vertical"></Button>
-                                                </TabStripItem>
-                                                <TabStripItem value="File 2">
-                                                    <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
-                                                    <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
-                                                </TabStripItem>
-                                            </>
-                                        }
-                                    >
-                                        <TabStripContent active>
+            <DockManagerUnpinnedPane>
+                <DockManagerSplitter orientation="vertical">
+                    <SplitterPane flexBasis="70%">
+                        <DockManagerSplitter>
+                            <SplitterPane tabbed flexBasis="40%">
+                                <TabStripNormal style={{ height: "100%" }}
+                                    tabStripItems={
+                                        <>
+                                            <TabStripItem first value="File 1" active>
+                                                <Button fillMode="flat" icon="pin"></Button>
+                                                <Button fillMode="flat" icon="more-vertical"></Button>
+                                            </TabStripItem>
+                                            <TabStripItem value="File 2">
+                                                <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
+                                                <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
+                                            </TabStripItem>
+                                        </>
+                                    }
+                                >
+                                    <TabStripContent active>
                                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi suscipit porro optio veritatis obcaecati nostrum molestias dolor maxime! Molestias ducimus placeat quia possimus esse atque odio, a recusandae iste exercitationem. Sapiente fugit assumenda at explicabo quos consequuntur dicta cumque ex aspernatur, nesciunt itaque doloremque exercitationem eaque iusto. Iste non officia consequatur quis veritatis magni, quo autem dolores numquam dignissimos nemo.
-                                        </TabStripContent>
-                                    </TabStripNormal>
-                                </div>
-                                <div className="k-splitbar k-splitbar-horizontal k-splitbar-draggable-horizontal">
-                                    <div className="k-resize-handle"></div>
-                                </div>
-                                <div className="k-pane">
-                                    <div className="k-splitter k-splitter-flex k-splitter-vertical k-dock-manager-splitter">
-                                        <div className="k-pane k-pane-static k-pane-tabbed" style={{ flexBasis: "50%" }}>
-                                            <TabStripNormal style={{ height: "100%" }}
-                                                tabStripItems={
-                                                    <>
-                                                        <TabStripItem first value="File 5" active>
-                                                            <Button fillMode="flat" icon="pin"></Button>
-                                                            <Button fillMode="flat" icon="more-vertical"></Button>
-                                                        </TabStripItem>
-                                                        <TabStripItem value="File 6">
-                                                            <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
-                                                            <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
-                                                        </TabStripItem>
-                                                    </>
-                                                }
-                                            >
-                                                <TabStripContent active>
-                                                    Properties content.
-                                                </TabStripContent>
-                                            </TabStripNormal>
-                                        </div>
-                                        <div className="k-splitbar k-splitbar-vertical k-splitbar-draggable-vertical">
-                                            <div className="k-resize-handle"></div>
-                                        </div>
-                                        <div className="k-pane k-pane-pinned">
-                                            <div className="k-pane-scrollable">
-                                                <div className="k-pane-header">
-                                                    <span className="k-pane-title">Layers</span>
-                                                    <div className="k-pane-actions">
+                                    </TabStripContent>
+                                </TabStripNormal>
+                            </SplitterPane>
+                            <SplitterPane>
+                                <DockManagerSplitter orientation="vertical">
+                                    <SplitterPane tabbed flexBasis="50%">
+                                        <TabStripNormal style={{ height: "100%" }}
+                                            tabStripItems={
+                                                <>
+                                                    <TabStripItem first value="File 5" active>
                                                         <Button fillMode="flat" icon="pin"></Button>
                                                         <Button fillMode="flat" icon="more-vertical"></Button>
-                                                    </div>
-                                                </div>
-                                                <div className="k-pane-content">Layers content.</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="k-splitbar k-splitbar-vertical k-splitbar-draggable-vertical">
-                            <div className="k-resize-handle"></div>
-                        </div>
-                        <div className="k-pane k-pane-pinned">
-                            <div className="k-pane-scrollable">
-                                <div className="k-pane-header">
-                                    <span className="k-pane-title">Console</span>
-                                    <div className="k-pane-actions">
-                                        <Button fillMode="flat" icon="pin"></Button>
-                                        <Button fillMode="flat" icon="more-vertical"></Button>
-                                    </div>
-                                </div>
-                                <div className="k-pane-content">Console content.</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                                                    </TabStripItem>
+                                                    <TabStripItem value="File 6">
+                                                        <Button fillMode="flat" themeColor="primary" icon="pin"></Button>
+                                                        <Button fillMode="flat" themeColor="primary" icon="more-vertical"></Button>
+                                                    </TabStripItem>
+                                                </>
+                                            }
+                                        >
+                                            <TabStripContent active>
+                                                    Properties content.
+                                            </TabStripContent>
+                                        </TabStripNormal>
+                                    </SplitterPane>
+                                    <SplitterPane pinned>
+                                        <PaneScrollable title="Layers">Layers content.</PaneScrollable>
+                                    </SplitterPane>
+                                </DockManagerSplitter>
+                            </SplitterPane>
+                        </DockManagerSplitter>
+                    </SplitterPane>
+                    <SplitterPane pinned>
+                        <PaneScrollable title="Console">Console content.</PaneScrollable>
+                    </SplitterPane>
+                </DockManagerSplitter>
+            </DockManagerUnpinnedPane>
         </div>
     </>
 );
