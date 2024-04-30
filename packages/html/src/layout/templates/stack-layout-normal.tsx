019bc13c96808dc18427afc0b8d86218dbea15e2
Zhuliyan Penkov <penkov@progress.com>
2024-04-30 16:21:05 +0300

    chore: add grid/stack layout templates

---
+++ packages/html/src/layout/templates/stack-layout-normal.tsx
@@ -0,0 +1,15 @@
+import { Layout } from "..";
+
+export const StackLayoutNormal = ({ alignItems = "start", justifyContent = "start", ...other }: any) => (
+    <Layout type="stack" orientation="horizontal"
+        alignItems={alignItems}
+        justifyContent={justifyContent}
+        children={
+            <>
+                <div className="box">Box</div>
+                <div className="box">Box</div>
+                <div className="box">Box</div>
+            </>
+        }
+        {...other} />
+);
