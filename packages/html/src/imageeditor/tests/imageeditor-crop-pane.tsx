7fc390a602ccc7e2d64419604d1b81d60142c6a1
Zhuliyan Penkov <penkov@progress.com>
2024-04-30 14:23:45 +0300

    chore: update imageeditor tests

--- packages/html/src/imageeditor/tests/imageeditor-crop-pane.tsx
+++ packages/html/src/imageeditor/tests/imageeditor-crop-pane.tsx
@@ -1,10 +1,4 @@
-import { Button } from '../../button';
-import { Checkbox } from '../../checkbox';
-import { DropdownList } from '../../dropdownlist';
-import { NumericTextbox } from '../../numerictextbox';
-import { Toolbar, ToolbarSeparator } from '../../toolbar';
-import { ButtonGroup } from '../../button-group';
-import { FormNormal, FormField, Fieldset } from '../../form';
+import { ImageEditorCrop } from '..';
 
 
 export default () =>(
@@ -12,94 +6,22 @@ export default () =>(
         <div id="test-area" className="k-d-grid k-grid-cols-1">
 
             <h4>Image Editor Crop Pane</h4>
-            <div className="k-widget k-imageeditor">
 
-                {/* Header */}
-                <div className="k-imageeditor-header">
-                    <Toolbar className="k-imageeditor-toolbar">
-                        <Button icon="upload"></Button>
-                        <Button icon="download"></Button>
-                        <ToolbarSeparator></ToolbarSeparator>
-                        <Button icon="undo"></Button>
-                        <Button icon="redo"></Button>
-                        <ToolbarSeparator></ToolbarSeparator>
-                        <Button icon="crop"></Button>
-                        <Button icon="image-resize"></Button>
-                        <Button icon="zoom-in"></Button>
-                        <Button icon="zoom-out"></Button>
-                        <div>
-                            <DropdownList value="Zoom options" />
-                        </div>
-                    </Toolbar>
-                </div>
-
-                {/* Content */}
-                <div className="k-imageeditor-content" style={{ height: "482px" }}>
-
-                    {/* Canvas */}
-                    <div className="k-imageeditor-canvas-container">
-                        <div className="k-imageeditor-canvas">
-                            <canvas width="700" height="400" style={{ backgroundImage: "url('/packages/html/assets/sofia.jpg')", backgroundPosition: "50px -350px" }}></canvas>
-                            <div className="k-imageeditor-crop-overlay">
-                                <div className="k-imageeditor-crop" style={{ width: "400px", height: "400px", backgroundImage: "url('/packages/html/assets/sofia.jpg')", backgroundClip: "content-box", backgroundPosition: "748px -350px" }}>
-                                    <span className="k-resize-handle k-resize-nw"></span>
-                                    <span className="k-resize-handle k-resize-n"></span>
-                                    <span className="k-resize-handle k-resize-ne"></span>
-                                    <span className="k-resize-handle k-resize-w"></span>
-                                    <span className="k-resize-handle k-resize-e"></span>
-                                    <span className="k-resize-handle k-resize-sw"></span>
-                                    <span className="k-resize-handle k-resize-s"></span>
-                                    <span className="k-resize-handle k-resize-se"></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Crop Pane */}
-                    <div className="k-imageeditor-action-pane">
-                        <FormNormal tag="div" className="k-imageeditor-pane-form" formButtons={
-                            <>
-                                <Button themeColor="primary">Confirm</Button>
-                                <Button>Cancel</Button>
-                            </>
-                        }>
-                            <Fieldset layout="grid" legend="Crop Image" cols={2} gapX={2}>
-                                <FormField
-                                    colSpan="2"
-                                    label="Aspect Ratio:"
-                                    editor={ <DropdownList value="Original ratio" /> }
-                                />
-                                <FormField
-                                    colSpan="2"
-                                    label="Orientation:"
-                                    editor={
-                                        <ButtonGroup>
-                                            <Button className="k-group-start" selected>Portrait</Button>
-                                            <Button className="k-group-end">Landscape</Button>
-                                        </ButtonGroup>
-                                    }
-                                />
-                                <FormField
-                                    colSpan="1"
-                                    label="Width:"
-                                    editor={ <NumericTextbox showClearButton={false} value="61" /> }
-                                />
-                                <FormField
-                                    colSpan="1"
-                                    label="Height:"
-                                    editor={ <NumericTextbox showClearButton={false} value="68" /> }
-                                />
-                                <FormField
-                                    colSpan="2"
-                                    label="Lock aspect ratio:"
-                                    editor={ <Checkbox /> }
-                                />
-                            </Fieldset>
-                        </FormNormal>
+            <ImageEditorCrop contentHeight="482px">
+                <canvas width="700" height="400" style={{ backgroundImage: "url('/packages/html/assets/sofia.jpg')", backgroundPosition: "50px -350px" }}></canvas>
+                <div className="k-imageeditor-crop-overlay">
+                    <div className="k-imageeditor-crop" style={{ width: "400px", height: "400px", backgroundImage: "url('/packages/html/assets/sofia.jpg')", backgroundClip: "content-box", backgroundPosition: "748px -350px" }}>
+                        <span className="k-resize-handle k-resize-nw"></span>
+                        <span className="k-resize-handle k-resize-n"></span>
+                        <span className="k-resize-handle k-resize-ne"></span>
+                        <span className="k-resize-handle k-resize-w"></span>
+                        <span className="k-resize-handle k-resize-e"></span>
+                        <span className="k-resize-handle k-resize-sw"></span>
+                        <span className="k-resize-handle k-resize-s"></span>
+                        <span className="k-resize-handle k-resize-se"></span>
                     </div>
                 </div>
-
-            </div>
+            </ImageEditorCrop>
 
         </div>
     </>
--- packages/html/src/imageeditor/tests/imageeditor-resize-pane.tsx
+++ packages/html/src/imageeditor/tests/imageeditor-resize-pane.tsx
@@ -1,9 +1,4 @@
-import { Button } from '../../button';
-import { Checkbox } from '../../checkbox';
-import { DropdownList } from '../../dropdownlist';
-import { NumericTextbox } from '../../numerictextbox';
-import { Toolbar, ToolbarSeparator } from '../../toolbar';
-import { FormNormal, FormField, Fieldset } from '../../form';
+import { ImageEditorResize } from '..';
 
 
 export default () =>(
@@ -11,76 +6,7 @@ export default () =>(
         <div id="test-area" className="k-d-grid k-grid-cols-1">
 
             <h4>Image Editor Resize Pane</h4>
-            <div className="k-widget k-imageeditor">
-
-                {/* Header */}
-                <div className="k-imageeditor-header">
-                    <Toolbar className="k-imageeditor-toolbar">
-                        <Button icon="upload"></Button>
-                        <Button icon="download"></Button>
-                        <ToolbarSeparator></ToolbarSeparator>
-                        <Button icon="undo"></Button>
-                        <Button icon="redo"></Button>
-                        <ToolbarSeparator></ToolbarSeparator>
-                        <Button icon="crop"></Button>
-                        <Button icon="image-resize"></Button>
-                        <Button icon="zoom-in"></Button>
-                        <Button icon="zoom-out"></Button>
-                        <div>
-                            <DropdownList value="Zoom options" />
-                        </div>
-                    </Toolbar>
-                </div>
-
-                {/* Content */}
-                <div className="k-imageeditor-content" style={{ height: "482px" }}>
-
-                    <div className="k-imageeditor-canvas-container">
-                        <div className="k-imageeditor-canvas">
-                            <canvas width="61" height="68" style={{ height: "68px" }}></canvas>
-                        </div>
-                    </div>
-
-                    <div className="k-imageeditor-action-pane">
-                        <FormNormal tag="div" className="k-imageeditor-pane-form" formButtons={
-                            <>
-                                <Button themeColor="primary">Confirm</Button>
-                                <Button>Cancel</Button>
-                            </>
-                        }>
-                            <Fieldset layout="grid" legend="Resize Image" cols={2} gapX={2}>
-                                <FormField
-                                    colSpan="1"
-                                    label="Width:"
-                                    editor={ <NumericTextbox showClearButton={false} value="61" /> }
-                                />
-                                <FormField
-                                    colSpan="1"
-                                    label="&nbsp;"
-                                    editor={ <DropdownList value="Pixels" /> }
-                                />
-                                <FormField
-                                    colSpan="1"
-                                    label="Height:"
-                                    editor={ <NumericTextbox showClearButton={false} value="68" /> }
-                                />
-                                <FormField
-                                    colSpan="1"
-                                    label="&nbsp;"
-                                    editor={ <DropdownList value="Pixels" /> }
-                                />
-                                <FormField
-                                    colSpan="2"
-                                    label="Lock aspect ratio:"
-                                    editor={ <Checkbox /> }
-                                />
-                            </Fieldset>
-                        </FormNormal>
-                    </div>
-
-                </div>
-            </div>
-
+            <ImageEditorResize contentHeight="482px"></ImageEditorResize>
         </div>
     </>
 );
