c07f10827f63de0413dd184d55a8123a5f92aa93
joneff <joneff@gmail.com>
2024-05-15 14:23:53 +0300

    chore: update classic theme sass config

--- packages/classic/sass.config.js
+++ packages/classic/sass.config.js
@@ -6,6 +6,8 @@ module.exports = {
         {
             file: 'scss/all.scss',
             outFile: 'dist/all.css',
+            compiler: 'sass',
+            api: 'legacy',
             sassOptions: {
                 loadPaths: [
                     '../../node_modules/'
