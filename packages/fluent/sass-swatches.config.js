779b886c4ad22255994459f1c563a2052db6a992
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2024-05-23 11:01:37 +0300

    chore: minify swatches in dist

--- packages/fluent/sass-swatches.config.js
+++ packages/fluent/sass-swatches.config.js
@@ -10,7 +10,8 @@ module.exports = {
             sassOptions: {
                 loadPaths: [
                     '../../node_modules/'
-                ]
+                ],
+                minify: true
             }
         }
     ]
