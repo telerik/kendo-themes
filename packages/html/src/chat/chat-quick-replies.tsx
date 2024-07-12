870d4d42df4cd1cb580d3462f9dab3cd653a3c8b
Silviya Boteva <sboteva@progress.com>
2024-07-12 11:05:55 +0300

    feat(html): create chat quick reply component spec

--- packages/html/src/chat/chat-quick-replies.tsx
+++ packages/html/src/chat/chat-quick-replies.tsx
@@ -1,54 +1,33 @@
 import { classNames } from '../misc';
 
-const CHATQUICKREPLY_CLASSNAME = 'k-quick-replies';
+const CHATQUICKREPLIES_CLASSNAME = 'k-quick-replies';
 
 const states = [];
 
 const options = {};
 
-const defaultOptions = {
-    actions: [ 'Quick', 'Reply', 'Another quick', 'Reply with different length' ]
-};
+const defaultOptions = {};
 
-
-export type KendoChatQuickRepliesOptions = {
-    actions?: Array<string>;
-};
-
-export const ChatQuickReply = (
-    props: KendoChatQuickRepliesOptions &
-        React.HTMLAttributes<HTMLDivElement>
+export const ChatQuickReplies = (
+    props: React.HTMLAttributes<HTMLDivElement>
 ) => {
-    const {
-        actions = defaultOptions.actions,
-        ...other
-    } = props;
-
-    const children: JSX.Element[] = [];
-
-    if (actions) {
-        actions.map((action, index) => {
-            children.push(
-                <span className="k-quick-reply" key={index}>{action}</span>
-            );
-        });
-    }
+    const { ...other } = props;
 
     return (
         <div
             {...other}
             className={classNames(
-                CHATQUICKREPLY_CLASSNAME,
+                CHATQUICKREPLIES_CLASSNAME,
                 props.className
             )}>
-            {children}
+            {props.children}
         </div>
     );
 };
 
-ChatQuickReply.states = states;
-ChatQuickReply.options = options;
-ChatQuickReply.className = CHATQUICKREPLY_CLASSNAME;
-ChatQuickReply.defaultOptions = defaultOptions;
+ChatQuickReplies.states = states;
+ChatQuickReplies.options = options;
+ChatQuickReplies.className = CHATQUICKREPLIES_CLASSNAME;
+ChatQuickReplies.defaultOptions = defaultOptions;
 
-export default ChatQuickReply;
+export default ChatQuickReplies;
