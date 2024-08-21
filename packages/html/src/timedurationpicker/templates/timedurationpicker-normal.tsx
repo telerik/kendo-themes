824587ff7893408bacd20d1d7b506d418e45b783
Zhuliyan Penkov <penkov@progress.com>
2024-08-21 18:45:46 +0300

    feat(html): add timedurationpicker templates

---
+++ packages/html/src/timedurationpicker/templates/timedurationpicker-normal.tsx
@@ -0,0 +1,3 @@
+import TimeDurationPicker from "../timedurationpicker.spec";
+
+export const TimeDurationPickerNormal = (props) => <TimeDurationPicker {...props} />;
---
+++ packages/html/src/timedurationpicker/templates/timedurationpicker-popup.tsx
@@ -0,0 +1,5 @@
+import TimeDurationPicker from "../timedurationpicker.spec";
+
+export const TimeDurationPickerPopup = (props) => (
+    <TimeDurationPicker opened {...props}/>
+);
