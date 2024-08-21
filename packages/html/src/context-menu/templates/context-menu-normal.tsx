6b35f98a7736f66e63dcbed461f44962de760858
Zhuliyan Penkov <penkov@progress.com>
2024-08-21 18:24:14 +0300

    fix(html): add missing context-menu first/last classes

--- packages/html/src/context-menu/templates/context-menu-normal.tsx
+++ packages/html/src/context-menu/templates/context-menu-normal.tsx
@@ -4,9 +4,9 @@ import { MenuListItem } from "../..";
 export const ContextMenuNormal = (props) => (
     <ContextMenu
         children={[
-            <MenuListItem key="menu-item-1" text="Item 1" showArrow />,
+            <MenuListItem first key="menu-item-1" text="Item 1" showArrow />,
             <MenuListItem key="menu-item-2" text="Item 2" />,
-            <MenuListItem key="menu-item-3" text="Item 3" />
+            <MenuListItem last key="menu-item-3" text="Item 3" />
         ]}
         {...props}
     />
