cebe0684d15c04fcd47ad97c09c0fb43470ea0b8
Emil Petrov <epetrov@progress.com>
2024-09-05 18:21:13 +0300

    feat(utils): migrate to the dart-sass module system
    BREAKING CHANGE
    
    Starting with the R4 2024 release, all Kendo themes are now migrated to
    the SASS module system.
    
    This breaking change is due to the `node-sass` package reaching its
    official end of life.
    
    For more information, please refer to the following links:
    
    * https://sass-lang.com/blog/the-module-system-is-launched/
    * https://github.com/sass/node-sass/commit/107ded9434cfca43dbd9dfbaeb5c349228bf5cee

--- packages/utils/sass-flat.config.js
+++ packages/utils/sass-flat.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    extends: [
-        'sass-build:recommended'
-    ],
-    build: [
-        {
-            file: 'dist/all.scss',
-            outFile: 'dist/all.css'
-        }
-    ]
-};
+// module.exports = {
+//     extends: [
+//         'sass-build:recommended'
+//     ],
+//     build: [
+//         {
+//             file: 'dist/all.scss',
+//             outFile: 'dist/all.css'
+//         }
+//     ]
+// };
