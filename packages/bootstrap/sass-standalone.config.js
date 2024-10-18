217a5002c4e085d403fb86ae0d2ff47e1a73b547
Iliyana Dyankova <inikolova@progress.com>
2024-10-18 18:08:25 +0300

    chore: streamline components deps and vars

--- packages/bootstrap/sass-standalone.config.js
+++ packages/bootstrap/sass-standalone.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+    extends: [
+        'sass-build:recommended'
+    ],
+    build: [
+        {
+            entry: [ 'scss/!(common|styling)*/_index.scss' ],
+            api: "modern",
+            compiler: "sass",
+            sassOptions: {
+                loadPaths: [
+                    '../../node_modules/'
+                ]
+            }
+        }
+    ]
+};
