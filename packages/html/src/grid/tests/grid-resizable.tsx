68087e842e7a6cbfcde675a31d6e96049f3671ea
Iliyana Dyankova <inikolova@progress.com>
2025-01-28 10:59:02 +0200

    feat(html): add grid resizable option and test

---
+++ packages/html/src/grid/tests/grid-resizable.tsx
@@ -0,0 +1,9 @@
+import { GridNormal } from '../../grid';
+
+export default () =>(
+    <>
+        <div id="test-area" className="k-d-grid">
+            <GridNormal resizable={true} />
+        </div>
+    </>
+);
