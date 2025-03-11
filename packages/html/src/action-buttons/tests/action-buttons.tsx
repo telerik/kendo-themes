4376f29640b459dd9341b86253ea04a15dd9a347
Zhuliyan Penkov <penkov@progress.com>
2025-03-11 16:01:39 +0200

    feat(action-buttons): enhance alignments and positions

--- packages/html/src/action-buttons/tests/action-buttons.tsx
+++ packages/html/src/action-buttons/tests/action-buttons.tsx
@@ -8,12 +8,14 @@ const styles = `
     }
 `;
 
-export default () =>(
+export default () => (
     <>
         <style>{styles}</style>
-        <div id="test-area" className="k-d-grid k-grid-cols-2">
+        <div id="test-area" className="k-d-grid k-grid-cols-3">
             <span>start</span>
             <span>end</span>
+            <span>center</span>
+
             <div>
                 <ActionButtons alignment="start">
                     <Button>First</Button>
@@ -28,9 +30,6 @@ export default () =>(
                     <Button>Third</Button>
                 </ActionButtons>
             </div>
-
-            <span>center</span>
-            <span>stretch</span>
             <div>
                 <ActionButtons alignment="center">
                     <Button>First</Button>
@@ -38,8 +37,27 @@ export default () =>(
                     <Button>Third</Button>
                 </ActionButtons>
             </div>
+
+            <span>stretch</span>
+            <span>justify</span>
+            <span>orientation - vertical</span>
+
             <div>
                 <ActionButtons alignment="stretched">
+                    <Button>First</Button>
+                    <Button>Second</Button>
+                    <Button>Third</Button>
+                </ActionButtons>
+            </div>
+            <div>
+                <ActionButtons alignment="justify">
+                    <Button>First</Button>
+                    <Button>Second</Button>
+                    <Button>Third</Button>
+                </ActionButtons>
+            </div>
+            <div>
+                <ActionButtons orientation="vertical">
                     <Button>First</Button>
                     <Button>Second</Button>
                     <Button>Third</Button>
@@ -48,6 +66,7 @@ export default () =>(
 
             <span>rtl start</span>
             <span>rtl end</span>
+            <span>rtl center</span>
             <div dir="rtl">
                 <ActionButtons alignment="start">
                     <Button>First</Button>
@@ -62,9 +81,6 @@ export default () =>(
                     <Button>Third</Button>
                 </ActionButtons>
             </div>
-
-            <span>rtl center</span>
-            <span>rtl stretch</span>
             <div dir="rtl">
                 <ActionButtons alignment="center">
                     <Button>First</Button>
@@ -72,8 +88,27 @@ export default () =>(
                     <Button>Third</Button>
                 </ActionButtons>
             </div>
+
+            <span>rtl stretch</span>
+            <span>rtl justify</span>
+            <span>rtl orientation - vertical</span>
+
             <div dir="rtl">
                 <ActionButtons alignment="stretched">
+                    <Button>First</Button>
+                    <Button>Second</Button>
+                    <Button>Third</Button>
+                </ActionButtons>
+            </div>
+            <div dir="rtl">
+                <ActionButtons alignment="justify">
+                    <Button>First</Button>
+                    <Button>Second</Button>
+                    <Button>Third</Button>
+                </ActionButtons>
+            </div>
+            <div dir="rtl">
+                <ActionButtons orientation="vertical">
                     <Button>First</Button>
                     <Button>Second</Button>
                     <Button>Third</Button>
