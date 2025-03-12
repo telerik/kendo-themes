e06eb32b7548f62b71f8d74e78a1af8e5212764e
Emil Petrov <epetrov@progress.com>
2025-03-12 16:49:29 +0200

    feat(html): enhance column-menu spec
    - add support for sort and filter indicators
    - add support for item actions
    - add support for draggable items

---
+++ packages/html/src/column-menu/column-menu-item-action.tsx
@@ -0,0 +1,49 @@
+import { Icon } from '../icon';
+import { classNames } from '../misc';
+
+export const COLUMNMENUITEM_ACTION_CLASSNAME = `k-columnmenu-item-action`;
+
+export enum columnMenuItemActionType {
+    REMOVE = 'remove',
+    ADD = 'add'
+};
+
+export type KendoColumnMenuItemActionProps = {
+    type?: columnMenuItemActionType | string;
+};
+
+export const ColumnMenuItemAction = (
+    props: KendoColumnMenuItemActionProps &
+        React.HTMLAttributes<HTMLSpanElement>
+) => {
+    const {
+        type,
+        ...other
+    } = props;
+
+    let icon;
+
+    switch (type) {
+        case columnMenuItemActionType.REMOVE:
+            icon = <Icon icon="x-circle" size="medium" />;
+            break;
+        case columnMenuItemActionType.ADD:
+            icon = <Icon themeColor="primary" icon="plus-circle" size="medium" />;
+            break;
+        default:
+            return <Icon icon={type} size="medium" />;
+    }
+
+    return (
+        <span
+            {...other}
+            className={classNames(
+                props.className,
+                COLUMNMENUITEM_ACTION_CLASSNAME,
+                `k-columnmenu-item-${type}-action`
+            )}
+        >
+            {icon}
+        </span>
+    );
+};
