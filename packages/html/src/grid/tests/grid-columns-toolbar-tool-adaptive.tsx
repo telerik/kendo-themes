5618145a8b0b0e256c2851a3e516bfe00dc698a7
Zhuliyan Penkov <penkov@progress.com>
2025-03-24 13:37:47 +0200

    chore(html): add grid columns toolbar tool tests

---
+++ packages/html/src/grid/tests/grid-columns-toolbar-tool-adaptive.tsx
@@ -0,0 +1,363 @@
+import { Grid, GridToolbar, GridHeader, GridHeaderCell, GridHeaderTable, GridContainer, GridContent, GridTable, GridPager } from "../";
+import { TableThead, TableTbody, TableRow, TableTd } from "../../table";
+import { ActionSheetFooter, ActionSheetHeader, ActionSheetNormal } from "../../action-sheet";
+import { Button } from "../../button";
+import { Treeview, TreeviewItem } from "../../treeview";
+
+const styles = `
+    #test-area {
+        max-width: 1060px;
+        --kendo-actionsheet-height: 400px;
+        --kendo-actionsheet-max-height: 400px;
+    }
+    #test-area > section {
+        height: 600px;
+        outline: 1px dotted;
+        overflow: hidden;
+        position: relative;
+        transform: translateZ(0);
+    }
+`;
+
+export default () => (
+    <>
+        <style>{styles}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-2">
+            <span className="k-col-span-2" >resolutions 500 &lt;= 768</span>
+
+            <section>
+                <Grid
+                    className="k-grid-no-scrollbar"
+                    toolbar={
+                        <>
+                            <GridToolbar>
+                                <Button size="large" icon="filter" />
+                                <Button size="large" icon="arrows-swap" />
+                                <Button size="large" icon="group" />
+                                <Button size="large" icon="columns" />
+                            </GridToolbar>
+                        </>
+                    }
+                    pager={
+                        <>
+                            <GridPager type="input" pageTitleInfo={false} refresh={false} info={false} />
+                        </>
+                    }
+                    children={
+                        <>
+                            <GridHeader>
+                                <div className="k-grid-header-wrap">
+                                    <GridHeaderTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableThead>
+                                            <TableRow>
+                                                <GridHeaderCell columnTitle="Header Name 1"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 2"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 3"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 4"></GridHeaderCell>
+                                            </TableRow>
+                                        </TableThead>
+                                    </GridHeaderTable>
+                                </div>
+                            </GridHeader>
+                            <GridContainer>
+                                <GridContent>
+                                    <GridTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableTbody>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row" alt>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                            </TableRow>
+                                        </TableTbody>
+                                    </GridTable>
+                                </GridContent>
+                            </GridContainer>
+                        </>
+                    }
+                ></Grid>
+            </section>
+
+            <section>
+                <Grid
+                    className="k-grid-no-scrollbar"
+                    toolbar={
+                        <>
+                            <GridToolbar>
+                                <Button size="large" icon="filter" />
+                                <Button size="large" icon="arrows-swap" />
+                                <Button size="large" icon="group" />
+                                <Button selected size="large" icon="columns" />
+                            </GridToolbar>
+                        </>
+                    }
+                    pager={
+                        <>
+                            <GridPager type="input" pageTitleInfo={false} refresh={false} info={false} />
+                        </>
+                    }
+                    children={
+                        <>
+                            <GridHeader>
+                                <div className="k-grid-header-wrap">
+                                    <GridHeaderTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableThead>
+                                            <TableRow>
+                                                <GridHeaderCell columnTitle="Header Name 1"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 2"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 3"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 4"></GridHeaderCell>
+                                            </TableRow>
+                                        </TableThead>
+                                    </GridHeaderTable>
+                                </div>
+                            </GridHeader>
+                            <GridContainer>
+                                <GridContent>
+                                    <GridTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableTbody>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row" alt>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                            </TableRow>
+                                        </TableTbody>
+                                    </GridTable>
+                                </GridContent>
+                            </GridContainer>
+                        </>
+                    }
+                ></Grid>
+                <ActionSheetNormal
+                    adaptive
+                    header={
+                        <ActionSheetHeader actionsEnd={<Button icon="x" size="large" fillMode="flat" />} title="Columns visibility" subTitle="Selected fields are visible" />
+                    }
+                    footer={
+                        <ActionSheetFooter>
+                            <Button icon="arrow-rotate-ccw" size="large">Reset</Button>
+                            <Button icon="check" size="large" themeColor="primary">Apply</Button>
+                        </ActionSheetFooter>
+                    }
+                >
+                    <Treeview size="large">
+                        <TreeviewItem showCheckbox checked disabled top text="Header Name 1" />
+                        <TreeviewItem showCheckbox text="Header Name 2" />
+                        <TreeviewItem showCheckbox text="Header Name 3" />
+                        <TreeviewItem showCheckbox bottom text="Header Name 4" />
+                    </Treeview>
+                </ActionSheetNormal>
+            </section>
+
+            <section>
+            <Grid
+                    className="k-grid-no-scrollbar"
+                    toolbar={
+                        <>
+                            <GridToolbar>
+                                <Button size="large" icon="filter" />
+                                <Button size="large" icon="arrows-swap" />
+                                <Button size="large" icon="group" />
+                                <Button selected size="large" icon="columns" />
+                            </GridToolbar>
+                        </>
+                    }
+                    pager={
+                        <>
+                            <GridPager type="input" pageTitleInfo={false} refresh={false} info={false} />
+                        </>
+                    }
+                    children={
+                        <>
+                            <GridHeader>
+                                <div className="k-grid-header-wrap">
+                                    <GridHeaderTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableThead>
+                                            <TableRow>
+                                                <GridHeaderCell columnTitle="Header Name 1"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 2"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 3"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 4"></GridHeaderCell>
+                                            </TableRow>
+                                        </TableThead>
+                                    </GridHeaderTable>
+                                </div>
+                            </GridHeader>
+                            <GridContainer>
+                                <GridContent>
+                                    <GridTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableTbody>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row" alt>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                            </TableRow>
+                                        </TableTbody>
+                                    </GridTable>
+                                </GridContent>
+                            </GridContainer>
+                        </>
+                    }
+                ></Grid>
+                <ActionSheetNormal
+                    adaptive
+                    header={
+                        <ActionSheetHeader actionsEnd={<Button icon="x" size="large" fillMode="flat" />} title="Columns visibility" subTitle="Selected fields are visible" />
+                    }
+                    footer={
+                        <ActionSheetFooter>
+                            <Button icon="arrow-rotate-ccw" size="large">Reset</Button>
+                            <Button icon="check" size="large" themeColor="primary">Apply</Button>
+                        </ActionSheetFooter>
+                    }
+                >
+                    <Treeview size="large">
+                        <TreeviewItem showCheckbox checked top text="Header Name 1" />
+                        <TreeviewItem showCheckbox checked text="Header Name 2" />
+                        <TreeviewItem showCheckbox text="Header Name 3" />
+                        <TreeviewItem showCheckbox bottom text="Header Name 4" />
+                    </Treeview>
+                </ActionSheetNormal>
+            </section>
+
+            <section>
+            <Grid
+                    className="k-grid-no-scrollbar"
+                    toolbar={
+                        <>
+                            <GridToolbar>
+                                <Button size="large" icon="filter" />
+                                <Button size="large" icon="arrows-swap" />
+                                <Button size="large" icon="group" />
+                                <Button selected size="large" icon="columns" />
+                            </GridToolbar>
+                        </>
+                    }
+                    pager={
+                        <>
+                            <GridPager type="input" pageTitleInfo={false} refresh={false} info={false} />
+                        </>
+                    }
+                    children={
+                        <>
+                            <GridHeader>
+                                <div className="k-grid-header-wrap">
+                                    <GridHeaderTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableThead>
+                                            <TableRow>
+                                                <GridHeaderCell columnTitle="Header Name 1"></GridHeaderCell>
+                                                <GridHeaderCell columnTitle="Header Name 2"></GridHeaderCell>
+                                            </TableRow>
+                                        </TableThead>
+                                    </GridHeaderTable>
+                                </div>
+                            </GridHeader>
+                            <GridContainer>
+                                <GridContent>
+                                    <GridTable>
+                                        <colgroup>
+                                            <col />
+                                            <col />
+                                        </colgroup>
+                                        <TableTbody>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 1</TableTd>
+                                                <TableTd>Value 1</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row" alt>
+                                                <TableTd>Value 2</TableTd>
+                                                <TableTd>Value 2</TableTd>
+                                            </TableRow>
+                                            <TableRow className="k-master-row">
+                                                <TableTd>Value 3</TableTd>
+                                                <TableTd>Value 3</TableTd>
+                                            </TableRow>
+                                        </TableTbody>
+                                    </GridTable>
+                                </GridContent>
+                            </GridContainer>
+                        </>
+                    }
+                ></Grid>
+            </section>
+        </div>
+    </>
+);
