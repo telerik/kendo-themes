fe51d35c7e46e594064abcc991cca1a43808f90d
Iliyana Dyankova <inikolova@progress.com>
2025-04-17 16:17:02 +0300

    chore: update showcase example

--- packages/html/src/action-buttons/tests/material-test.tsx
+++ packages/html/src/action-buttons/tests/material-test.tsx
@@ -2,7 +2,7 @@ import { BottomNavItem, BottomNavNormal } from "../../bottom-nav";
 import { BreadcrumbCollapsingNone } from "../../breadcrumb";
 import { Button, TextButton } from "../../button";
 import { ButtonGroup } from "../../button-group";
-import { CalendarNormal } from "../../calendar";
+import {  Calendar, CalendarTable, CalendarCell, CalendarTableRow } from "../../calendar";
 import { CardNormal } from "../../card";
 import { Chip, ChipAction } from "../../chip";
 import { ColorPicker } from "../../colorpicker";
@@ -15,15 +15,24 @@ import { ProgressBarNormal } from "../../progressbar";
 import { SliderNormal } from "../../slider";
 import { Step, StepList, StepperNormal } from "../../stepper";
 import { SwitchChecked, SwitchNormal } from "../../switch";
-import { TabStripContent, TabStripItem, TabStripLeft, TabStripNormal } from "../../tabstrip";
+import { TabStripContent, TabStripItem, TabStripLeft, TabStripClosable } from "../../tabstrip";
 import { TextboxNormal } from "../../textbox";
 import { ToolbarItem, ToolbarResizable, ToolbarSeparator } from "../../toolbar";
 import { WindowNormal } from "../../window";
+import { Menu, MenuList, MenuListItem, MenuItem, MenuSeparator } from "../../menu";
+import { Popup } from "../../popup";
+import { TreeviewNormal, TreeviewItem } from "../../treeview";
+import { ExpansionPanelExpanded, ExpansionPanelNormal } from "../../expansion-panel";
 
 const style = `
 
     #test-area {
-        max-width: 1440px;
+        max-width: 1240px;
+        border: 2px solid red;
+        padding: 0;
+    }
+    .grid-with-calendar {
+        grid-template-columns: 400px 1fr 1fr;
     }
     .k-progressbar {
         grid-column-start: 2;
@@ -36,6 +45,10 @@ const style = `
 
     .k-window {
         position: relative;
+    }
+    .k-animation-container {
+        position: absolute;
+        top: 100%;
     }
 `;
 
@@ -51,11 +64,13 @@ export default () => (
                             <DrawerItem icon="bell" text="Notification" />
                             <DrawerItem icon="star" text="Favourites" />
                             <DrawerItem icon="calendar" text="Calendar" />
+                            <DrawerItem text="Selected" selected icon="star-outline" />
+                            <DrawerItem text="Hover"hover icon="star-outline" />
                         </>
                     }
                 >
                 <DrawerContent>
-                    <div className="k-d-grid k-grid-rows k-gap-8">
+                    <div className="k-d-grid k-grid-rows k-gap-4">
                         <div>
                             <ToolbarResizable>
                                 <Button icon="bold"></Button>
@@ -80,7 +95,7 @@ export default () => (
                         </div>
                         <div className="k-d-grid k-grid-cols-2 k-gap-8">
                             <div>
-                                <TabStripNormal>
+                                <TabStripClosable>
                                     <TabStripContent active>
                                         <div className="k-tabstrip-wrapper">
                                             <TabStripLeft
@@ -96,7 +111,7 @@ export default () => (
                                             </TabStripLeft>
                                         </div>
                                     </TabStripContent>
-                                </TabStripNormal>
+                                </TabStripClosable>
                             </div>
 
                             <div className="k-d-grid k-grid-rows k-gap-8">
@@ -120,9 +135,9 @@ export default () => (
 
                         </div>
 
-                        <div className="k-d-grid k-grid-cols-3 k-gap-8">
-                            <div className="k-d-grid k-grid-rows k-gap-8">
-                                <div className="k-d-grid k-grid-cols-3 k-gap-2">
+                        <div className="k-d-grid k-grid-cols-3 k-gap-4 grid-with-calendar">
+                            <div className="k-d-grid k-grid-rows k-gap-4">
+                                <div className="k-d-grid k-grid-cols-3 k-gap-4">
                                     <div><TextButton>Solid</TextButton></div>
                                     <div><TextButton fillMode="outline">Outline</TextButton></div>
                                     <div><TextButton fillMode="flat">Flat</TextButton></div>
@@ -149,14 +164,77 @@ export default () => (
                                     />
                                 </div>
                             </div>
-                            <div className="k-d-grid k-grid-rows k-gap-8">
+                            <div className="k-d-grid k-grid-rows k-gap-8 k-align-items-baseline">
                                 <BreadcrumbCollapsingNone />
 
                                 <SliderNormal />
                             </div>
 
                             <div className="k-d-grid k-grid-rows k-gap-8">
-                                <CalendarNormal />
+                                <Calendar className="k-calendar-range" calendarView="month" showWeek>
+                                                    <CalendarTable showWeek showTableHead>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="39" weekCell />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell text="1" />
+                                                            <CalendarCell text="2" weekend />
+                                                        </CalendarTableRow>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="40" weekCell />
+                                                            <CalendarCell text="3" weekend />
+                                                            <CalendarCell text="4" />
+                                                            <CalendarCell text="5" />
+                                                            <CalendarCell text="6" />
+                                                            <CalendarCell text="7" />
+                                                            <CalendarCell text="8" />
+                                                            <CalendarCell text="9" weekend />
+                                                        </CalendarTableRow>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="41" weekCell />
+                                                            <CalendarCell text="10" weekend />
+                                                            <CalendarCell text="11" />
+                                                            <CalendarCell text="12" today />
+                                                            <CalendarCell text="13" />
+                                                            <CalendarCell text="14" />
+                                                            <CalendarCell text="15" />
+                                                            <CalendarCell text="16" weekend />
+                                                        </CalendarTableRow>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="42" weekCell />
+                                                            <CalendarCell text="17" weekend />
+                                                            <CalendarCell text="18" />
+                                                            <CalendarCell text="19" />
+                                                            <CalendarCell text="20" />
+                                                            <CalendarCell text="21" rangeStart selected />
+                                                            <CalendarCell text="22" rangeMid />
+                                                            <CalendarCell text="23" weekend rangeMid />
+                                                        </CalendarTableRow>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="43" weekCell />
+                                                            <CalendarCell text="24" weekend rangeMid />
+                                                            <CalendarCell text="25" rangeEnd focus selected />
+                                                            <CalendarCell text="26" />
+                                                            <CalendarCell text="27" />
+                                                            <CalendarCell text="28" />
+                                                            <CalendarCell text="29" />
+                                                            <CalendarCell text="30" weekend />
+                                                        </CalendarTableRow>
+                                                        <CalendarTableRow>
+                                                            <CalendarCell text="44" weekCell />
+                                                            <CalendarCell text="31" weekend />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                            <CalendarCell empty />
+                                                        </CalendarTableRow>
+                                                    </CalendarTable>
+                                                </Calendar>
                             </div>
                             <div className="k-d-grid k-grid-rows k-gap-5">
                                 <CardNormal />
@@ -198,6 +276,53 @@ export default () => (
                         <div>
                             <GridWithFilterRow></GridWithFilterRow>
                         </div>
+                        <div className="k-d-grid k-grid-cols-3 k-gap-4">
+                            <div>
+                                <Menu
+                                    children={
+                                        <>
+                                            <MenuItem first text="Item 1"></MenuItem>
+                                            <MenuItem showArrow arrowIconName="caret-alt-down" text="Item 2" active></MenuItem>
+                                            <MenuItem showArrow arrowIconName="caret-alt-down" text="Item 3"></MenuItem>
+                                        </>
+                                    }
+                                popup={
+                                    <>
+                                        <Popup className="k-menu-popup" offset={{ left: "175px", top: "auto" }}>
+                                            <MenuList>
+                                                <MenuListItem first text="Disabled" disabled></MenuListItem>
+                                                <MenuListItem text="Normal"></MenuListItem>
+                                                <MenuListItem text="Hover" hover showArrow></MenuListItem>
+                                                <MenuSeparator></MenuSeparator>
+                                                <MenuListItem text="Active" active showArrow></MenuListItem>
+                                                <MenuListItem text="Focus" focus></MenuListItem>
+                                                <MenuListItem last text="Focus & Active" active focus></MenuListItem>
+                                            </MenuList>
+                                        </Popup>
+                                    </>
+                                } />
+                            </div>
+                            <div>
+                                <TreeviewNormal>
+                                    <TreeviewItem top text="Normal" />
+                                    <TreeviewItem text="Hover" hover />
+                                    <TreeviewItem text="Focus" focus/>
+                                    <TreeviewItem text="Hover + Focus" hover focus/>
+                                    <TreeviewItem text="Selected" selected/>
+                                    <TreeviewItem text="Selected + Hover" selected hover/>
+                                    <TreeviewItem text="Selected + Hover + Focus" selected hover focus/>
+                                    <TreeviewItem text="Selected focus" selected focus/>
+                                    <TreeviewItem text="Disabled" disabled/>
+                                    <TreeviewItem text="Disabled focus" disabled focus/>
+                                    <TreeviewItem bottom leafClassName="k-treeview-load-more-button" text="Load more..."/>
+                                </TreeviewNormal>
+                            </div>
+                            <div>
+                                <ExpansionPanelNormal title="Collapsed panel" subtitle="Subtitle" />
+                                <ExpansionPanelExpanded title="Expanded panel" subtitle="Subtitle" />
+                            </div>
+                        </div>
+
                     </div>
                 </DrawerContent>
             </Drawer>
