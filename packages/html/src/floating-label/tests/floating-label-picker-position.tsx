5adc6bffbd5dfad835fb885b6025b1eb3f85d810
Teya Veselinova <tveselin@progress.com>
2025-05-19 10:09:49 +0300

    chore: add class to pickers

--- packages/html/src/floating-label/tests/floating-label-picker-position.tsx
+++ packages/html/src/floating-label/tests/floating-label-picker-position.tsx
@@ -20,153 +20,153 @@ export default () =>(
             <div>Outside Small</div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="small" />
+                    <DropdownList focus size="small" className="dropdown-solid-outside-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="small" fillMode="outline" />
+                    <DropdownList focus size="small" fillMode="outline" className="dropdown-outline-outside-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="small" fillMode="flat" />
+                    <DropdownList focus size="small" fillMode="flat" className="dropdown-flat-outside-small" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Outside Medium</div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus />
+                    <DropdownList focus className="dropdown-solid-outside-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus fillMode="outline" />
+                    <DropdownList focus fillMode="outline" className="dropdown-outline-outside-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus fillMode="flat" />
+                    <DropdownList focus fillMode="flat" className="dropdown-flat-outside-medium" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Outside Large</div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="large" />
+                    <DropdownList focus size="large" className="dropdown-solid-outside-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="large" fillMode="outline" />
+                    <DropdownList focus size="large" fillMode="outline" className="outline-outside-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus label="Floating label">
-                    <DropdownList focus size="large" fillMode="flat" />
+                    <DropdownList focus size="large" fillMode="flat" className="dropdown-flat-outside-large" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Border Small</div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="small" />
+                    <DropdownList focus size="small" className="dropdown-solid-border-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="small" fillMode="outline" />
+                    <DropdownList focus size="small" fillMode="outline" className="dropdown-outline-border-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="small" fillMode="flat" />
+                    <DropdownList focus size="small" fillMode="flat" className="dropdown-flat-border-small" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Border Medium</div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus />
+                    <DropdownList focus className="dropdown-solid-border-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus fillMode="outline" />
+                    <DropdownList focus fillMode="outline" className="dropdown-outline-border-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus fillMode="flat" />
+                    <DropdownList focus fillMode="flat" className="dropdown-flat-border-medium" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Border Large</div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="large" />
+                    <DropdownList focus size="large" className="dropdown-solid-border-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="large" fillMode="outline" />
+                    <DropdownList focus size="large" fillMode="outline" className="dropdown-outline-border-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="border" label="Floating label">
-                    <DropdownList focus size="large" fillMode="flat" />
+                    <DropdownList focus size="large" fillMode="flat" className="dropdown-flat-border-large" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Inside Small</div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="small" />
+                    <DropdownList focus size="small" className="dropdown-solid-inside-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="small" fillMode="outline" />
+                    <DropdownList focus size="small" fillMode="outline" className="dropdown-outline-inside-small" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="small" fillMode="flat" />
+                    <DropdownList focus size="small" fillMode="flat" className="dropdown-flat-inside-small" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Inside Medium</div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus />
+                    <DropdownList focus className="dropdown-solid-inside-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus fillMode="outline" />
+                    <DropdownList focus fillMode="outline" className="dropdown-outline-inside-medium" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus fillMode="flat" />
+                    <DropdownList focus fillMode="flat" className="dropdown-flat-inside-medium" />
                 </FloatingLabelNormal>
             </div>
 
             <div>Inside Large</div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="large" />
+                    <DropdownList focus size="large" className="dropdown-solid-inside-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="large" fillMode="outline" />
+                    <DropdownList focus size="large" fillMode="outline" className="dropdown-outline-inside-large" />
                 </FloatingLabelNormal>
             </div>
             <div>
                 <FloatingLabelNormal focus position="inside" label="Floating label">
-                    <DropdownList focus size="large" fillMode="flat" />
+                    <DropdownList focus size="large" fillMode="flat" className="dropdown-flat-inside-large" />
                 </FloatingLabelNormal>
             </div>
 
