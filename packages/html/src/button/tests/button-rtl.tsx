09a4021fcf8d14c8d4e9cbfa805db797cad229d8
Iliyana Dyankova <inikolova@progress.com>
2025-06-11 10:16:47 +0300

    chore(test): add buttons with selected+focus state

--- packages/html/src/button/tests/button-rtl.tsx
+++ packages/html/src/button/tests/button-rtl.tsx
@@ -2,7 +2,8 @@ import { IconButton, IconTextButton, TextButton } from '../../button';
 
 const styles = `
     #test-area {
-        grid-template-columns: 120px repeat(6, 1fr);
+        grid-template-columns: 120px repeat(7, 1fr);
+        max-width: 1240px;
     }
 `;
 
@@ -17,6 +18,7 @@ export default () => (
             <span>Focus</span>
             <span>Active</span>
             <span>Selected</span>
+            <span>Selected+Focus</span>
             <span>Disabled</span>
 
             <span>base</span>
@@ -25,6 +27,7 @@ export default () => (
             <span dir="rtl"><TextButton focus={true}>Focus</TextButton></span>
             <span dir="rtl"><TextButton active={true}>Active</TextButton></span>
             <span dir="rtl"><TextButton selected={true}>Selected</TextButton></span>
+            <span dir="rtl"><TextButton selected={true} focus={true}>Selected+Focus</TextButton></span>
             <span dir="rtl"><TextButton disabled={true}>Disabled</TextButton></span>
 
             <span>^ text + icon</span>
@@ -33,6 +36,7 @@ export default () => (
             <span dir="rtl"><IconTextButton focus={true}>Focus</IconTextButton></span>
             <span dir="rtl"><IconTextButton active={true}>Active</IconTextButton></span>
             <span dir="rtl"><IconTextButton selected={true}>Selected</IconTextButton></span>
+            <span dir="rtl"><IconTextButton selected={true} focus={true}>Selected+Focus</IconTextButton></span>
             <span dir="rtl"><IconTextButton disabled={true}>Disabled</IconTextButton></span>
 
             <span>^ icon</span>
@@ -41,6 +45,7 @@ export default () => (
             <span dir="rtl"><IconButton focus={true}></IconButton></span>
             <span dir="rtl"><IconButton active={true}></IconButton></span>
             <span dir="rtl"><IconButton selected={true}></IconButton></span>
+            <span dir="rtl"><IconButton selected={true} focus={true}></IconButton></span>
             <span dir="rtl"><IconButton disabled={true}></IconButton></span>
 
             <span>primary</span>
@@ -49,6 +54,7 @@ export default () => (
             <span dir="rtl"><TextButton themeColor="primary" focus={true}>Focus</TextButton></span>
             <span dir="rtl"><TextButton themeColor="primary" active={true}>Active</TextButton></span>
             <span dir="rtl"><TextButton themeColor="primary" selected={true}>Selected</TextButton></span>
+            <span dir="rtl"><TextButton themeColor="primary" selected={true} focus={true}>Selected+Focus</TextButton></span>
             <span dir="rtl"><TextButton themeColor="primary" disabled={true}>Disabled</TextButton></span>
 
             <span>^ text + icon</span>
@@ -57,6 +63,7 @@ export default () => (
             <span dir="rtl"><IconTextButton themeColor="primary" focus={true}>Focus</IconTextButton></span>
             <span dir="rtl"><IconTextButton themeColor="primary" active={true}>Active</IconTextButton></span>
             <span dir="rtl"><IconTextButton themeColor="primary" selected={true}>Selected</IconTextButton></span>
+            <span dir="rtl"><IconTextButton themeColor="primary" selected={true} focus={true}>Selected+Focus</IconTextButton></span>
             <span dir="rtl"><IconTextButton themeColor="primary" disabled={true}>Disabled</IconTextButton></span>
 
             <span>^ icon</span>
@@ -65,6 +72,7 @@ export default () => (
             <span dir="rtl"><IconButton themeColor="primary" focus={true}></IconButton></span>
             <span dir="rtl"><IconButton themeColor="primary" active={true}></IconButton></span>
             <span dir="rtl"><IconButton themeColor="primary" selected={true}></IconButton></span>
+            <span dir="rtl"><IconButton themeColor="primary" selected={true} focus={true}></IconButton></span>
             <span dir="rtl"><IconButton themeColor="primary" disabled={true}></IconButton></span>
         </div>
     </>
