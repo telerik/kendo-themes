1fac278b013359933578b9658f507a5f1e9414f0
Emil Petrov <epetrov@progress.com>
2025-06-26 17:09:55 +0300

    chore(html): fix textarea typescript errors

--- packages/html/src/textarea/textarea.spec.tsx
+++ packages/html/src/textarea/textarea.spec.tsx
@@ -57,7 +57,7 @@ const defaultOptions = {
 export const Textarea = (
     props: KendoTextareaProps &
         KendoTextareaState &
-        React.HTMLAttributes<HTMLSpanElement>
+        Omit<React.HTMLAttributes<HTMLSpanElement>, 'prefix'>
 ) => {
     const {
         size = defaultOptions.size,
