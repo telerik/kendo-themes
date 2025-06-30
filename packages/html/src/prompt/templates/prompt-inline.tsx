d2180370ed3adfe682e8ea443a42e13a9e4f2ef6
Zhuliyan Penkov <penkov@progress.com>
2025-06-30 18:00:32 +0300

    chore(html): update inline-prompt tests and templates

--- packages/html/src/prompt/templates/prompt-inline.tsx
+++ packages/html/src/prompt/templates/prompt-inline.tsx
@@ -1,8 +1,9 @@
 import { ActionButtons } from "../../action-buttons";
 import { IconButton, IconTextButton } from "../../button";
-import { Prompt, PromptContent, PromptView } from "..";
+import { Prompt, PromptContent, PromptView, PromptSendButton } from "..";
 import { Card, CardBody } from "../../card";
-import { Textbox } from "../../textbox";
+import { Textarea } from "../../textarea";
+import { SpeechToTextButton } from "../../speech-to-text-button";
 
 export const PromptInline = ({ text, ...other }: any) => (
     <Prompt {...other} >
@@ -11,17 +12,19 @@ export const PromptInline = ({ text, ...other }: any) => (
                 <Card>
                     <CardBody style={{maxHeight: "150px", overflowY: "auto" }}>{text}</CardBody>
                     <ActionButtons className="k-card-actions" alignment="start">
+                        <IconTextButton fillMode="flat" themeColor="primary" icon="copy">Copy</IconTextButton>
                         <IconTextButton fillMode="flat" themeColor="primary" icon="insert-bottom">Insert</IconTextButton>
                         <IconTextButton fillMode="flat" themeColor="primary" icon="check">Replace</IconTextButton>
                         <IconTextButton fillMode="flat" themeColor="base" icon="cancel-outline">Discard</IconTextButton>
                     </ActionButtons>
                 </Card>
-                <Textbox placeholder="Edit, generate or explain ..." separators={false}
+                <Textarea rows={1} placeholder="Placeholder ..." resize="none" flow="horizontal" suffixSeparator={false}
                     prefix={<>
-                        <IconButton fillMode="flat" icon="sparkles" />
+                        <IconButton fillMode="flat" icon="menu" />
+                        <SpeechToTextButton fillMode="flat" />
                     </>}
                     suffix={<>
-                        <IconButton fillMode="flat" icon="paper-plane" />
+                        <PromptSendButton disabled />
                     </>} />
             </PromptView>
         </PromptContent>
