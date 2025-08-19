dc6bef429387491258d73574a90accb4a86317f8
Teya Veselinova <tveselin@progress.com>
2025-08-19 16:27:19 +0300

    chore: add map rtl test

---
+++ packages/html/src/map/tests/map-rtl.tsx
@@ -0,0 +1,18 @@
+import { Map } from '..';
+
+
+const styles = `
+    .k-map .k-layer:last-child {
+        background-color: #f0f0f0;
+        top: 0; right: 0; bottom: 0; left: 0;
+    }
+`;
+
+export default () =>(
+    <>
+        <style>{styles}</style>
+        <div id="test-area">
+            <Map className="k-rtl" style={{ position: "relative" }}></Map>
+        </div>
+    </>
+);
