57c379e15af678197ffa846c4c26f35d2dca41e6
Iliyana Dyankova <inikolova@progress.com>
2025-08-20 11:57:50 +0300

    chore(html): update chat tests

--- packages/html/src/chat/tests/chat-rtl.tsx
+++ packages/html/src/chat/tests/chat-rtl.tsx
@@ -1,9 +1,10 @@
-import { ChatHeader, ChatMessage, ChatNormal, ChatTimestamp, ChatMessageGroup } from '..';
+import { ChatHeader, ChatNormal, ChatMessage, ChatFileWrapper, ChatFile, ChatMessageReference, ChatMessageGroup } from '..';
 
 
 const styles = `
     .k-chat {
-        min-height: 370px;
+        min-height: 300px;
+        height: 300px;
         min-width: auto;
     }
 `;
@@ -12,54 +13,11 @@ export default () =>(
     <>
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-3">
-            <span>Chat normal</span>
-            <span>Link in sender message</span>
-            <span>Link in receiver message</span>
-            <section>
-                <ChatNormal dir="rtl">
-                    <ChatMessageGroup>
-                        <ChatMessage />
-                    </ChatMessageGroup>
-                </ChatNormal>
-            </section>
-            <section>
-                <ChatNormal dir="rtl">
-                    <ChatMessageGroup type="sender" avatar={null}>
-                        <ChatMessage text={
-                            <>
-                                Here you go, mate<br />
-                                <a href="https://www.myportfolio.com/alex_barton" target="_blank" rel="noopener noreferrer">
-                                    www.myportfolio.com/alex_barton
-                                </a>
-                                <br />
-                                Let me know if you have any problem opening it.
-                            </>
-                        } />
-                    </ChatMessageGroup>
-                </ChatNormal>
-            </section>
-            <section>
-                <ChatNormal dir="rtl">
-                    <ChatMessageGroup avatar={null}>
-                        <ChatMessage text={
-                            <>
-                                Here you go, mate<br />
-                                <a href="https://www.myportfolio.com/alex_barton" target="_blank" rel="noopener noreferrer">
-                                    www.myportfolio.com/alex_barton
-                                </a>
-                                <br />
-                                Let me know if you have any problem opening it.
-                            </>
-                        } />
-                    </ChatMessageGroup>
-                </ChatNormal>
-            </section>
-
             <span>Chat with Header</span>
-            <span>Chat with Time stamp</span>
-            <span>Chat with Header and Time stamp</span>
+            <span>Pin file - receiver</span>
+            <span>Pin file - Sender</span>
             <section>
-                <ChatNormal dir="rtl" header={
+                 <ChatNormal dir="rtl" header={
                     <ChatHeader>
                         Alex
                     </ChatHeader>
@@ -67,52 +25,86 @@ export default () =>(
                     <ChatMessageGroup>
                         <ChatMessage />
                     </ChatMessageGroup>
-                </ChatNormal>
-            </section>
-            <section>
-                <ChatNormal dir="rtl">
-                    <ChatTimestamp>Day, Month 7, 2023</ChatTimestamp>
-                    <ChatMessageGroup>
-                        <ChatMessage />
-                    </ChatMessageGroup>
-                    <ChatTimestamp>Day, Month 7, 2023</ChatTimestamp>
                     <ChatMessageGroup type="sender">
                         <ChatMessage />
                     </ChatMessageGroup>
                 </ChatNormal>
             </section>
             <section>
-                <ChatNormal dir="rtl" header={
-                    <ChatHeader>
-                        Alex
-                    </ChatHeader>
-                }>
-                    <ChatTimestamp>Day, Month 7, 2023</ChatTimestamp>
-                    <ChatMessageGroup>
-                        <ChatMessage />
+                <ChatNormal dir="rtl" pinned={<ChatMessageReference pinned content={<ChatFile />} closeable />}>
+                    <ChatMessageGroup avatar={null}>
+                        <ChatMessage files={
+                            <>
+                                <ChatFileWrapper>
+                                    <ChatFile />
+                                </ChatFileWrapper>
+                            </>
+                        } />
                     </ChatMessageGroup>
-                    <ChatMessageGroup type="sender">
-                        <ChatMessage />
+                </ChatNormal>
+            </section>
+            <section>
+                <ChatNormal dir="rtl" pinned={<ChatMessageReference type="sender" pinned content={<ChatFile />} closeable />}>
+                    <ChatMessageGroup type="sender" avatar={null}>
+                        <ChatMessage files={
+                            <ChatFileWrapper>
+                                <ChatFile />
+                            </ChatFileWrapper>
+                        } />
                     </ChatMessageGroup>
                 </ChatNormal>
             </section>
 
-            <span>Chat with typing indicator</span>
+            <span>Pin message / receiver</span>
+            <span>Pin message / sender</span>
             <span></span>
-            <span></span>
-
             <section>
-                <ChatNormal dir="rtl">
-                    <ChatTimestamp>Day, Month 7, 2023</ChatTimestamp>
-                    <ChatMessageGroup type="sender">
+                <ChatNormal dir="rtl" pinned={<ChatMessageReference pinned content={"I really appreciate it.I’ve been trying to diversify,"} closeable />}>
+                    <ChatMessageGroup type="sender" avatar={null} author={null}>
                         <ChatMessage />
                     </ChatMessageGroup>
-                    <ChatMessageGroup>
-                        <ChatMessage typing />
+                    <ChatMessageGroup avatar={null} author={null}>
+                        <ChatMessage text="I really appreciate it.I’ve been trying to diversify," />
                     </ChatMessageGroup>
                 </ChatNormal>
             </section>
+            <section>
+                <ChatNormal dir="rtl" pinned={<ChatMessageReference pinned type="sender" content={"I really appreciate it.I’ve been trying to diversify,"} closeable />}>
+                    <ChatMessageGroup author={null}>
+                        <ChatMessage />
+                    </ChatMessageGroup>
+                    <ChatMessageGroup type="sender" avatar={null} author={null}>
+                        <ChatMessage text="I really appreciate it.I’ve been trying to diversify," />
+                    </ChatMessageGroup>
+                </ChatNormal>
+            </section>
+            <section></section>
 
+            <span>Reply message / receiver</span>
+            <span>Reply message / sender</span>
+            <span></span>
+
+            <section>
+                <ChatNormal dir="rtl" replied={<ChatMessageReference content={"I really appreciate it.I’ve been trying to diversify,"} closeable />} >
+                    <ChatMessageGroup type="sender" author={null}>
+                        <ChatMessage />
+                    </ChatMessageGroup>
+                    <ChatMessageGroup avatar={null} author={null}>
+                        <ChatMessage text="I really appreciate it.I’ve been trying to diversify," />
+                    </ChatMessageGroup>
+                </ChatNormal>
+            </section>
+            <section>
+                <ChatNormal dir="rtl" replied={<ChatMessageReference type="sender" content={"I really appreciate it.I’ve been trying to diversify,"} closeable />} >
+                    <ChatMessageGroup author={null}>
+                        <ChatMessage />
+                    </ChatMessageGroup>
+                    <ChatMessageGroup type="sender" avatar={null} author={null}>
+                        <ChatMessage text="I really appreciate it.I’ve been trying to diversify," />
+                    </ChatMessageGroup>
+                </ChatNormal>
+            </section>
+            <section></section>
         </div>
     </>
 );
