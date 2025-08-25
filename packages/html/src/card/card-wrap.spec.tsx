4fe7901483509652f60dec30d46c2e09c3bc8c80
Kiril Peyanski <peyanski@progress.com>
2025-08-25 11:57:10 +0300

    chore(html): add CardWrap component and update index exports

---
+++ packages/html/src/card/card-wrap.spec.tsx
@@ -0,0 +1,47 @@
+import { classNames, stateClassNames } from '../misc';
+
+export const CARD_WRAP_CLASSNAME = `k-card-wrap`;
+
+const states = ['focus', 'selected'];
+
+const options = {};
+
+const defaultOptions = {};
+
+export type KendoCardWrapOptions = {};
+
+export type KendoCardWrapProps = KendoCardWrapOptions & {};
+
+export type KendoCardWrapState = { [K in (typeof states)[number]]?: boolean };
+
+export const CardWrap = (
+    props: KendoCardWrapProps & KendoCardWrapState & React.HTMLAttributes<HTMLDivElement>
+) => {
+    const {
+        focus,
+        selected,
+        ...other
+    } = props;
+
+    return (
+        <div
+            {...other}
+            className={classNames(
+                props.className,
+                CARD_WRAP_CLASSNAME,
+                stateClassNames(CARD_WRAP_CLASSNAME, {
+                    focus,
+                    selected,
+                }),
+            )}>
+            {props.children}
+        </div>
+    );
+};
+
+CardWrap.states = states;
+CardWrap.options = options;
+CardWrap.className = CARD_WRAP_CLASSNAME;
+CardWrap.defaultOptions = defaultOptions;
+
+export default CardWrap;
