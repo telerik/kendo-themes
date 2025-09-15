61bd5c8cec9f68fc389524ecd2603765e46bce8b
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2025-09-15 10:47:39 +0300

    chore(integrations): migrate vite to v7

--- integrations/vite/vite.config.js
+++ integrations/vite/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                api: "modern",
+                api: "modern-compiler",
                 loadPaths: [ "../../node_modules" ]
             }
         }
@@ -13,11 +13,11 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                default: resolve(__dirname, 'assets/default/index.html'),
-                bootstrap: resolve(__dirname, 'assets/bootstrap/index.html'),
-                material: resolve(__dirname, 'assets/material/index.html'),
-                fluent: resolve(__dirname, 'assets/fluent/index.html'),
-                classic: resolve(__dirname, 'assets/classic/index.html')
+                default: resolve(import.meta.dirname, 'assets/default/index.html'),
+                bootstrap: resolve(import.meta.dirname, 'assets/bootstrap/index.html'),
+                material: resolve(import.meta.dirname, 'assets/material/index.html'),
+                fluent: resolve(import.meta.dirname, 'assets/fluent/index.html'),
+                classic: resolve(import.meta.dirname, 'assets/classic/index.html')
             }
         },
         outDir: 'dist/'
