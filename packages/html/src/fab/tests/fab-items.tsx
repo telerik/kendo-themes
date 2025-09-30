1badff8f2883351b915d5ed01f9b55866fab6616
Teya Veselinova <tveselin@progress.com>
2025-09-30 09:59:31 +0300

    chore(html): update fab tests

--- packages/html/src/fab/tests/fab-items.tsx
+++ packages/html/src/fab/tests/fab-items.tsx
@@ -31,7 +31,7 @@ export default () => (
             <span>Downward - Left Labels</span>
             {FloatingActionButton.options.size.map((size, index) => (
                 <span className="relative-container">
-                    <IconFloatingActionButton size={size} themeColor="primary" position="top-end" style={{ top: "16px", right: "16px" }} />
+                    <IconFloatingActionButton size={size} position="top-end" style={{ top: "16px", right: "16px" }} />
                     <div className="k-fab-popup k-popup k-popup-transparent" style={{ position: "absolute", top: `calc(16px + ${indexMap[index]}px)`, right: `calc(16px + calc(${indexMap[index]}px / 2) - calc(36px /2))` }}>
                         <FloatingActionButtonItems position="bottom">
                             {[ "normal", ...FloatingActionButtonItem.states ].map((state) => (
@@ -45,7 +45,7 @@ export default () => (
             <span>Downward - Right Labels</span>
             {FloatingActionButton.options.size.map((size, index) => (
                 <span className="relative-container">
-                    <IconFloatingActionButton size={size} position="top-start" themeColor="primary" style={{ top: "16px", left: "16px" }} />
+                    <IconFloatingActionButton size={size} position="top-start" style={{ top: "16px", left: "16px" }} />
                     <div className="k-fab-popup k-popup k-popup-transparent" style={{ position: "absolute", top: `calc(16px + ${indexMap[index]}px)`, left: `calc(16px + calc(${indexMap[index]}px / 2) - calc(36px /2))` }}>
                         <FloatingActionButtonItems position="bottom">
                             {[ "normal", ...FloatingActionButtonItem.states ].map((state) => (
@@ -59,7 +59,7 @@ export default () => (
             <span>Upward - Left Labels</span>
             {FloatingActionButton.options.size.map((size, index) => (
                 <span className="relative-container">
-                    <IconFloatingActionButton size={size} position="bottom-end" themeColor="primary" style={{ bottom: "16px", right: "16px" }} />
+                    <IconFloatingActionButton size={size} position="bottom-end" style={{ bottom: "16px", right: "16px" }} />
                     <div className="k-fab-popup k-popup k-popup-transparent" style={{ position: "absolute", bottom: `calc(16px + ${indexMap[index]}px)`, right: `calc(16px + calc(${indexMap[index]}px / 2) - calc(36px /2))` }}>
                         <FloatingActionButtonItems position="top">
                             {[ "normal", ...FloatingActionButtonItem.states ].map((state) => (
@@ -73,7 +73,7 @@ export default () => (
             <span>Upward - Right Labels</span>
             {FloatingActionButton.options.size.map((size, index) => (
                 <span className="relative-container">
-                    <IconFloatingActionButton size={size} position="bottom-start" themeColor="primary" style={{ bottom: "16px", left: "16px" }} />
+                    <IconFloatingActionButton size={size} position="bottom-start" style={{ bottom: "16px", left: "16px" }} />
                     <div className="k-fab-popup k-popup k-popup-transparent" style={{ position: "absolute", bottom: `calc(16px + ${indexMap[index]}px)`, left: `calc(16px + calc(${indexMap[index]}px / 2) - calc(36px /2))` }}>
                         <FloatingActionButtonItems position="top">
                             {[ "normal", ...FloatingActionButtonItem.states ].map((state) => (
