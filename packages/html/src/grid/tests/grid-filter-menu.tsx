b1d87d1859729edecbb46062e04b2c439a8f173d
Emil Petrov <epetrov@progress.com>
2025-10-09 16:21:26 +0300

    test(grid): include filter menu with long operator text

--- packages/html/src/grid/tests/grid-filter-menu.tsx
+++ packages/html/src/grid/tests/grid-filter-menu.tsx
@@ -22,6 +22,9 @@ export default () =>(
         <style>{styles}</style>
         <div id="test-area" className="k-d-grid k-grid-cols-2">
 
+            <span>Normal</span>
+            <span>Long filter operator</span>
+
             {/* jQuery rendering */}
             <section>
                 <AnimationContainer>
@@ -29,6 +32,24 @@ export default () =>(
                         <div className="k-filter-menu-container">
                             <div className="k-filter-help-text">Show items with value that:</div>
                             <DropdownList value="Is equal to" />
+                            <NumericTextbox />
+                            <DropdownList value="And" className="k-filter-and" />
+                            <DropdownList value="Is equal to" />
+                            <NumericTextbox />
+                            <ActionButtons alignment="stretched">
+                                <Button icon="filter" themeColor="primary">Filter</Button>
+                                <Button icon="filter-clear">Clear</Button>
+                            </ActionButtons>
+                        </div>
+                    </form>
+                </AnimationContainer>
+            </section>
+            <section>
+                <AnimationContainer>
+                    <form className="k-filter-menu k-popup k-group k-reset">
+                        <div className="k-filter-menu-container">
+                            <div className="k-filter-help-text">Show items with value that:</div>
+                            <DropdownList value="Is greater than or equal to" />
                             <NumericTextbox />
                             <DropdownList value="And" className="k-filter-and" />
                             <DropdownList value="Is equal to" />
