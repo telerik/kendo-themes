5d5b73d77e2271563ec5d58582cb7fe67cdc4bb9
zhpenkov <penkov@progress.com>
2025-10-21 20:09:24 +0300

    test(units): update tests

--- units/fluent/grid.test.ts
+++ units/fluent/grid.test.ts
@@ -33,12 +33,22 @@ const dependencyClassNames = [
 const expected = [
     "kendo-grid-padding-x", // Variable customizations work, but is used by another variable.
     "kendo-grid-padding-y", // Variable customizations work, but is used by another variable.
-    "kendo-grid-group-drop-hint-height" // Variable customizations work, but is used by another variable.
+    "kendo-grid-group-drop-hint-height", // Variable customizations work, but is used by another variable.
+    "kendo-grid-grouping-header-padding-x", // Variable customizations work, but is used by another variable.
+    "kendo-grid-filter-cell-padding-x", // Variable customizations work, but is used by another variable.
+    "kendo-grid-filter-cell-padding-y", // Variable customizations work, but is used by another variable.
+    "kendo-grid-edit-cell-padding-x", // Variable customizations work, but is used by another variable.
+    "kendo-grid-edit-cell-padding-y", // Variable customizations work, but is used by another variable.
+    "kendo-grid-sm-padding-x", // Variable customizations work, but is used by another variable.
+    "kendo-grid-sm-padding-y", // Variable customizations work, but is used by another variable.
+    "kendo-grid-md-padding-x", // Variable customizations work, but is used by another variable.
+    "kendo-grid-md-padding-y" // Variable customizations work, but is used by another variable.
 ];
 
 const unexpected = [
     "kendo-grid-alt-bg", // This variable is used in the Core.
     "kendo-grid-sticky-footer-hover-bg", // This variable is used in the Bootstap and Material themes.
+    "kendo-grid-header-padding-x" // This variable is only used in the Gantt component.
 ];
 
 testKendoComponent(component, group, className, dependencyClassNames, [...expected, ...unexpected]);
