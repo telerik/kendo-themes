60620f09461bd5b1b940e7dd291338092019a5c5
Iliyana Dyankova <inikolova@progress.com>
2025-12-05 14:04:37 +0200

    chore: update template

--- packages/html/src/prompt-input/templates/prompt-input-opened.tsx
+++ packages/html/src/prompt-input/templates/prompt-input-opened.tsx
@@ -7,18 +7,21 @@ export const PromptInputOpened = (props) => (
     <PromptInput
         opened
         popup={
-            <List>
-                <ListItemNew showIcon iconName="gear" description='Long long long description'>List item</ListItemNew>
-                <ListItemNew>List item</ListItemNew>
-                <ListItemNew showIcon iconName="gear">List item</ListItemNew>
-                <ListItemNew description='Long long long description' endContent={
-                        <>
-                            <IconButton icon="star" />
-                            <Icon icon="star" />
-                            <IconButton icon="star" />
-                        </>
-                    }>List item</ListItemNew>
-            </List>
+            <>
+                <div style={{border: "2px solid red"}}>Segmented control</div>
+                <List>
+                    <ListItemNew showIcon iconName="gear" description='Long long long description'>List item</ListItemNew>
+                    <ListItemNew>List item</ListItemNew>
+                    <ListItemNew showIcon iconName="gear">List item</ListItemNew>
+                    <ListItemNew description='Long long long description' endContent={
+                            <>
+                                <IconButton icon="star" />
+                                <Icon icon="star" />
+                                <IconButton icon="star" />
+                            </>
+                        }>List item</ListItemNew>
+                </List>
+            </>
         }
         {...props}
     />
