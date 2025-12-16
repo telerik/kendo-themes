baf91f5d514355c6f50224ab01625acffc53cf1d
Iliyana Dyankova <inikolova@progress.com>
2025-12-16 17:59:16 +0200

    feat(html): add smart-box component rendering and tests

---
+++ packages/html/src/smart-box/constants.ts
@@ -0,0 +1,3 @@
+export const SMART_BOX_MODULE_NAME = 'smart-box';
+export const SMART_BOX_FOLDER_NAME = SMART_BOX_MODULE_NAME;
+
---
+++ packages/html/src/smart-box/index.ts
@@ -0,0 +1,3 @@
+export * from './smart-box.spec';
+export * from "./templates/smart-box-normal";
+export * from "./templates/smart-box-opened";
