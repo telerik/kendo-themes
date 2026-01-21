a5b5d6e5c85ce63cec4ea6f399ae6bf5bb94f559
Emil Petrov <epetrov@progress.com>
2026-01-21 14:23:20 +0200

    chore(html): decouple popup from menu-button spec and templates

--- packages/html/src/menu-button/templates/menu-button-popup.tsx
+++ packages/html/src/menu-button/templates/menu-button-popup.tsx
@@ -1,17 +1,16 @@
 import MenuButton from "../menu-button.spec";
 import { MenuList, MenuListItem } from '../../menu';
+import { Popup } from '../../popup';
 
 export const MenuButtonPopup = (props) => (
-    <MenuButton
-        children="button"
-        opened={true}
-        popup={
+    <>
+        <MenuButton {...props}>button</MenuButton>
+        <Popup className="k-menu-popup">
             <MenuList>
                 <MenuListItem first text="Menu Item"></MenuListItem>
                 <MenuListItem text="Menu Item"></MenuListItem>
                 <MenuListItem last text="Menu Item"></MenuListItem>
             </MenuList>
-        }
-        {...props}
-    />
+        </Popup>
+    </>
 );
