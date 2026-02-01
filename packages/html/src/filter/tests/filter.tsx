119c1542eb581aa775d3f4f4454dea9f2c16de85
dtopalov <dimiter.topalov79@gmail.com>
2026-02-01 15:19:41 +0200

    chore: filter

--- packages/html/src/filter/tests/filter.tsx
+++ packages/html/src/filter/tests/filter.tsx
@@ -14,114 +14,114 @@ export default () =>(
 
             <FilterNormal>
                 <FilterToolbar>
-                    <Toolbar focus>
+                    <Toolbar focus aria-label="Filter group toolbar">
                         <ButtonGroup>
                             <Button className="k-group-start" selected>And</Button>
                             <Button className="k-group-end">Or</Button>
                         </ButtonGroup>
                         <Button icon="filter-add-expression">Add Expression</Button>
                         <Button icon="filter-add-group">Add Group</Button>
-                        <Button fillMode="flat" icon="x"></Button>
+                        <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                     </Toolbar>
                 </FilterToolbar>
                 <FilterLines>
                     <FilterItem>
                         <FilterToolbar>
-                            <Toolbar>
+                            <Toolbar aria-label="Filter group toolbar">
                                 <ToolbarItem className="k-filter-field">
-                                    <DropdownList value="Name" />
+                                    <DropdownList aria-label="Field" value="Name" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-operator" focus={true}>
-                                    <DropdownList value="Is equal to" />
+                                    <DropdownList aria-label="Operator" value="Is equal to" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-value">
                                     <Textbox placeholder="James"/>
                                 </ToolbarItem>
-                                <Button fillMode="flat" icon="x"></Button>
+                                <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                             </Toolbar>
                         </FilterToolbar>
                     </FilterItem>
                     <FilterItem>
                         <FilterToolbar>
-                            <Toolbar>
+                            <Toolbar aria-label="Filter group toolbar">
                                 <ToolbarItem className="k-filter-field">
-                                    <DropdownList value="Price" />
+                                    <DropdownList aria-label="Field" value="Price" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-operator">
-                                    <DropdownList value="Is greater than" />
+                                    <DropdownList aria-label="Operator" value="Is greater than" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-value">
                                     <NumericTextbox placeholder="100,00"/>
                                 </ToolbarItem>
-                                <Button fillMode="flat" icon="x"></Button>
+                                <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                             </Toolbar>
                         </FilterToolbar>
                     </FilterItem>
                     <FilterItem>
                         <FilterToolbar>
-                            <Toolbar focus>
+                            <Toolbar focus aria-label="Filter group toolbar">
                                 <ToolbarItem className="k-filter-field">
-                                    <DropdownList value="Discontinued" />
+                                    <DropdownList aria-label="Field" value="Discontinued" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-value">
-                                    <DropdownList value="Is false"/>
+                                    <DropdownList aria-label="Value" value="Is false"/>
                                 </ToolbarItem>
-                                <Button fillMode="flat" icon="x"></Button>
+                                <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                             </Toolbar>
                         </FilterToolbar>
                     </FilterItem>
                     <FilterItem>
                         <FilterToolbar>
-                            <Toolbar>
+                            <Toolbar aria-label="Filter group toolbar">
                                 <ToolbarItem className="k-filter-field">
-                                    <DropdownList value="First Ordered" />
+                                    <DropdownList aria-label="Field" value="First Ordered" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-operator">
-                                    <DropdownList value="Is before" />
+                                    <DropdownList aria-label="Operator" value="Is before" />
                                 </ToolbarItem>
                                 <ToolbarItem className="k-filter-value">
                                     <DatePicker placeholder="1/21/2021" />
                                 </ToolbarItem>
-                                <Button fillMode="flat" icon="x"></Button>
+                                <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                             </Toolbar>
                         </FilterToolbar>
                     </FilterItem>
                     <FilterItem>
                         <FilterToolbar>
-                            <Toolbar>
+                            <Toolbar aria-label="Filter group toolbar">
                                 <ButtonGroup>
                                     <Button className="k-group-start" selected>And</Button>
                                     <Button className="k-group-end">Or</Button>
                                 </ButtonGroup>
                                 <Button icon="filter-add-expression">Add Expression</Button>
                                 <Button icon="filter-add-group">Add Group</Button>
-                                <Button fillMode="flat" icon="x"></Button>
+                                <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                             </Toolbar>
                         </FilterToolbar>
                         <FilterLines>
                             <FilterItem>
                                 <FilterToolbar>
-                                    <Toolbar>
+                                    <Toolbar aria-label="Filter group toolbar">
                                         <ToolbarItem className="k-filter-field">
-                                            <DropdownList value="Name" />
+                                            <DropdownList aria-label="Field" value="Name" />
                                         </ToolbarItem>
                                         <ToolbarItem className="k-filter-operator">
-                                            <DropdownList value="Is empty" />
+                                            <DropdownList aria-label="Operator" value="Is empty" />
                                         </ToolbarItem>
-                                        <Button fillMode="flat" icon="x"></Button>
+                                        <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                                     </Toolbar>
                                 </FilterToolbar>
                             </FilterItem>
                             <FilterItem>
                                 <FilterToolbar>
-                                    <Toolbar>
+                                    <Toolbar aria-label="Filter group toolbar">
                                         <ToolbarItem className="k-filter-field">
-                                            <DropdownList value="Price" />
+                                            <DropdownList aria-label="Field" value="Price" />
                                         </ToolbarItem>
                                         <ToolbarItem className=" k-filter-operator">
-                                            <DropdownList value="Is not null" />
+                                            <DropdownList aria-label="Operator" value="Is not null" />
                                         </ToolbarItem>
-                                        <Button fillMode="flat" icon="x"></Button>
+                                        <Button fillMode="flat" icon="x" aria-label="Remove filter"></Button>
                                     </Toolbar>
                                 </FilterToolbar>
                             </FilterItem>
