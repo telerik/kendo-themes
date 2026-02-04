1014453afcd2d2ac94e2a8b999af56fc73ff0d35
Emil Petrov <epetrov@progress.com>
2026-02-04 14:13:50 +0200

    chore(html): add hint and error specs and demos

---
+++ packages/html/src/form/demos/form-error.tsx
@@ -0,0 +1,39 @@
+import { FormError, KendoFormErrorProps } from '../form-error.spec';
+import { Textbox } from '../../textbox';
+
+const options = FormError.options;
+const states = FormError.states;
+const defaults = {
+    ...FormError.defaultOptions,
+};
+const variants = [];
+const modifiers = [
+    {
+        name: 'showError',
+        title: 'Show Error',
+    },
+];
+
+export const FormErrorDemo = (
+    props: KendoFormErrorProps & {
+        modifiers?: { [key: string]: boolean };
+    }
+) => {
+    const { modifiers: mods, ...other } = { ...defaults, ...props };
+    const showError = mods?.showError ?? true;
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
+            <Textbox placeholder="Component" invalid={showError} style={{ width: '200px' }} />
+            {showError && <FormError {...other}>Error text</FormError>}
+        </div>
+    );
+};
+
+FormErrorDemo.options = options;
+FormErrorDemo.states = states;
+FormErrorDemo.variants = variants;
+FormErrorDemo.defaultOptions = defaults;
+FormErrorDemo.modifiers = modifiers;
+
+export default FormErrorDemo;
