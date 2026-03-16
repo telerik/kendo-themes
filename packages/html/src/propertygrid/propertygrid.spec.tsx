47d6e5928235830e291a30296be3178819592ce7
dtopalov <dimiter.topalov79@gmail.com>
2026-03-16 17:54:43 +0200

    chore: add pivot disabled configurator template

--- packages/html/src/propertygrid/propertygrid.spec.tsx
+++ packages/html/src/propertygrid/propertygrid.spec.tsx
@@ -50,23 +50,23 @@ PropertyGrid.ariaSpec = {
         // ── Toolbar ──
         { selector: '.k-grid-toolbar', attribute: 'role=toolbar', usage: 'The toolbar is a collection of command buttons and inputs.' },
         { selector: '.k-grid-toolbar', attribute: 'aria-label', usage: 'Clarifies the purpose of the toolbar.' },
-        { selector: '.k-grid-toolbar[aria-controls]', attribute: 'aria-controls=.k-grid-aria-root id', usage: 'Points to the id of the element with role=treegrid.' },
+        { selector: '.k-grid-toolbar', attribute: 'aria-controls=.k-grid-aria-root id', usage: 'Points to the id of the element with role=treegrid.' },
 
         // ── TreeGrid Element ──
         { selector: '.k-property-grid .k-grid-aria-root', attribute: 'role=treegrid', usage: 'Sets the proper role for the PropertyGrid component.' },
-        { selector: '.k-grid-aria-root[aria-colcount]', attribute: 'aria-colcount', usage: 'The total number of columns.' },
-        { selector: '.k-grid-aria-root[aria-rowcount]', attribute: 'aria-rowcount', usage: 'The total number of rows in the table.' },
+        { selector: '.k-grid-aria-root', attribute: 'aria-colcount', usage: 'The total number of columns.' },
+        { selector: '.k-grid-aria-root', attribute: 'aria-rowcount', usage: 'The total number of rows in the table.' },
 
         // ── Content ──
         { selector: '.k-grid-content>table', attribute: 'role=none', usage: 'Negates the default semantic role of the <table> element.' },
         { selector: '.k-grid-content>table>tbody', attribute: 'role=rowgroup', usage: 'Required as the owner <table> element has its semantic role removed.' },
         { selector: '.k-grid-content>table>tbody>tr:not(.k-hidden)', attribute: 'role=row', usage: 'Required as the owner <table> element has its semantic role removed.' },
-        { selector: '.k-grid-content>table>tbody>tr[aria-rowindex]', attribute: 'aria-rowindex', usage: 'Row number.' },
+        { selector: '.k-grid-content>table>tbody>tr:not(.k-hidden)', attribute: 'aria-rowindex', usage: 'Row number.' },
         { selector: '.k-grid-content>table>tbody>tr:not(.k-hidden)>td', attribute: 'role=gridcell', usage: 'Required as the owner <table> element has its semantic role removed.' },
-        { selector: '.k-grid-content>table>tbody>tr>td[aria-colindex]', attribute: 'aria-colindex', usage: 'Col number.' },
+        { selector: '.k-grid-content>table>tbody>tr:not(.k-hidden)>td', attribute: 'aria-colindex', usage: 'Col number.' },
 
         // ── TreeList-Specific ──
-        { selector: '.k-grid-content>table>tbody>tr[aria-expanded]', attribute: 'aria-expanded', usage: 'Set on the currently expanded row(s).' },
+        { selector: '.k-grid-content>table>tbody>tr:not(.k-hidden)', attribute: 'aria-expanded (when expandable)', usage: 'Set on the currently expanded row(s).' },
         { selector: '.k-treelist-toggle[class*="i-caret-alt-down"],.k-treelist-toggle[class*="i-caret-alt-right"]', attribute: 'aria-hidden=true', usage: 'Removes the expand/collapse icon from the accessibility tree.' },
     ]
 };
