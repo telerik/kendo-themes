facfc51934bb9251c40b22d8bc12c855aab50bd8
zhpenkov <penkov@progress.com>
2026-06-02 19:13:04 +0300

    fix(html): correct scheduler templates

--- packages/html/src/scheduler/templates/scheduler-flex-week.tsx
+++ packages/html/src/scheduler/templates/scheduler-flex-week.tsx
@@ -17,7 +17,7 @@ export const SchedulerFlexWeek = ({ style, ...props }: any) => (
                     <Button className="k-group-end" icon="chevron-right" aria-label="Navigate to next period"></Button>
                 </ButtonGroup>
                 <Button icon="calendar" className="k-nav-current" fillMode="flat" aria-live="polite">
-                    6/23/2022 - 6/29/2022
+                    6/12/2022 - 6/18/2022
                 </Button>
                 <span className="k-spacer"></span>
                 <SegmentedControl thumbStyles={{ width: "18%", left: "16%" }} className="k-scheduler-views">
@@ -42,13 +42,13 @@ export const SchedulerFlexWeek = ({ style, ...props }: any) => (
                             <SchedulerCell as="div" cellType={[ "side" ]} style={{ minWidth: "65px" }} />
                             <SchedulerCell as="div" cellType={[ "group" ]}>
                                 <SchedulerRow>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/23"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/24"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/25"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/26"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/27"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/28"></SchedulerCell>
-                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/29"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sun, 6/12"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Mon, 6/13"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Tue, 6/14"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Wed, 6/15"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Thu, 6/16"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Fri, 6/17"></SchedulerCell>
+                                    <SchedulerCell as="div" cellType={[ "heading" ]} text="Sat, 6/18"></SchedulerCell>
                                 </SchedulerRow>
                             </SchedulerCell>
                         </SchedulerRow>
