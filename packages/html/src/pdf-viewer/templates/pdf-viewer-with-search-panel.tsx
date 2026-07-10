2f54ca73162d3573f3a4a38e4d99554abfcca7a5
Emil Petrov <epetrov@progress.com>
2026-07-10 14:04:28 +0300

    chore(html): use floating toolbar instead of pdf-viewer search panel

--- packages/html/src/pdf-viewer/templates/pdf-viewer-with-search-panel.tsx
+++ packages/html/src/pdf-viewer/templates/pdf-viewer-with-search-panel.tsx
@@ -1,8 +1,12 @@
 import { PDFViewer, PDFViewerPage } from "..";
+import { FloatingToolbar } from "../../toolbar/floating-toolbar.spec";
+import { ToolbarSeparator } from "../../toolbar/toolbar-separator";
+import { Textbox } from "../../textbox/textbox.spec";
+import { Button } from "../../button/button.spec";
 
 export const PDFViewerWithSearchPanel = (props) => (
+    <>
     <PDFViewer
-        showSearchPanel
         children={
             <>
                 <PDFViewerPage />
@@ -11,4 +15,19 @@ export const PDFViewerWithSearchPanel = (props) => (
         }
         {...props}
     />
+    <FloatingToolbar fillMode="flat">
+        <Button className="k-search-dialog-draghandle" fillMode="flat" icon="handle-drag"/>
+        <ToolbarSeparator />
+        <Textbox fillMode="flat" placeholder="Search" suffix={<Button className="k-match-case-button" fillMode="flat" icon="convert-lowercase" />}  />
+        <div className="k-search-matches">
+            of <span>0</span>
+        </div>
+        <ToolbarSeparator />
+        <Button fillMode="flat" icon="arrow-up" />
+        <Button fillMode="flat" icon="arrow-down" />
+        <ToolbarSeparator />
+        <Button fillMode="flat" icon="x" />
+    </FloatingToolbar>
+    </>
+
 );
--- packages/html/src/pdf-viewer/tests/pdf-viewer-search-panel.tsx
+++ packages/html/src/pdf-viewer/tests/pdf-viewer-search-panel.tsx
@@ -1,8 +1,22 @@
 import { PDFViewerWithAnnotations, PDFViewerWithSearchPanel } from '../../pdf-viewer';
+import { FloatingToolbar } from '../../toolbar/floating-toolbar.spec';
+import { ToolbarSeparator } from '../../toolbar/toolbar-separator';
+import { Textbox } from '../../textbox/textbox.spec';
+import { Button } from '../../button/button.spec';
 
 const styles = `
     #test-area .k-pdf-viewer {
         height: 250px;
+    }
+
+    .k-animation-container {
+        position: relative;
+        bottom: 213px;
+        left: 300px;
+    }
+
+    .k-animation-container:has(.annotations-search-panel) {
+        bottom: 155px;
     }
 `;
 
@@ -14,7 +28,20 @@ export default () =>(
             <PDFViewerWithSearchPanel />
 
             <span>PDF Viewer + Annotations Toolbar with Search Panel</span>
-            <PDFViewerWithAnnotations showSearchPanel />
+            <PDFViewerWithAnnotations />
+            <FloatingToolbar className="annotations-search-panel">
+                <Button className="k-search-dialog-draghandle" fillMode="flat" icon="handle-drag"/>
+                <ToolbarSeparator />
+                <Textbox fillMode="flat" placeholder="Search" suffix={<Button className="k-match-case-button" fillMode="flat" icon="convert-lowercase" />}  />
+                <div className="k-search-matches">
+                    of <span>0</span>
+                </div>
+                <ToolbarSeparator />
+                <Button fillMode="flat" icon="arrow-up" />
+                <Button fillMode="flat" icon="arrow-down" />
+                <ToolbarSeparator />
+                <Button fillMode="flat" icon="x" />
+            </FloatingToolbar>
         </div>
     </>
 );
