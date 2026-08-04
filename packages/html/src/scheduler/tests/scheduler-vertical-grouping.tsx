0a9a4bd6bd0254d71f24e92a38472c0f3eda767a
zhpenkov <penkov@progress.com>
2026-08-04 14:24:14 +0300

    chore: add missing tests and correct styles

---
+++ packages/html/src/scheduler/tests/scheduler-vertical-grouping.tsx
@@ -0,0 +1,20 @@
+import { SchedulerVerticalGrouping, SchedulerFlexVerticalGrouping } from '../';
+
+const style = `
+    #test-area {
+        max-width: 1080px;
+    }
+`;
+
+export default () => (
+    <>
+        <style>{style}</style>
+        <div id="test-area" className="k-d-grid k-grid-cols-1">
+            <span>Scheduler Table Layout</span>
+            <SchedulerVerticalGrouping />
+
+            <span>Scheduler Flex Layout</span>
+            <SchedulerFlexVerticalGrouping today />
+        </div>
+    </>
+);
