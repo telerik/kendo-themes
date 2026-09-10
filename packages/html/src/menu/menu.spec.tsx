8ea9055a888a1b21c0d3fc18a92e50252074b99f
zhpenkov <penkov@progress.com>
2026-09-10 18:07:51 +0300

    feat(menu): add menu link item sizes

--- packages/html/src/menu/menu.spec.tsx
+++ packages/html/src/menu/menu.spec.tsx
@@ -1,5 +1,5 @@
 import { Button } from '../button';
-import { classNames } from '../misc';
+import { classNames, Size, optionClassNames } from '../misc';
 import MenuScrollableWrapper from './menu-scrollable-wrapper.spec';
 
 import { KendoComponent } from '../_types/component';
@@ -8,7 +8,9 @@ export const MENU_CLASSNAME = `k-menu`;
 
 const states = [];
 
-const options = {};
+const options = {
+    size: [ Size.undefined, Size.small, Size.medium, Size.large ]
+};
 
 export type KendoMenuProps = {
     orientation?: 'horizontal' | 'vertical';
@@ -19,6 +21,7 @@ export type KendoMenuProps = {
     dir?: "rtl" | "ltr";
     popup?: React.JSX.Element;
     contextMenu?: boolean;
+    size?: (typeof options.size)[number] | null;
 };
 
 const defaultOptions = {
@@ -50,6 +53,7 @@ export const Menu: KendoComponent<KendoMenuProps & React.HTMLAttributes<HTMLULis
         scrollButtonsPosition = defaultOptions.scrollButtonsPosition,
         wrapperStyles,
         dir,
+        size,
         popup,
         contextMenu,
         ...other
@@ -71,7 +75,10 @@ export const Menu: KendoComponent<KendoMenuProps & React.HTMLAttributes<HTMLULis
                 {
                     [`${MENU_CLASSNAME}-${orientation}`]: orientation,
                     ["k-header"]: header
-                }
+                },
+                optionClassNames(MENU_CLASSNAME, {
+                    size,
+                })
             )}
             role={contextMenu ? 'menu' : 'menubar'}
             {...(orientation === 'vertical' && { 'aria-orientation': 'vertical' as React.AriaAttributes['aria-orientation'] })}
