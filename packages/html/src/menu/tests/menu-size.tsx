76a4d64357bd35032128c55c32fe3cc99331aae7
zhpenkov <penkov@progress.com>
2026-09-10 18:07:51 +0300

    feat(menu): add menu link item sizes

---
+++ packages/html/src/menu/tests/menu-size.tsx
@@ -0,0 +1,23 @@
+import { Menu, MenuItem, MenuNormal } from "..";
+
+export default () => (
+  <>
+    <div id="test-area" className="k-d-grid k-grid-cols-1">
+      <span>Horizontal Menu</span>
+
+      {Menu.options.size.map((size, i) => (
+        <section key={i}>
+          <span>{`${size}`}</span>
+          <MenuNormal size={size}>
+            <MenuItem first text="Normal" icon="folder"></MenuItem>
+            <MenuItem text="Hover" hover icon="folder"></MenuItem>
+            <MenuItem text="Active" icon="folder" active></MenuItem>
+            <MenuItem text="Selected" selected icon="folder"></MenuItem>
+            <MenuItem focus text="Focused" icon="folder"></MenuItem>
+            <MenuItem last text="Disabled" icon="folder" disabled></MenuItem>
+          </MenuNormal>
+        </section>
+      ))}
+    </div>
+  </>
+);
